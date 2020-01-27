3d5e5d2823a946fd698ed66fc3edf6c918d21888
Pete K <krautscheid@mitre.org>
2020-01-27 15:37:01 -0500

    Adds assessments relationship to patient model

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,4 +1,5 @@
 class Patient < ApplicationRecord
   belongs_to :responder, class_name: 'Patient'
   has_many :dependents, class_name: 'Patient', foreign_key: 'responder_id'
+  has_many :assessments
 end
