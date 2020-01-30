284971390adc1595495013b15520855fd50fffbf
Adam M. Holmes <aholmes@mitre.org>
2020-01-30 13:03:58 -0500

    DiseaseTrakker wip name

--- app/mailers/patient_mailer.rb
+++ app/mailers/patient_mailer.rb
@@ -1,5 +1,5 @@
 class PatientMailer < ApplicationMailer
-  default from: 'notifications@sara.mitre.org'
+  default from: 'notifications@DiseaseTrakker.org'
 
   def assessment_email(patient)
     @patient = patient
@@ -12,7 +12,7 @@ class PatientMailer < ApplicationMailer
   end
 
   def enrollment_sms(patient)
-    contents = "This is the SARA system please complete your assessment at #{new_patient_assessment_url(patient.submission_token)}"
+    contents = "This is the DiseaseTrakker system please complete your assessment at #{new_patient_assessment_url(patient.submission_token)}"
     account_sid = ENV['TWILLIO_API_ACCOUNT']
     auth_token = ENV['TWILLIO_API_KEY']
     from = ENV['TWILLIO_SENDING_NUMBER']
