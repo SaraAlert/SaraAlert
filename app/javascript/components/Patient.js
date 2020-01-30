3475ab74d8a1c01e97fe81da076cea1215e834f4
Adam M. Holmes <aholmes@mitre.org>
2020-01-30 09:01:44 -0500

    Refactored patient view data population mechanism

--- app/javascript/components/Patient.js
+++ app/javascript/components/Patient.js
@@ -1,17 +1,46 @@
 import React from "react"
-import { Button } from 'react-bootstrap';
+import { Button, Col, Row } from 'react-bootstrap';
 
 class Patient extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = { };
   }
 
   render () {
+    if (!!!this.props.details) {
+      return (<React.Fragment></React.Fragment>);
+    }
     return (
       <React.Fragment>
-
+        <Row>
+          <Col>
+            <Row>
+              <Col>
+                <span className="font-weight-normal">DOB:</span> <span className="font-weight-light">{this.props.details.dob_day && this.props.details.dob_month && this.props.details.dob_year && `${this.props.details.dob_day}-${this.props.details.dob_month}-${this.props.details.dob_year}`}</span><br />
+                <span className="font-weight-normal">Age:</span> <span className="font-weight-light">{`${this.props.details.age ? this.props.details.age : ''}`}</span><br />
+                <span className="font-weight-normal">Language:</span> <span className="font-weight-light">{`${this.props.details.primary_language ? this.props.details.primary_language : ''}`}</span>
+              </Col>
+              <Col>
+                <span className="font-weight-normal">Sex:</span> <span className="font-weight-light">{`${this.props.details.sex ? this.props.details.sex : ''}`}</span><br />
+                <span className="font-weight-normal">Race:</span> <span className="font-weight-light">{`${this.props.details.white ? 'White' : ''}${this.props.details.black_or_african_american ? ' Black or African American' : ''}${this.props.details.asian ? ' Asian' : ''}${this.props.details.american_indian_or_alaska_native ? ' American Indian or Alaska Native' : ''}${this.props.details.native_hawaiian_or_other_pacific_islander ? ' Native Hawaiian or Other Pacific Islander' : ''}`}</span><br />
+                <span className="font-weight-normal">Ethnicity:</span> <span className="font-weight-light">{`${this.props.details.ethnicity ? this.props.details.ethnicity : ''}`}</span>
+              </Col>
+            </Row>
+          </Col>
+          <Col>
+            <Row>
+              <Col>
+              </Col>
+              <Col>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+        <Row>
+          <Col></Col>
+          <Col></Col>
+        </Row>
       </React.Fragment>
     );
   }
--- app/javascript/components/enrollment/steps/Review.js
+++ app/javascript/components/enrollment/steps/Review.js
@@ -6,16 +6,6 @@ class Review extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = { ...this.props };
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  handleChange(event) {
-    let value = event.target.type === "checkbox" ? event.target.checked : event.target.value;
-    let current = this.state.current;
-    this.setState({current: {...current, [event.target.id]: value}}, () => {
-      this.props.setEnrollmentState({ ...this.state.current });
-    });
   }
 
   render () {
@@ -24,7 +14,7 @@ class Review extends React.Component {
         <Card className="mx-4 card-square">
           <Card.Header as="h5">Subject Review</Card.Header>
           <Card.Body>
-            <Patient className="pb-3" details={this.state.current} />
+            <Patient className="pb-3" details={this.props.currentState} />
             {this.props.previous && <Button variant="outline-primary" size="lg" className="btn-square px-5" onClick={this.props.previous}>Previous</Button>}
             {this.props.next && <Button variant="outline-primary" size="lg" className="float-right btn-square px-5" onClick={this.props.next}>Next</Button>}
             {this.props.finish && <Button variant="primary" size="lg" className="float-right btn-square px-5" onClick={this.props.finish}>Finish</Button>}
