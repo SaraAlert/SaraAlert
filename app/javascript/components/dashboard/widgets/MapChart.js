d859833200fdb32ca6e3e76fa19ae65ecae3e2ad
Matt Mayer <mmayer@mitre.org>
2020-02-05 11:45:12 -0500

    Add zoom

--- app/javascript/components/dashboard/widgets/MapChart.js
+++ app/javascript/components/dashboard/widgets/MapChart.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useState }  from "react";
 import { Card } from 'react-bootstrap';
-import { ComposableMap, Geographies, Geography } from "react-simple-maps";
+import { ComposableMap, Geographies, Geography, ZoomableGroup} from "react-simple-maps";
 import ReactTooltip from 'react-tooltip';
 import { scaleQuantize } from "d3-scale";
 import { USAMap, stateOptions } from '../../data';
@@ -14,7 +14,8 @@ class MapChart extends React.Component {
   state = {
     highlighted: "",
     hovered: false,
-    content: ""
+    content: "",
+    zoom: 1
   };
   stateOpts = stateOptions
   handleMove = geo => {
@@ -31,9 +32,28 @@ class MapChart extends React.Component {
     });
   };
 
-
+  projection = (width, height) => {
+    const albersUsa = d3
+      .geoAlbersUsa()
+      .scale(1500)
+      .translate([width / 2, height / 2]);
+    albersUsa.rotate = function rotate(...args) {
+      return args.length ? () => [width / 2, height / 2] : [0, 0, 0];
+    };
+    return albersUsa;
+  };
 
   render () {
+
+    const handleZoomIn = () => {
+      if (this.state.zoom >= 4) return;
+      this.setState({zoom: this.state.zoom * 2 })
+    }
+  
+    const handleZoomOut = () => {
+      if (this.state.zoom <= 1) return;
+      this.setState({zoom: this.state.zoom / 2 })
+    }
 
     const data = this.props.stats.monitoring_distribution_by_state;
     const colorScale = scaleQuantize()
@@ -59,6 +79,7 @@ class MapChart extends React.Component {
             <Card.Header as="h5">Location of Symptomatic Subjects</Card.Header>
               <Card.Body>
                 <ComposableMap data-tip="" projection="geoAlbersUsa">>
+                <ZoomableGroup center={[ -97, 40 ]} zoom={this.state.zoom}>
                   <Geographies geography={USAMap}>
                     {({ geographies }) =>
                       geographies.map(geo => (
@@ -90,7 +111,33 @@ class MapChart extends React.Component {
                       ))
                     }
               </Geographies>
+              </ZoomableGroup>
             </ComposableMap>
+            <div className="controls">
+        <button class="btn btn-outline-primary" onClick={handleZoomIn}>
+          <svg
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth="3"
+          >
+            <line x1="12" y1="5" x2="12" y2="19" />
+            <line x1="5" y1="12" x2="19" y2="12" />
+          </svg>
+        </button>
+        <button class="btn btn-outline-primary" onClick={handleZoomOut}>
+          <svg
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth="3"
+          >
+            <line x1="5" y1="12" x2="19" y2="12" />
+          </svg>
+        </button>
+      </div>
           </Card.Body>
         </Card>
       </React.Fragment>
