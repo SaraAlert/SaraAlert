924198624a0e6808ecd9a1d14b305d46370c8ffc
Matt Mayer <mmayer@mitre.org>
2020-02-10 13:58:16 -0500

    Add backend validation to patient and assessment and tests for validation

--- app/models/assessment.rb
+++ app/models/assessment.rb
@@ -1,3 +1,12 @@
 class Assessment < ApplicationRecord
+  # TODO: We assume that we can rely on the frontend for "context-aware" validation
+  columns.each do |column|
+    case column.type
+    when :text
+      validates column.name.to_sym, length: { maximum: 2000 }
+    when :string
+      validates column.name.to_sym, length: { maximum: 200 }
+    end
+  end
   belongs_to :patient
 end
--- app/models/group.rb
+++
@@ -1,4 +0,0 @@
-class Group < ApplicationRecord
-  has_many :people
-  has_many :reports
-end
