6660f2bcd0c9e3d16d760f45307de18c1088a220
Adam M. Holmes <aholmes@mitre.org>
2020-02-11 12:06:52 -0500

    Form validation for Arrival information

--- app/javascript/components/enrollment/steps/Arrival.js
+++ app/javascript/components/enrollment/steps/Arrival.js
@@ -6,8 +6,9 @@ import * as yup from 'yup';
 class Arrival extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { ...this.props, current: { ...this.props.currentState } };
+    this.state = { ...this.props, current: { ...this.props.currentState }, errors: {} };
     this.handleChange = this.handleChange.bind(this);
+    this.validate = this.validate.bind(this);
   }
 
   handleChange(event) {
@@ -18,8 +19,29 @@ class Arrival extends React.Component {
     });
   }
 
+  validate(callback) {
+    let self = this;
+    schema
+      .validate(this.state.current, { abortEarly: false })
+      .then(function() {
+        // No validation issues? Invoke callback (move to next step)
+        self.setState({ errors: {} }, () => {
+          callback();
+        });
+      })
+      .catch(function(err) {
+        // Validation errors, update state to display to user
+        if (err && err.inner) {
+          let issues = {};
+          for (var issue of err.inner) {
+            issues[issue['path']] = issue['errors'];
+          }
+          self.setState({ errors: issues });
+        }
+      });
+  }
+
   render() {
-    let today = new Date().toISOString().substr(0, 10);
     return (
       <React.Fragment>
         <Card className="mx-2 card-square">
@@ -28,60 +50,115 @@ class Arrival extends React.Component {
             <Form>
               <Form.Row className="pt-2">
                 <Form.Group as={Col} md="8" controlId="port_of_origin">
-                  <Form.Label className="nav-input-label">PORT OF ORIGIN</Form.Label>
-                  <Form.Control size="lg" className="form-square" value={this.state.current.port_of_origin || ''} onChange={this.handleChange} />
-                </Form.Group>
-                <Form.Group as={Col} md="6" controlId="date_of_departure">
-                  <Form.Label className="nav-input-label">DATE OF DEPARTURE</Form.Label>
+                  <Form.Label className="nav-input-label">PORT OF ORIGIN{schema?.fields?.port_of_origin?._exclusive?.required && ' *'}</Form.Label>
                   <Form.Control
+                    isInvalid={this.state.errors['port_of_origin']}
                     size="lg"
-                    type="date"
                     className="form-square"
-                    value={this.state.current.date_of_departure || today}
+                    value={this.state.current.port_of_origin || ''}
                     onChange={this.handleChange}
                   />
                 </Form.Group>
+                <Form.Group as={Col} md="6" controlId="date_of_departure">
+                  <Form.Label className="nav-input-label">DATE OF DEPARTURE{schema?.fields?.date_of_departure?._exclusive?.required && ' *'}</Form.Label>
+                  <Form.Control
+                    isInvalid={this.state.errors['date_of_departure']}
+                    size="lg"
+                    type="date"
+                    className="form-square"
+                    value={this.state.current.date_of_departure || ''}
+                    onChange={this.handleChange}
+                  />
+                  <Form.Control.Feedback className="d-block" type="invalid">
+                    {this.state.errors['port_of_origin']}
+                  </Form.Control.Feedback>
+                </Form.Group>
                 <Form.Group as={Col} md="2"></Form.Group>
                 <Form.Group as={Col} md="8" controlId="source_of_report">
-                  <Form.Label className="nav-input-label">SOURCE OF REPORT</Form.Label>
-                  <Form.Control as="select" size="lg" className="form-square" value={this.state.current.source_of_report || ''} onChange={this.handleChange}>
+                  <Form.Label className="nav-input-label">SOURCE OF REPORT{schema?.fields?.source_of_report?._exclusive?.required && ' *'}</Form.Label>
+                  <Form.Control
+                    isInvalid={this.state.errors['source_of_report']}
+                    as="select"
+                    size="lg"
+                    className="form-square"
+                    value={this.state.current.source_of_report || ''}
+                    onChange={this.handleChange}>
                     <option></option>
                     <option>Self-Identified</option>
                     <option>CDC</option>
                     <option>Other</option>
                   </Form.Control>
+                  <Form.Control.Feedback className="d-block" type="invalid">
+                    {this.state.errors['source_of_report']}
+                  </Form.Control.Feedback>
                 </Form.Group>
               </Form.Row>
               <Form.Row className="pt-2">
                 <Form.Group as={Col} md="8" controlId="flight_or_vessel_number">
-                  <Form.Label className="nav-input-label">FLIGHT OR VESSEL NUMBER</Form.Label>
-                  <Form.Control size="lg" className="form-square" value={this.state.current.flight_or_vessel_number || ''} onChange={this.handleChange} />
+                  <Form.Label className="nav-input-label">
+                    FLIGHT OR VESSEL NUMBER{schema?.fields?.flight_or_vessel_number?._exclusive?.required && ' *'}
+                  </Form.Label>
+                  <Form.Control
+                    isInvalid={this.state.errors['flight_or_vessel_number']}
+                    size="lg"
+                    className="form-square"
+                    value={this.state.current.flight_or_vessel_number || ''}
+                    onChange={this.handleChange}
+                  />
+                  <Form.Control.Feedback className="d-block" type="invalid">
+                    {this.state.errors['flight_or_vessel_number']}
+                  </Form.Control.Feedback>
                 </Form.Group>
                 <Form.Group as={Col} md="7" controlId="flight_or_vessel_carrier">
-                  <Form.Label className="nav-input-label">CARRIER</Form.Label>
-                  <Form.Control size="lg" className="form-square" value={this.state.current.flight_or_vessel_carrier || ''} onChange={this.handleChange} />
+                  <Form.Label className="nav-input-label">CARRIER{schema?.fields?.flight_or_vessel_carrier?._exclusive?.required && ' *'}</Form.Label>
+                  <Form.Control
+                    isInvalid={this.state.errors['flight_or_vessel_carrier']}
+                    size="lg"
+                    className="form-square"
+                    value={this.state.current.flight_or_vessel_carrier || ''}
+                    onChange={this.handleChange}
+                  />
+                  <Form.Control.Feedback className="d-block" type="invalid">
+                    {this.state.errors['flight_or_vessel_carrier']}
+                  </Form.Control.Feedback>
                 </Form.Group>
               </Form.Row>
               <Form.Row className="pt-2">
                 <Form.Group as={Col} md="8" controlId="port_of_entry_into_usa">
-                  <Form.Label className="nav-input-label">PORT OF ENTRY INTO USA</Form.Label>
-                  <Form.Control size="lg" className="form-square" value={this.state.current.port_of_entry_into_usa || ''} onChange={this.handleChange} />
+                  <Form.Label className="nav-input-label">
+                    PORT OF ENTRY INTO USA{schema?.fields?.port_of_entry_into_usa?._exclusive?.required && ' *'}
+                  </Form.Label>
+                  <Form.Control
+                    isInvalid={this.state.errors['port_of_entry_into_usa']}
+                    size="lg"
+                    className="form-square"
+                    value={this.state.current.port_of_entry_into_usa || ''}
+                    onChange={this.handleChange}
+                  />
+                  <Form.Control.Feedback className="d-block" type="invalid">
+                    {this.state.errors['port_of_entry_into_usa']}
+                  </Form.Control.Feedback>
                 </Form.Group>
                 <Form.Group as={Col} md="6" controlId="date_of_arrival">
-                  <Form.Label className="nav-input-label">DATE OF ARRIVAL</Form.Label>
+                  <Form.Label className="nav-input-label">DATE OF ARRIVAL{schema?.fields?.date_of_arrival?._exclusive?.required && ' *'}</Form.Label>
                   <Form.Control
+                    isInvalid={this.state.errors['date_of_arrival']}
                     size="lg"
                     type="date"
                     className="form-square"
-                    value={this.state.current.date_of_arrival || today}
+                    value={this.state.current.date_of_arrival || ''}
                     onChange={this.handleChange}
                   />
+                  <Form.Control.Feedback className="d-block" type="invalid">
+                    {this.state.errors['date_of_arrival']}
+                  </Form.Control.Feedback>
                 </Form.Group>
               </Form.Row>
               <Form.Row className="pt-2 pb-3">
                 <Form.Group as={Col} md="24" controlId="travel_related_notes">
-                  <Form.Label className="nav-input-label">TRAVEL RELATED NOTES</Form.Label>
+                  <Form.Label className="nav-input-label">TRAVEL RELATED NOTES{schema?.fields?.travel_related_notes?._exclusive?.required && ' *'}</Form.Label>
                   <Form.Control
+                    isInvalid={this.state.errors['travel_related_notes']}
                     as="textarea"
                     rows="5"
                     size="lg"
@@ -90,6 +167,9 @@ class Arrival extends React.Component {
                     value={this.state.current.travel_related_notes || ''}
                     onChange={this.handleChange}
                   />
+                  <Form.Control.Feedback className="d-block" type="invalid">
+                    {this.state.errors['travel_related_notes']}
+                  </Form.Control.Feedback>
                 </Form.Group>
               </Form.Row>
             </Form>
@@ -99,7 +179,7 @@ class Arrival extends React.Component {
               </Button>
             )}
             {this.props.next && (
-              <Button variant="outline-primary" size="lg" className="float-right btn-square px-5" onClick={this.props.next}>
+              <Button variant="outline-primary" size="lg" className="float-right btn-square px-5" onClick={() => this.validate(this.props.next)}>
                 Next
               </Button>
             )}
@@ -114,6 +194,17 @@ class Arrival extends React.Component {
     );
   }
 }
+
+const schema = yup.object().shape({
+  port_of_origin: yup.string().max(200, 'Max length exceeded, please limit to 200 characters.'),
+  date_of_departure: yup.date('Date must correspond to the "mm/dd/yyyy" format.'),
+  source_of_report: yup.string().max(200, 'Max length exceeded, please limit to 200 characters.'),
+  flight_or_vessel_number: yup.string().max(200, 'Max length exceeded, please limit to 200 characters.'),
+  flight_or_vessel_carrier: yup.string().max(200, 'Max length exceeded, please limit to 200 characters.'),
+  port_of_entry_into_usa: yup.string().max(200, 'Max length exceeded, please limit to 200 characters.'),
+  date_of_arrival: yup.date('Date must correspond to the "mm/dd/yyyy" format.'),
+  travel_related_notes: yup.string().max(2000, 'Max length exceeded, please limit to 2000 characters.'),
+});
 
 Arrival.propTypes = {
   currentState: PropTypes.object,
