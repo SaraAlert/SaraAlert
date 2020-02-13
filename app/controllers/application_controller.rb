7601f14ad33842d5e7c4a5151ae2e8fd438e5a4b
Pete K <krautscheid@mitre.org>
2020-02-13 16:14:41 -0500

    Adjust security redirects, tweak  demo data, add comments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,8 @@ class ApplicationController < ActionController::Base
   before_action :new_users_must_change_password
   def new_users_must_change_password
     if current_user && current_user.force_password_change
-      # Redirect unless we're already at the change password page
-      unless request.url == edit_user_registration_url || request.url == user_registration_url
+      # Redirect unless we're already at the change password page or logging out
+      unless request.url == edit_user_registration_url || request.url == user_registration_url || request.url == destroy_user_session_url
         redirect_to edit_user_registration_url
       end
     end
