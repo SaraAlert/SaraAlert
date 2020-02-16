e7b84fe202c1aff218407f0d7d80f9a789987840
Adam M. Holmes <aholmes@mitre.org>
2020-02-16 10:26:55 -0500

    element closest matches polyfills

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,14 +1,35 @@
+// Browser Compatibility
+
 require('es6-shim');
 require('core-js');
 
+// https://developer.mozilla.org/en-US/docs/Web/API/Element/closest
+if (!Element.prototype.matches) {
+  Element.prototype.matches = Element.prototype.msMatchesSelector ||
+                              Element.prototype.webkitMatchesSelector;
+}
+if (!Element.prototype.closest) {
+  Element.prototype.closest = function(s) {
+    var el = this;
+    do {
+      if (el.matches(s)) return el;
+      el = el.parentElement || el.parentNode;
+    } while (el !== null && el.nodeType === 1);
+    return null;
+  };
+}
+
+// Rails
 require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
 
+// React
 var componentRequireContext = require.context("components", true);
 var ReactRailsUJS = require("react_ujs");
 ReactRailsUJS.useContext(componentRequireContext);
 
+// Styling
 import 'bootstrap'
 import './stylesheets/application.scss'
 import "@fortawesome/fontawesome-free/js/all";
