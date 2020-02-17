c8ee65d3bb6969eeecc165f45cf4ce5b6b251246
Adam M. Holmes <aholmes@mitre.org>
2020-02-17 14:56:32 -0500

    Renamed analytics file

---
+++ app/javascript/components/dashboard/Analytics.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import { Row, Col } from 'react-bootstrap';
+import SystemStatistics from './widgets/SystemStatistics';
+import SubjectStatus from './widgets/SubjectStatus';
+import ReportingSummary from './widgets/ReportingSummary';
+import MonitoringDistributionDay from './widgets/MonitoringDistributionDay';
+import AssessmentsDay from './widgets/AssessmentsDay';
+import { PropTypes } from 'prop-types';
+import MapChart from './widgets/MapChart';
+
+class Analytics extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <div className="mx-2 pb-4">
+          <Row>
+            <Col md="12">
+              <Row>
+                <Col md="24">
+                  <SystemStatistics stats={this.props.stats} />
+                </Col>
+              </Row>
+              <Row className="mt-4">
+                <Col md="24">
+                  <MapChart stats={this.props.stats} />
+                </Col>
+              </Row>
+              <Row className="mt-4">
+                <Col md="24">
+                  <SubjectStatus stats={this.props.stats} />
+                </Col>
+              </Row>
+            </Col>
+            <Col md="12">
+              <Row>
+                <Col md="24">
+                  <ReportingSummary stats={this.props.stats} />
+                </Col>
+              </Row>
+              <Row className="mt-4">
+                <Col md="24">
+                  <MonitoringDistributionDay stats={this.props.stats} />
+                </Col>
+              </Row>
+              <Row className="mt-4">
+                <Col md="24">
+                  <AssessmentsDay stats={this.props.stats} />
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+        </div>
+        <div className="pb-2"></div>
+      </React.Fragment>
+    );
+  }
+}
+
+Analytics.propTypes = {
+  stats: PropTypes.object,
+};
+
+export default Analytics;
