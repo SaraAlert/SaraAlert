4a1c3d5ab764d79909ee188c3c38e50d1be93f43
Adam M. Holmes <aholmes@mitre.org>
2020-02-21 11:37:15 -0500

    Removed future date restriction for arrival

--- app/javascript/components/enrollment/steps/Arrival.js
+++ app/javascript/components/enrollment/steps/Arrival.js
@@ -223,7 +223,6 @@ const schema = yup.object().shape({
     .nullable(),
   date_of_arrival: yup
     .date('Date must correspond to the "mm/dd/yyyy" format.')
-    .max(new Date(), 'Date can not be in the future.')
     .when('date_of_departure', dod => {
       if (dod && dod instanceof Date) {
         return yup.date().min(dod, 'Date of Arrival must occur after the Date of Departure.');
