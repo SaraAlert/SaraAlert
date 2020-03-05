45d1763d03294b248f3d0e11a0edcdfd674c422c
Matt Mayer <mmayer@mitre.org>
2020-03-05 12:42:55 -0500

    Add validation to symptom

--- app/models/symptom.rb
+++ app/models/symptom.rb
@@ -1,4 +1,12 @@
 class Symptom < ApplicationRecord
+    columns.each do |column|
+      case column.type
+      when :text
+        validates column.name.to_sym, length: { maximum: 2000 }
+      when :string
+        validates column.name.to_sym, length: { maximum: 200 }
+      end
+    end
     def as_json(options = {})
         super(options).merge({
           field_type: type
