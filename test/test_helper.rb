5bf68791bdb7331404133e3f6aa1e2228cdfe61e
Adam M. Holmes <aholmes@mitre.org>
2020-03-11 11:37:44 -0400

    Code coverage badge

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require 'simplecov'
-SimpleCov.start
+SimpleCov.start 'rails'
 if ENV['CI'] == 'true'
   require 'codecov'
   SimpleCov.formatter = SimpleCov::Formatter::Codecov
