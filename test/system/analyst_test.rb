068f6c7ecd45bffed78d2cce6494f3e98880d301
Matt Mayer <mmayer@mitre.org>
2020-03-15 15:50:36 -0400

    Add an analytic onto jurisdictions for analyst_test

--- test/system/analyst_test.rb
+++ test/system/analyst_test.rb
@@ -7,6 +7,24 @@ require_relative 'lib/system_test_utils'
 class AnalystTest < ApplicationSystemTestCase
   @@system_test_utils = SystemTestUtils.new(nil)
 
+  def setup
+    Jurisdiction.find_each do |jur|
+      empty_analytic = Analytic.create(
+        monitorees_count: 0,
+        symptomatic_monitorees_count: 0,
+        asymptomatic_monitorees_count: 0,
+        confirmed_cases_count: 0,
+        closed_cases_count: 0,
+        open_cases_count: 0,
+        total_reports_count: 0,
+        non_reporting_monitorees_count: 0,
+        monitoree_state_map: {},
+        symptomatic_state_map: {}
+      )
+      jur.analytics.push(empty_analytic)
+    end
+  end
+
   test 'analyst viewing analytics' do
     login_and_view_analytics('analyst_all', true)
   end
