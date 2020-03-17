e4c707f70f7d50b5e27c4dfd44203b021fcefc0b
Matt Mayer <mmayer@mitre.org>
2020-03-17 14:16:52 -0400

    Add assessment active jobs

---
+++ app/jobs/consume_assessments_job.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+require 'redis'
+
+# ConsumeAssessmentsJob: Pulls assessments created in the split instance and saves them
+class ConsumeAssessmentsJob < ApplicationJob
+    queue_as :default
+  
+    def perform
+        connection = Redis.new
+        connection.subscribe 'reports' do |on|
+          on.message do |channel, msg|
+            begin
+              message = JSON.parse(msg)
+              patient = Patient.find_by(submission_token: message['patient_submission_token'])
+              threshold_condition = ThresholdCondition.where(threshold_condition_hash: message['threshold_condition_hash']).first
+              reported_symptoms_array = message['reported_symptoms_array']
+              typed_reported_symptoms = Condition.build_symptoms(reported_symptoms_array)
+              reported_condition = ReportedCondition.new(symptoms: typed_reported_symptoms, threshold_condition_hash: message['threshold_condition_hash'])
+              assessment = Assessment.new(reported_condition: reported_condition)
+              assessment.symptomatic = assessment.symptomatic?
+              assessment.patient = patient
+              assessment.who_reported = 'Monitoree'
+              assessment.save!
+            rescue
+            end
+          end
+        end     
+    end
+  end
+  
