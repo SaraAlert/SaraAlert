e3154ee2596210c16cadbfffd771fbfa94dd892e
Matt Mayer <mmayer@mitre.org>
2020-04-17 12:26:27 -0400

    Rubocop and authy forms spacing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,13 +7,16 @@ class ApplicationController < ActionController::Base
 
   def user_must_change_password
     return unless current_user&.force_password_change || (current_user && current_user&.authy_enabled && current_user&.authy_id.nil?)
+
     # First login (and first password change) must occur within three days
     current_user.lock_access! if current_user.password_changed_at < 3.days.ago
 
-    return if request.url == edit_user_registration_url || request.url == user_registration_url || request.url == destroy_user_session_url || request.url == user_enable_authy_url
+    if request.url == edit_user_registration_url || request.url == user_registration_url ||
+       request.url == destroy_user_session_url || request.url == user_enable_authy_url
+      return
+    end
 
     redirect_to edit_user_registration_url if current_user&.force_password_change
-    redirect_to user_enable_authy_url if (current_user&.authy_id.nil? && current_user&.authy_enabled)
-
+    redirect_to user_enable_authy_url if current_user&.authy_id.nil? && current_user&.authy_enabled
   end
 end
