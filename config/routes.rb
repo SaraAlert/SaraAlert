15060d24bfe732fa807229178834cb93c9f3a5ab
David Giangrave <dgiangrave@mitre.org>
2020-04-20 10:23:35 -0400

    Switched to route hash syntax & removed extra newlines

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,10 @@ Rails.application.routes.draw do
   end
 
   devise_for :users, only: [:sessions, :authy], :path_names => {
-    :verify_authy => "/verify-token",
-    :enable_authy => "/enable-two-factor",
-    :verify_authy_installation => "/verify-installation",
-    :authy_onetouch_status => "/onetouch-status"
+    verify_authy: "/verify-token",
+    enable_authy: "/enable-two-factor",
+    verify_authy_installation: "/verify-installation",
+    authy_onetouch_status: "/onetouch-status"
   }
 
   as :user do
