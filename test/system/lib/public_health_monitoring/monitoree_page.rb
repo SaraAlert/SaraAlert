35a039ee3ceb94bdf4e12a1fe6e91cba703bfdf7
Adam M. Holmes <aholmes@mitre.org>
2020-04-20 21:50:34 -0400

    Adjusting system test to use non hidden pause notifications

--- test/system/lib/public_health_monitoring/monitoree_page.rb
+++ test/system/lib/public_health_monitoring/monitoree_page.rb
@@ -8,16 +8,16 @@ require_relative '../system_test_utils'
 class PublicHealthMonitoringMonitoreePage < ApplicationSystemTestCase
   @@public_health_monitoring_monitoree_page_verifier = PublicHealthMonitoringMonitoreePageVerifier.new(nil)
   @@system_test_utils = SystemTestUtils.new(nil)
-  
+
   def view_patients_details_and_reports(jurisdiction_id)
     monitorees = Jurisdiction.find(jurisdiction_id).all_patients
     click_on 'All Monitorees'
-    monitorees.where(isolation: false).each { |patient|
+    monitorees.where(isolation: false).where(monitoring: true).each { |patient|
       @@public_health_monitoring_monitoree_page_verifier.verify_patient_details_and_reports(patient, 'exposure')
     }
     @@system_test_utils.go_to_workflow('isolation')
     click_on 'All Cases'
-    monitorees.where(isolation: true).each { |patient|
+    monitorees.where(isolation: true).where(monitoring: true).each { |patient|
       @@public_health_monitoring_monitoree_page_verifier.verify_patient_details_and_reports(patient, 'isolation')
     }
   end
