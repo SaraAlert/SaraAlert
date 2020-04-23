a5665a13a6daa79c6aaa6df94a898b38442bdbf9
Matt Mayer <mmayer@mitre.org>
2020-04-23 14:57:39 -0400

    Add views for excel exports

---
+++ app/javascript/components/subject/DownloadMonitoreeExcel.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+import { PropTypes } from 'prop-types';
+import axios from 'axios';
+import FileDownload from 'js-file-download';
+import base64StringToBlob from 'base64toblob';
+import moment from 'moment';
+
+class DownloadMonitoreeExcel extends React.Component {
+  constructor(props) {
+    super(props);
+    this.downloadExcel = this.downloadExcel.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  toggleDownloadExcelModal() {
+    let current = this.state.showDownloadExcelModal;
+    this.setState({
+      showDownloadExcelModal: !current,
+    });
+  }
+
+  handleChange(event) {
+    this.setState({ [event.target.id]: event.target.value });
+  }
+
+  downloadExcel() {
+    axios.defaults.headers.common['X-CSRF-Token'] = this.props.authenticity_token;
+    axios
+      .get(window.BASE_PATH + '/export/' + this.props.patient.id + '/full_history_single_monitoree', {})
+      .then(response => {
+        var fileDate = moment().format();
+        FileDownload(base64StringToBlob(response.data, 'application/xlsx'), 'Monitoree-' + this.props.patient.id + '-Sara-Alert-' + fileDate + '.xlsx');
+        location.href = window.BASE_PATH + '/patients/' + this.props.patient.id;
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <Button className="mx-2 mt-1 mb-3" onClick={this.downloadExcel}>
+          <i className="fas fa-download"></i> Download Excel Export
+        </Button>
+      </React.Fragment>
+    );
+  }
+}
+
+DownloadMonitoreeExcel.propTypes = {
+  patient: PropTypes.object,
+  authenticity_token: PropTypes.string,
+};
+
+export default DownloadMonitoreeExcel;
