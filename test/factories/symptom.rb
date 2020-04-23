3ffef12eae7750d918f8b2aa989456e78f780238
Kyle Fagan <kfagan@mitre.org>
2020-04-23 17:23:25 -0400

    Add Symptom type validation for BoolSymptom, FloatSymptom, and IntegerSymptom. Add additional create tests for Symptom model and rework existing create tests to use factories. Add tests for fever scope and Symptom#as_json.

--- test/factories/symptom.rb
+++ test/factories/symptom.rb
@@ -2,5 +2,19 @@
 
 FactoryBot.define do
   factory :symptom do
+    type { Symptom.valid_types.select }
+
+    after(:build) do |symptom|
+      case symptom.type
+      when 'IntegerSymptom'
+        symptom.int_value = symptom.int_value || Faker::Number.between(from: 1, to: 2_147_483_648)
+      when 'BoolSymptom'
+        if symptom.bool_value.nil?
+          symptom.bool_value = Faker::Boolean.boolean
+        end
+      when 'FloatSymptom'
+        symptom.float_value = symptom.float_value || Faker::Number.decimal
+      end
+    end
   end
 end
