0384be75d451b0f9cb628258b172b3060f99a9e8
David Giangrave <dgiangrave@mitre.org>
2020-04-24 09:46:00 -0400

    Fixed rubocop offenses

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -12,9 +12,9 @@ class UserMailer < ApplicationMailer
     mail(to: user.email, subject: 'Welcome to the Sara Alert system')
   end
 
-  def purge_notification()
+  def purge_notification
     receipients = User.with_any_role(:public_health, :public_health_enroller)
-    @expiration_date = Chronic.parse(ADMIN_OPTIONS['weekly_purge_date']).strftime("%A %B %d, at %l:%M %p %Z")
+    @expiration_date = Chronic.parse(ADMIN_OPTIONS['weekly_purge_date']).strftime('%A %B %d, at %l:%M %p %Z')
 
     receipients.each do |user|
       @user = user
@@ -23,5 +23,4 @@ class UserMailer < ApplicationMailer
       mail(to: user.email, subject: 'Sara Alert User Records Expiring Soon')
     end
   end
-
 end
