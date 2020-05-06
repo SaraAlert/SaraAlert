5259a7d0aa71c72235966d498fbf66399340b0f9
Kyle Fagan <kfagan@mitre.org>
2020-05-06 10:34:09 -0400

    Add negative testing for condition model tets

--- app/models/condition.rb
+++ app/models/condition.rb
@@ -5,11 +5,16 @@ class Condition < ApplicationRecord
   has_many :symptoms
 
   def self.build_symptoms(symptoms_array)
+    raise TypeError, "no conversion of #{symptoms_array.class} to Symptoms Array" unless symptoms_array.is_a?(Array)
+
     typed_symptoms = []
 
     symptoms_array.each do |symp|
       symptom = Symptom.new(symp)
       typed_symptoms.push(symptom)
+    rescue ActiveRecord::ActiveRecordError => e
+      Rails.logger.error("Attempting to create Symptom with `#{symp}` caused the following: #{e}")
+      next
     end
     typed_symptoms
   end
