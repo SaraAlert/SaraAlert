f29660df193777fe042c336cd813a222b7160276
Kyle Fagan <kfagan@mitre.org>
2020-05-12 16:39:58 -0400

    Fix bug in symptom factory where generic create(:symptom) were not creating objects with the correct symptom type

--- test/factories/symptom.rb
+++ test/factories/symptom.rb
@@ -3,12 +3,17 @@
 FactoryBot.define do
   factory :symptom do
     type { Symptom.valid_types.sample }
+    name { Faker::Alphanumeric.alphanumeric(number: 10) }
+    label { Faker::Alphanumeric.alphanumeric(number: 10) }
+    notes { Faker::Alphanumeric.alphanumeric(number: 10) }
+
+    initialize_with { type.constantize.new }
 
     after(:build) do |symptom|
       if symptom.type == 'IntegerSymptom' && symptom.int_value.nil?
         symptom.int_value = Faker::Number.between(from: 0, to: 2_147_483_648)
       elsif symptom.type == 'BoolSymptom' && symptom.bool_value.nil?
-        symptom.bool_value = Faker::Boolean.boolean
+        symptom.bool_value = false
       elsif symptom.type == 'FloatSymptom' && symptom.float_value.nil?
         symptom.float_value = Faker::Number.decimal
       end
