5150b10611d17c2a52a0c7210ceb22db24122050
Adam M. Holmes <aholmes@mitre.org>
2020-05-13 13:12:42 -0400

    Added newlines after return guard per rubocop

--- app/mailers/patient_mailer.rb
+++ app/mailers/patient_mailer.rb
@@ -6,6 +6,7 @@ class PatientMailer < ApplicationMailer
 
   def enrollment_email(patient)
     return if patient&.email&.blank?
+
     # Gather patients and jurisdictions
     @patients = ([patient] + patient.dependents).uniq.collect do |p|
       { patient: p, jurisdiction_unique_id: Jurisdiction.find_by_id(p.jurisdiction_id).unique_identifier }
@@ -17,6 +18,7 @@ class PatientMailer < ApplicationMailer
 
   def enrollment_sms_weblink(patient)
     return if patient&.primary_telephone&.blank?
+
     patient_name = "#{patient&.first_name&.first || ''}#{patient&.last_name&.first || ''}-#{patient&.calc_current_age || '0'}"
     contents = "This is the Sara Alert system please complete the report for #{patient_name} at "
     contents += new_patient_assessment_jurisdiction_report_url(patient.submission_token, patient.jurisdiction.unique_identifier).to_s
@@ -33,6 +35,7 @@ class PatientMailer < ApplicationMailer
 
   def enrollment_sms_text_based(patient)
     return if patient&.primary_telephone&.blank?
+
     patient_name = "#{patient&.first_name&.first || ''}#{patient&.last_name&.first || ''}-#{patient&.calc_current_age || '0'}"
     contents = "Welcome to the Sara Alert system, we will be sending your daily reports for #{patient_name} to this phone number."
     account_sid = ENV['TWILLIO_API_ACCOUNT']
@@ -49,6 +52,7 @@ class PatientMailer < ApplicationMailer
   # Right now the wording of this message is the same as for enrollment
   def assessment_sms_weblink(patient)
     add_fail_history(patient, 'primary phone number') && return if patient&.primary_telephone&.blank?
+
     add_success_history(patient)
     patient_name = "#{patient&.first_name&.first || ''}#{patient&.last_name&.first || ''}-#{patient&.calc_current_age || '0'}"
     contents = "This is the Sara Alert system please complete the daily report for #{patient_name} at "
@@ -66,6 +70,7 @@ class PatientMailer < ApplicationMailer
 
   def assessment_sms_reminder(patient)
     add_fail_history(patient, 'primary phone number') && return if patient&.primary_telephone&.blank?
+
     add_success_history(patient)
     contents = 'This is the Sara Alert system reminding you to please reply to our daily-report messages.'
     account_sid = ENV['TWILLIO_API_ACCOUNT']
@@ -81,6 +86,7 @@ class PatientMailer < ApplicationMailer
 
   def assessment_sms(patient)
     add_fail_history(patient, 'primary phone number') && return if patient&.primary_telephone&.blank?
+
     add_success_history(patient)
     patient_names = ([patient] + patient.dependents).uniq.collect do |p|
       "#{p&.first_name&.first || ''}#{p&.last_name&.first || ''}-#{p&.calc_current_age || '0'}"
@@ -114,6 +120,7 @@ class PatientMailer < ApplicationMailer
 
   def assessment_voice(patient)
     add_fail_history(patient, 'primary phone number') && return if patient&.primary_telephone&.blank?
+
     add_success_history(patient)
     patient_names = ([patient] + patient.dependents).uniq.collect do |p|
       "#{p&.first_name&.first || ''}, #{p&.last_name&.first || ''}, Age #{p&.calc_current_age || '0'},"
@@ -147,6 +154,7 @@ class PatientMailer < ApplicationMailer
 
   def assessment_email(patient)
     add_fail_history(patient, 'email') && return if patient&.email&.blank?
+
     add_success_history(patient)
     # Gather patients and jurisdictions
     @patients = ([patient] + patient.dependents).uniq.collect do |p|
@@ -159,6 +167,7 @@ class PatientMailer < ApplicationMailer
 
   def closed_email(patient)
     return if patient&.email&.blank?
+
     @patient = patient
     mail(to: patient.email, subject: 'Sara Alert Reporting Complete') do |format|
       format.html { render layout: 'main_mailer' }
@@ -169,6 +178,7 @@ class PatientMailer < ApplicationMailer
 
   def add_success_history(patient)
     return if patient.nil?
+
     unless patient&.preferred_contact_method&.nil?
       history = History.new
       history.created_by = 'Sara Alert System'
@@ -183,6 +193,7 @@ class PatientMailer < ApplicationMailer
 
   def add_fail_history(patient, type)
     return if patient.nil?
+
     history = History.new
     history.created_by = 'Sara Alert System'
     comment = "Sara Alert could not send a report reminder to this monitoree via #{patient.preferred_contact_method}, because the monitoree #{type} was blank."
