4f593c6af3f0b80e40c86f82a83de489146b6231
David Giangrave <dgiangrave@mitre.org>
2020-05-20 10:17:20 -0400

    Seperate application filters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,11 @@
 # ApplicationController: base controller, handles password changes
 class ApplicationController < ActionController::Base
   before_action :user_must_change_password
+  before_action :ensure_authy_enabled
   protect_from_forgery prepend: true
 
   def user_must_change_password
-    return unless current_user&.force_password_change || (current_user && !current_user&.authy_enabled && current_user&.authy_enforced)
+    return unless current_user&.force_password_change
 
     # First login (and first password change) must occur within three days
     current_user.lock_access! if current_user.password_changed_at < 3.days.ago && current_user&.force_password_change
@@ -17,13 +18,12 @@ class ApplicationController < ActionController::Base
       redirect_to edit_user_registration_url
       return
     end
+  end
 
-    if !current_user&.authy_enforced || request.url == user_enable_authy_url || request.url == user_verify_authy_url ||
-       request.url == user_verify_authy_installation_url || request.url == user_request_sms_url || request.url == user_request_phone_call_url
-      return
+  def ensure_authy_enabled
+    return if params[:controller] == "devise_authy" || params[:controller] == "users/registrations" || (params[:controller] == "devise/sessions" && params[:action] == 'destroy')
+    if current_user and !current_user.authy_enabled? and current_user.authy_enforced
+      redirect_to user_enable_authy_url
     end
-
-    redirect_to user_enable_authy_url unless current_user&.authy_enabled || !current_user&.authy_id.nil?
-    redirect_to user_verify_authy_installation_url unless current_user&.authy_enabled || current_user&.authy_id.nil?
   end
 end
--- app/controllers/devise_authy_controller.rb
+++ app/controllers/devise_authy_controller.rb
@@ -1,9 +1,8 @@
 class DeviseAuthyController < Devise::DeviseAuthyController
-    before_action :check_enforcement
+    before_action :verify_enforcement
 
-    def check_enforcement
+    def verify_enforcement
       return unless current_user
-
       redirect_to root_url unless current_user&.authy_enforced
     end
   end
