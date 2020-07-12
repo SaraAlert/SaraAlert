d52553f777a3295b93eefaae845d41708312349c
Adam M. Holmes <aholmes@mitre.org>
2020-07-12 11:59:11 -0400

    Don't purge if HOH with active dependents

--- app/jobs/purge_job.rb
+++ app/jobs/purge_job.rb
@@ -6,6 +6,7 @@ class PurgeJob < ApplicationJob
 
   def perform(*_args)
     Patient.purge_eligible.find_each(batch_size: 5000) do |monitoree|
+      next if monitoree.dependents.where.not(id: monitoree.id).where(monitoring: true).count.positive?
       # Whitelist attributes to keep
       attributes = Patient.new.attributes.keys
       whitelist = %w[id created_at updated_at responder_id creator_id jurisdiction_id
