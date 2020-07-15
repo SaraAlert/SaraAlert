c3315331639a351a06eed04d1083e6715da40486
Kyle Fagan <kfagan@mitre.org>
2020-07-15 09:52:25 -0400

    Create history types constant

--- app/models/history.rb
+++ app/models/history.rb
@@ -5,6 +5,24 @@ require 'action_view/helpers'
 
 # History: history model
 class History < ApplicationRecord
+  HISTORY_TYPES = {
+    report_created: 'Report Created',
+    report_updated: 'Report Updated',
+    comment: 'Comment',
+    enrollment: 'Enrollment',
+    monitoring_change: 'Monitoring Change',
+    monitoree_data_downloaded: 'Monitoree Data Downloaded',
+    reports_reviewed: 'Reports Reviewed',
+    report_reviewed: 'Report Reviewed',
+    report_reminder: 'Report Reminder',
+    report_note: 'Report Note',
+    lab_result: 'Lab Result',
+    lab_result_edit: 'Lab Result Edit',
+    contact_attempt: 'Contact Attempt',
+    welcome_message_sent: 'Welcome Message Sent',
+    record_automatically_closed: 'Record Automatically Closed'
+  }.freeze
+
   columns.each do |column|
     case column.type
     when :text
@@ -14,21 +32,7 @@ class History < ApplicationRecord
     end
   end
 
-  validates :history_type, inclusion: { in: ['Report Created',
-                                             'Report Updated',
-                                             'Comment',
-                                             'Enrollment',
-                                             'Monitoring Change',
-                                             'Monitoree Data Downloaded',
-                                             'Reports Reviewed',
-                                             'Report Reviewed',
-                                             'Report Reminder',
-                                             'Report Note',
-                                             'Lab Result',
-                                             'Lab Result Edit',
-                                             'Contact Attempt',
-                                             'Welcome Message Sent',
-                                             'Record Automatically Closed'] }
+  validates :history_type, inclusion: { in: HISTORY_TYPES.values }
 
   belongs_to :patient
 
@@ -47,55 +51,55 @@ class History < ApplicationRecord
   }
 
   def self.report_created(patient: nil, created_by: 'Sara Alert System', comment: 'User created a new report.')
-    create_history(patient, created_by, 'Report Created', comment)
+    create_history(patient, created_by, HISTORY_TYPES[:report_created], comment)
   end
 
   def self.report_updated(patient: nil, created_by: 'Sara Alert System', comment: 'User updated existing report.')
-    create_history(patient, created_by, 'Report Updated', comment)
+    create_history(patient, created_by, HISTORY_TYPES[:report_updated], comment)
   end
 
   def self.enrollment(patient: nil, created_by: 'Sara Alert System', comment: 'User enrolled monitoree.')
-    create_history(patient, created_by, 'Enrollment', comment)
+    create_history(patient, created_by, HISTORY_TYPES[:enrollment], comment)
   end
 
   def self.monitoring_change(patient: nil, created_by: 'Sara Alert System', comment: 'User updated monitoree.')
-    create_history(patient, created_by, 'Monitoring Change', comment)
+    create_history(patient, created_by, HISTORY_TYPES[:monitoring_change], comment)
   end
 
   def self.monitoree_data_downloaded(patient: nil, created_by: 'Sara Alert System', comment: 'User downloaded monitoree\'s data in Excel Export.')
-    create_history(patient, created_by, 'Monitoree Data Downloaded', comment)
+    create_history(patient, created_by, HISTORY_TYPES[:monitoree_data_downloaded], comment)
   end
 
   def self.reports_reviewed(patient: nil, created_by: 'Sara Alert System', comment: 'User reviewed all reports.')
-    create_history(patient, created_by, 'Reports Reviewed', comment)
+    create_history(patient, created_by, HISTORY_TYPES[:reports_reviewed], comment)
   end
 
   def self.report_reviewed(patient: nil, created_by: 'Sara Alert System', comment: 'User reviewed a report.')
-    create_history(patient, created_by, 'Report Reviewed', comment)
+    create_history(patient, created_by, HISTORY_TYPES[:report_reviewed], comment)
   end
 
   def self.report_reminder(patient: nil, created_by: 'Sara Alert System', comment: 'User sent a report reminder to the monitoree.')
-    create_history(patient, created_by, 'Report Reminder', comment) unless patient&.preferred_contact_method.nil?
+    create_history(patient, created_by, HISTORY_TYPES[:report_reminder], comment) unless patient&.preferred_contact_method.nil?
   end
 
   def self.lab_result(patient: nil, created_by: 'Sara Alert System', comment: 'User added a new lab result.')
-    create_history(patient, created_by, 'Lab Result', comment)
+    create_history(patient, created_by, HISTORY_TYPES[:lab_result], comment)
   end
 
   def self.lab_result_edit(patient: nil, created_by: 'Sara Alert System', comment: 'User edited a lab result.')
-    create_history(patient, created_by, 'Lab Result Edit', comment)
+    create_history(patient, created_by, HISTORY_TYPES[:lab_result_edit], comment)
   end
 
   def self.contact_attempt(patient: nil, created_by: 'Sara Alert System', comment: 'The system attempted to make contact with the monitoree.')
-    create_history(patient, created_by, 'Contact Attempt', comment)
+    create_history(patient, created_by, HISTORY_TYPES[:contact_attempt], comment)
   end
 
   def self.welcome_message_sent(patient: nil, created_by: 'Sara Alert System', comment: 'Initial Sara Alert welcome message was sent.')
-    create_history(patient, created_by, 'Welcome Message Sent', comment)
+    create_history(patient, created_by, HISTORY_TYPES[:welcome_message_sent], comment)
   end
 
   def self.record_automatically_closed(patient: nil, created_by: 'Sara Alert System', comment: 'Monitoree has completed monitoring.')
-    create_history(patient, created_by, 'Record Automatically Closed', comment)
+    create_history(patient, created_by, HISTORY_TYPES[:record_automatically_closed], comment)
   end
 
   # Information about this history
