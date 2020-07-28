8643f9deeecdfce7f2f32144f4e27e648c26338a
Kyle Fagan <kfagan@mitre.org>
2020-07-28 18:09:00 -0400

    Update factories to have an actual symptom structure within a jurisdiction

--- test/factories/integer_symptom.rb
+++ test/factories/integer_symptom.rb
@@ -3,6 +3,7 @@
 FactoryBot.define do
   factory :integer_symptom, parent: :symptom, class: 'IntegerSymptom' do
     type { 'IntegerSymptom' }
-    int_value { Faker::Number.between(from: 0, to: 2_147_483_648) }
+    # Max Integer for database column - ActiveRecord errors on 648.
+    int_value { Faker::Number.between(from: 0, to: 2_147_483_647) }
   end
 end
