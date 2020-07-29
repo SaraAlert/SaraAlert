5ad22194c7e09a8afacc18d88745bdd566f9d4f5
Kyle Fagan <kfagan@mitre.org>
2020-07-29 17:17:06 -0400

    Add additional test for assessment sms weblink message contents to ensure that multiple messages are being sent for a head of household with dependents.

--- test/mailers/patient_mailer_test.rb
+++ test/mailers/patient_mailer_test.rb
@@ -174,6 +174,26 @@ class PatientMailerTest < ActionMailer::TestCase
     PatientMailer.assessment_sms_weblink(@patient).deliver_now
   end
 
+  test 'assessment sms weblink message contents with dependents' do
+    dependent = create(:patient)
+    dependent.update(responder_id: @patient.id, submission_token: SecureRandom.hex(20))
+
+    first_contents = "#{I18n.t('assessments.sms.weblink.intro1', locale: 'en')} -0 #{I18n.t('assessments.sms.weblink.intro2', locale: 'en')}"
+    second_contents = new_patient_assessment_jurisdiction_report_lang_url(dependent.submission_token, 'en', dependent.jurisdiction.unique_identifier[0, 32])
+
+    # Cannot do the same expectation as previous tests because the expectation that any instance gets called with create is taken up by the first loop of sending
+    # messages. So instead we count the amount of times create was called. Cannot do this with typical rspec methods because when you use any_instance_of the
+    # expectation for number of calls applies to EVERY instance, not just any single instance. Instead we calculate based on our mock method.
+    create_count = 0
+    allow_any_instance_of(::Twilio::REST::Api::V2010::AccountContext::MessageList).to(receive(:create) do
+      create_count += 1
+      true
+    end)
+
+    PatientMailer.assessment_sms_weblink(@patient).deliver_now
+    assert_equal create_count, 4
+  end
+
   test 'assessment sms reminder message contents' do
     contents = I18n.t('assessments.sms.prompt.reminder', locale: 'en')
 
