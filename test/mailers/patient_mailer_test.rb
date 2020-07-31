5d43eb7294686669831f423546eea943c883c033
Adam M. Holmes <aholmes@mitre.org>
2020-07-31 11:25:00 -0400

    Remove trailing whitespace

--- test/mailers/patient_mailer_test.rb
+++ test/mailers/patient_mailer_test.rb
@@ -180,7 +180,7 @@ class PatientMailerTest < ActionMailer::TestCase
 
     # Cannot do the same expectation as previous tests because the expectation that any instance gets called with create is taken up by the first loop of
     # sending messages. So instead we count the amount of times create was called. Cannot do this with typical rspec methods because when you use
-    # any_instance_of the expectation for number of calls applies to EVERY instance, not just any single instance. Instead we calculate 
+    # any_instance_of the expectation for number of calls applies to EVERY instance, not just any single instance. Instead we calculate
     # based on our mock method.
     create_count = 0
     allow_any_instance_of(::Twilio::REST::Api::V2010::AccountContext::MessageList).to(receive(:create) do
