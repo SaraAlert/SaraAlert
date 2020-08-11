235350c7d31348d7cc27f54f92beea24dd13010b
David Connolly <dpconnolly@mitre.org>
2020-08-11 14:03:17 -0400

    adds mock data that can be imported

--- app/javascript/tests/analytics/PublicHealthAnalytics.test.js
+++ app/javascript/tests/analytics/PublicHealthAnalytics.test.js
@@ -4,7 +4,15 @@ import { render, fireEvent, waitFor, screen } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 
 import PublicHealthAnalytics from '../../components/analytics/PublicHealthAnalytics.js'
+import { mockUser1, mockAnalyticsData } from '../mockData/mock'
 
 test('PublicHealthAnalytics properly renders', () => {
+  window.SVGPathElement = function () {};
+  // SVGPath exists in the browser, not node so we have to define it
+
+  render(<PublicHealthAnalytics current_user={mockUser1} stats={mockAnalyticsData}/>)
+
+  screen.debug(); // Can be thought of as a DOM-dump. Essentially `console.log()` for jest
+
 
 })
---
+++ app/javascript/tests/mockData/mock.js
@@ -0,0 +1,24 @@
+// module "my-module.js"
+
+import mockAnalyticsData from './mockAnalytics'
+
+const mockUser1 = {
+  api_enabled: true,
+  authy_enabled: false,
+  authy_enforced: false,
+  authy_id: null,
+  created_at: "2020-07-15T15:15:15.000Z",
+  email: "mockUser1@example.com",
+  force_password_change: false,
+  id: 16,
+  jurisdiction_id: 2,
+  jurisdiction_path: (2) ["USA", "State 1"],
+  last_sign_in_with_authy: null,
+  password_changed_at: "2020-07-15T15:15:15.000Z",
+  updated_at: "2020-07-15T15:15:15.000Z"
+}
+
+export {
+  mockUser1,
+  mockAnalyticsData
+};
