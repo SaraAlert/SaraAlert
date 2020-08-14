e270cde4f81488d5682488b0f6d8580715f586b7
Moses, Kylie T <ktmoses@mitre.org>
2020-08-14 18:08:32 -0400

    Updating sidekiq initialize to set redis timeout on all environments

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,13 +2,12 @@
 # According to sidekiq docs 25 retries will take 3 weeks to complete
 Sidekiq.options[:max_retries] = 5
 
-if Rails.env.production?
-  Sidekiq.configure_server do |config|
-    # Set the network timeout to be 5 seconds instead of the default 1 to help with
-    # cloud network latency and longer batch commands
-    config.redis = {url: ENV['REDIS_URL'], network_timeout: 5 }
-  end
-  Sidekiq.configure_client do |config|
-   config.redis = {url: ENV['REDIS_URL'], network_timeout: 5 }
-  end
+Sidekiq.configure_server do |config|
+  # Set the network timeout to be 5 seconds instead of the default 1 to help with
+  # cloud network latency and longer batch commands
+  config.redis = {url: ENV['REDIS_URL'], network_timeout: 5 }
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = {url: ENV['REDIS_URL'], network_timeout: 5 }
 end
