2523a1ac3fc0fec9fa5a65f623bd82799e3b6447
Adam M. Holmes <aholmes@mitre.org>
2020-08-20 13:02:18 -0400

    Slim down the HTML used in the assessment job emails

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -5,9 +5,9 @@ Sidekiq.options[:max_retries] = 5
 Sidekiq.configure_server do |config|
   # Set the network timeout to be 5 seconds instead of the default 1 to help with
   # cloud network latency and longer batch commands
-  config.redis = {url: ENV['REDIS_URL'], network_timeout: 5 }
+  config.redis = { url: ENV['REDIS_URL'], network_timeout: 5 }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = {url: ENV['REDIS_URL'], network_timeout: 5 }
+  config.redis = { url: ENV['REDIS_URL'], network_timeout: 5 }
 end
