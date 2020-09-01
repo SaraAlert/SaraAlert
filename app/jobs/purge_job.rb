a089f78f7ca2e43865793f3e492f8750598c341b
Kyle Fagan <kfagan@mitre.org>
2020-09-01 14:08:55 -0400

    Reorder purge_job such that laboratories are deleted before monitoree is updated so that negative_lab_count is not reset from 0 to nil by Laboratory before_delete action. Update purge_job tests to include a big tests for all the attributes that should be nil after the job is run.

--- app/jobs/purge_job.rb
+++ app/jobs/purge_job.rb
@@ -13,29 +13,16 @@ class PurgeJob < ApplicationJob
     # Loop through and purge
     eligible.find_each do |monitoree|
       next if monitoree.active_dependents.count.positive?
-
-      attributes = Patient.new.attributes.keys
-      # Everything except these will be set to nil
-      attributes_to_keep = %w[id created_at updated_at responder_id creator_id jurisdiction_id
-                              monitoring monitoring_reason exposure_risk_assessment
-                              monitoring_plan isolation symptom_onset public_health_action age sex
-                              address_county symptom_onset contact_of_known_case
-                              member_of_a_common_exposure_cohort travel_to_affected_country_or_area
-                              laboratory_personnel was_in_health_care_facility_with_known_cases
-                              healthcare_personnel crew_on_passenger_or_cargo_flight white
-                              black_or_african_american american_indian_or_alaska_native asian
-                              native_hawaiian_or_other_pacific_islander ethnicity purged
-                              continuous_exposure]
-      attributes -= attributes_to_keep
-      # Set everything else to nil
-      mask = Hash[attributes.collect { |a| [a, nil] }].symbolize_keys
-      mask[:monitoring] = false
-      mask[:continuous_exposure] = false
-      mask[:purged] = true
-      monitoree.update!(mask)
       monitoree.histories.destroy_all
       monitoree.close_contacts.destroy_all
       monitoree.laboratories.destroy_all
+
+      attributes = Patient.new.attributes.keys
+      attributes -= PurgeJob.attributes_to_keep
+      # Set everything else to nil
+      mask = Hash[attributes.collect { |a| [a, nil] }].symbolize_keys
+      mask[:purged] = true
+      monitoree.update!(mask)
       purged << { id: monitoree.id }
     rescue StandardError => e
       not_purged << { id: monitoree.id, reason: e.message }
@@ -51,5 +38,21 @@ class PurgeJob < ApplicationJob
 
     # Send results
     UserMailer.purge_job_email(purged, not_purged, eligible_count).deliver_now
+  end
+
+  private
+
+  # Everything except these will be set to nil
+  def self.attributes_to_keep
+    %w[id created_at updated_at responder_id creator_id jurisdiction_id
+       monitoring monitoring_reason exposure_risk_assessment
+       monitoring_plan isolation symptom_onset public_health_action age sex
+       address_county symptom_onset contact_of_known_case
+       member_of_a_common_exposure_cohort travel_to_affected_country_or_area
+       laboratory_personnel was_in_health_care_facility_with_known_cases
+       healthcare_personnel crew_on_passenger_or_cargo_flight white
+       black_or_african_american american_indian_or_alaska_native asian
+       native_hawaiian_or_other_pacific_islander ethnicity purged
+       continuous_exposure]
   end
 end
--- test/jobs/purge_job_test.rb
+++ test/jobs/purge_job_test.rb
@@ -40,17 +40,6 @@ class PurgeJobTest < ActiveSupport::TestCase
     assert_includes(email_body, 'Test StandardError')
   end
 
-  test 'sets monitoring, purged, and continuous_exposure' do
-    patient = create(:patient, monitoring: false, purged: false)
-    patient.update(updated_at: (ADMIN_OPTIONS['purgeable_after'].minutes + 14.days).ago)
-
-    PurgeJob.perform_now
-    patient.reload
-    assert_not(patient.monitoring)
-    assert_not(patient.continuous_exposure)
-    assert(patient.purged)
-  end
-
   test 'does not purge heads of household with active dependents' do
     patient = create(:patient, monitoring: false, purged: false, address_line_1: Faker::Alphanumeric.alphanumeric(number: 10))
     dependent = create(:patient, monitoring: true, responder_id: patient.id)
@@ -90,5 +79,61 @@ class PurgeJobTest < ActiveSupport::TestCase
     assert(Assessment.count.zero?)
     assert(ReportedCondition.count.zero?)
     assert(Symptom.count.zero?)
+  end
+
+  test 'nils out everything but kept attributes' do
+    patient = create(:patient, monitoring: false, purged: false, continuous_exposure: false, submission_token: 'a',
+                     monitoring_reason: 'Other', exposure_risk_assessment: 'Low', monitoring_plan: 'None', public_health_action: 'None',
+                     last_assessment_reminder_sent: 1.month.ago, user_defined_id_statelocal: '1', user_defined_id_cdc: '1',
+                     user_defined_id_nndss: '1', first_name: 'a', last_name: 'a', date_of_birth: 1.year.ago, age: 1, sex: 'a',
+                     white: false, black_or_african_american: false, american_indian_or_alaska_native: false, asian: false,
+                     native_hawaiian_or_other_pacific_islander: false, ethnicity: 'a', primary_language: 'a',
+                     secondary_language: 'a', interpretation_required: false, nationality: 'a', address_line_1: 'a',
+                     foreign_address_line_1: 'a', address_city: 'a', address_state: 'a', address_line_2: 'a', address_zip: 'a',
+                     address_county: 'a', monitored_address_line_1: 'a', monitored_address_city: 'a', monitored_address_state: 'a',
+                     monitored_address_line_2: 'a', monitored_address_zip: 'a', monitored_address_county: 'a',
+                     foreign_address_city: 'a', foreign_address_country: 'a', foreign_address_line_2: 'a', foreign_address_zip: 'a',
+                     foreign_address_line_3: 'a', foreign_address_state: 'a', foreign_monitored_address_line_1: 'a',
+                     foreign_monitored_address_city: 'a', foreign_monitored_address_state: 'a', foreign_monitored_address_line_2: 'a',
+                     foreign_monitored_address_zip: 'a', foreign_monitored_address_county: 'a', primary_telephone: 'a',
+                     primary_telephone_type: 'a', secondary_telephone: 'a', secondary_telephone_type: 'a', email: 'a',
+                     preferred_contact_method: 'a', preferred_contact_time: 'a', port_of_origin: 'a', source_of_report: 'a',
+                     flight_or_vessel_number: 'a', flight_or_vessel_carrier: 'a', port_of_entry_into_usa: 'a',
+                     travel_related_notes: 'a', additional_planned_travel_type: 'a', additional_planned_travel_destination: 'a',
+                     additional_planned_travel_destination_state: 'a', additional_planned_travel_destination_country: 'a',
+                     additional_planned_travel_port_of_departure: 'a', date_of_departure: 1.month.ago,
+                     date_of_arrival: 1.month.ago, additional_planned_travel_start_date: 1.month.from_now,
+                     additional_planned_travel_end_date: 2.months.from_now, additional_planned_travel_related_notes: 'a',
+                     last_date_of_exposure: 1.month.ago, potential_exposure_location: 'a', potential_exposure_country: 'a',
+                     contact_of_known_case: false, contact_of_known_case_id: '1', member_of_a_common_exposure_cohort: false,
+                     member_of_a_common_exposure_cohort_type: 'a', travel_to_affected_country_or_area: false,
+                     laboratory_personnel: false, laboratory_personnel_facility_name: 'a', healthcare_personnel: false,
+                     healthcare_personnel_facility_name: 'a', crew_on_passenger_or_cargo_flight: false,
+                     was_in_health_care_facility_with_known_cases: false, was_in_health_care_facility_with_known_cases_facility_name: 'a',
+                     exposure_notes: 'a', isolation: false, closed_at: 1.month.ago, source_of_report_specify: 'a',
+                     pause_notifications: false, symptom_onset: 1.month.ago, case_status: 'a', assigned_user: 1,
+                     latest_assessment_at: 1.month.ago, latest_fever_or_fever_reducer_at: 1.month.ago,
+                     latest_positive_lab_at: 1.month.ago, negative_lab_count: 0, latest_transfer_at: 1.month.ago,
+                     latest_transfer_from: 1, gender_identity: 'a', sexual_orientation: 'a', user_defined_symptom_onset: false)
+    patient.update(close_contacts: [create(:close_contact, patient: patient)])
+    patient.update(laboratories: [create(:laboratory, patient: patient)])
+    patient.update(histories: [create(:history, patient: patient, history_type: 'Comment')])
+    patient.update(updated_at: (ADMIN_OPTIONS['purgeable_after'].minutes + 14.days).ago)
+
+    PurgeJob.perform_now
+    patient.reload
+    patient_attributes = patient.attributes
+    assert_not(patient_attributes.delete('monitoring'))
+    assert_not(patient_attributes.delete('continuous_exposure'))
+    patient.attributes.each do |attribute|
+      if PurgeJob.send(:attributes_to_keep).include?(attribute[0])
+        assert(!attribute[1].nil?)
+      else
+        assert_nil(attribute[1])
+      end
+    end
+    assert_empty(patient.laboratories)
+    assert_empty(patient.close_contacts)
+    assert_empty(patient.histories)
   end
 end
