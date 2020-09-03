6d05e77d534e05c0e05c2e5fd45c0405def816e4
Kyle <Bialogs@users.noreply.github.com>
2020-09-03 10:16:38 -0400

    Update workflow_tab_cache_invalidator.rb
    Remove hanging puts statement

--- app/lib/workflow_tab_cache_invalidator.rb
+++ app/lib/workflow_tab_cache_invalidator.rb
@@ -3,7 +3,6 @@
 # Helper method that will invalidate the workflow tab counts
 module WorkflowTabCacheInvalidator
   def self.invalidate_tab_counts_cache(jurisdiction_id)
-    puts 'INVALIDATING CACHE'
     %w[exposure isolation].each do |workflow|
       %w[all symptomatic non_reporting asymptomatic pui closed transferred_in transferred_out requiring_review reporting].each do |tab|
         Rails.cache.delete("#{jurisdiction_id}-#{workflow}-#{tab}")
