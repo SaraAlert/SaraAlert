3660510d46f47a0d3b5fd6c3ad88ae4beb14eb13
Kyle Fagan <kfagan@mitre.org>
2020-09-03 20:56:25 -0400

    Wip: moving inline script to a pack

---
+++ app/javascript/packs/patients_show_datatable.js
@@ -0,0 +1,39 @@
+// $(document).ready(function() {
+//   $('.assessment_table').DataTable({
+//     "search": false,
+//     "info": false,
+//     "lengthMenu": [10, 15, 25, 50],
+//     "pageLength": 15,
+//     "columnDefs": [{
+//       "orderable": false,
+//       "targets": -1
+//     }],
+//     "oLanguage": {
+//       "sSearch": "Search Reports:"
+//     },
+//     "order": [
+//       [0, "desc"]
+//     ],
+//     "dom": "<'row'<'col-sm-24 col-md-12'l><'col-sm-24 col-md-12'f>>" + "<'row'<'col-sm-24'tr>>" + "<'row'<'col-sm-24 col-md-10'i><'col-sm-24 col-md-14'p>>"
+//   });
+//   $('.lab_table').DataTable({
+//     "search": false,
+//     "info": false,
+//     "lengthMenu": [10, 15, 25, 50],
+//     "pageLength": 15,
+//     "oLanguage": {
+//       "sSearch": "Search Lab Results:"
+//     },
+//     "dom": "<'row'<'col-sm-24 col-md-12'l><'col-sm-24 col-md-12'f>>" + "<'row'<'col-sm-24'tr>>" + "<'row'<'col-sm-24 col-md-10'i><'col-sm-24 col-md-14'p>>"
+//   });
+//   $('.cc_table').DataTable({
+//     "search": false,
+//     "info": false,
+//     "lengthMenu": [10, 15, 25, 50],
+//     "pageLength": 15,
+//     "oLanguage": {
+//       "sSearch": "Search Close Contacts:"
+//     },
+//     "dom": "<'row'<'col-sm-24 col-md-12'l><'col-sm-24 col-md-12'f>>" + "<'row'<'col-sm-24'tr>>" + "<'row'<'col-sm-24 col-md-10'i><'col-sm-24 col-md-14'p>>"
+//   });
+// });
