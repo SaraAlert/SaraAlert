0dc3fed3d1d03ef595ae121bbc7792bbdac0d019
Kyle Fagan <kfagan@mitre.org>
2020-09-14 16:36:05 -0400

    Update calls to helper methods in new system tests

--- test/system/roles/public_health/public_health_dashboard_test.rb
+++ test/system/roles/public_health/public_health_dashboard_test.rb
@@ -12,23 +12,23 @@ class PublicHealthTest < ApplicationSystemTestCase
   @@system_test_utils = SystemTestUtils.new(nil)
 
   test 'verify patient information on dashboard' do
-    @@public_health_test_helper.verify_patients_on_dashboard('locals1c2_epi', false)
+    @@public_health_test_helper.verify_patients_on_dashboard('locals1c2_epi', verify_scope: false)
   end
 
   test 'verify jurisdiction scope filtering logic on dashboard for state epi' do
-    @@public_health_test_helper.verify_patients_on_dashboard('state1_epi_enroller', true)
+    @@public_health_test_helper.verify_patients_on_dashboard('state1_epi_enroller', verify_scope: true)
   end
 
   test 'verify jurisdiction scope filtering logic on dashboard for local epi' do
-    @@public_health_test_helper.verify_patients_on_dashboard('locals1c1_epi', true)
+    @@public_health_test_helper.verify_patients_on_dashboard('locals1c1_epi', verify_scope: true)
   end
 
   test 'verify assigned user filtering logic on dashboard for state epi' do
-    @@public_health_test_helper.verify_patients_on_dashboard('state2_epi', false)
+    @@public_health_test_helper.verify_patients_on_dashboard('state2_epi', verify_scope: false)
   end
 
   test 'verify assigned user filtering logic on dashboard for local epi' do
-    @@public_health_test_helper.verify_patients_on_dashboard('locals2c3_epi', false)
-    @@public_health_test_helper.verify_patients_on_dashboard('locals2c4_epi', false)
+    @@public_health_test_helper.verify_patients_on_dashboard('locals2c3_epi', verify_scope: false)
+    @@public_health_test_helper.verify_patients_on_dashboard('locals2c4_epi', verify_scope: false)
   end
 end
