40d3bfd368f72c52fb704a8c9cec70d5357cb707
Matt Mayer <mmayer@mitre.org>
2020-09-15 07:33:11 -0400

    Fix typo
    Rubocop adjustments
    
    Update mailers rake

--- app/mailers/twilio_sender.rb
+++ app/mailers/twilio_sender.rb
@@ -1,30 +1,33 @@
-class TwilioSender
+# frozen_string_literal: true
 
+# TwilioSender: Methods to interact with Twilio REST API
+class TwilioSender
   def self.send_sms(patient, contents)
     account_sid = ENV['TWILLIO_API_ACCOUNT']
     auth_token = ENV['TWILLIO_API_KEY']
     from = ENV['TWILLIO_SENDING_NUMBER']
     messaging_service_sid = ENV['TWILLIO_MESSAGING_SERVICE_SID']
+    begin
+      client = Twilio::REST::Client.new(account_sid, auth_token)
 
-    client = Twilio::REST::Client.new(account_sid, auth_token)
-
-    if messaging_service_sid.present?
-      client.messages.create(
-      to: Phonelib.parse(patient.primary_telephone, 'US').full_e164,
-      body: contents,
-      messaging_service_sid: messaging_service_sid
-      )
-    else
-      client.messages.create(
-      to: Phonelib.parse(patient.primary_telephone, 'US').full_e164,
-      body: contents,
-      from: from
-      )
-    end
+      if messaging_service_sid.present?
+        client.messages.create(
+          to: Phonelib.parse(patient.primary_telephone, 'US').full_e164,
+          body: contents,
+          messaging_service_sid: messaging_service_sid
+        )
+      else
+        client.messages.create(
+          to: Phonelib.parse(patient.primary_telephone, 'US').full_e164,
+          body: contents,
+          from: from
+        )
+      end
     rescue Twilio::REST::RestError => e
       Rails.logger.warn e.error_message
       patient.update(last_assessment_reminder_sent: DateTime.now)
       return false
+    end
     true
   end
 
@@ -33,27 +36,27 @@ class TwilioSender
     auth_token = ENV['TWILLIO_API_KEY']
     from = ENV['TWILLIO_SENDING_NUMBER']
     messaging_service_sid = ENV['TWILLIO_MESSAGING_SERVICE_SID']
+    begin
+      client = Twilio::REST::Client.new(account_sid, auth_token)
 
-    client = Twilio::REST::Client.new(account_sid, auth_token)
-
-    if messaging_service_sid.present? || params[:medium] != 'VOICE'
-      client.studio.v1.flows(ENV['TWILLIO_STUDIO_FLOW']).executions.create(
-      to: Phonelib.parse(patient.primary_telephone, 'US').full_e164,
-      parameters: params,
-      messaging_service_sid: messaging_service_sid
-      )
-    else
-      client.studio.v1.flows(ENV['TWILLIO_STUDIO_FLOW']).executions.create(
-      to: Phonelib.parse(patient.primary_telephone, 'US').full_e164,
-      parameters: params,
-      from: from
-      )
-    end
+      if messaging_service_sid.present? || params[:medium] != 'VOICE'
+        client.studio.v1.flows(ENV['TWILLIO_STUDIO_FLOW']).executions.create(
+          to: Phonelib.parse(patient.primary_telephone, 'US').full_e164,
+          parameters: params,
+          messaging_service_sid: messaging_service_sid
+        )
+      else
+        client.studio.v1.flows(ENV['TWILLIO_STUDIO_FLOW']).executions.create(
+          to: Phonelib.parse(patient.primary_telephone, 'US').full_e164,
+          parameters: params,
+          from: from
+        )
+      end
     rescue Twilio::REST::RestError => e
       Rails.logger.warn e.error_message
       patient.update(last_assessment_reminder_sent: DateTime.now)
       return false
+    end
     true
   end
-
 end
