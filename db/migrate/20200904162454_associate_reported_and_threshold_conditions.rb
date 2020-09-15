2e18a16df14db99f13fca56c0d991cb06daa94d5
Kyle Fagan <kfagan@mitre.org>
2020-09-15 17:37:49 -0400

    Update migration to use batched groups of reported conditions

--- db/migrate/20200904162454_associate_reported_and_threshold_conditions.rb
+++ db/migrate/20200904162454_associate_reported_and_threshold_conditions.rb
@@ -1,8 +1,12 @@
 class AssociateReportedAndThresholdConditions < ActiveRecord::Migration[6.0]
   def change
     threshold_conditions = ThresholdCondition.all.map {|tc| [tc.threshold_condition_hash, tc.id]}.to_h
-    ReportedCondition.all.each do |rc|
-      rc.update!(threshold_condition_id: threshold_conditions[rc.threshold_condition_hash])
+    # Batches of 1000
+    ReportedCondition.all.in_batches do |rc_batch|
+      # Individual reported condition
+      rc_batch.each do |rc|
+        rc.update!(threshold_condition_id: threshold_conditions[rc.threshold_condition_hash])
+      end
     end
   end
 end
