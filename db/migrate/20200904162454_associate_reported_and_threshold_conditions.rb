3513efd7f4daa3ba6005cd23bd6080dbf7465fa3
Kyle Fagan <kfagan@mitre.org>
2020-09-15 15:39:27 -0400

    Improve performance of migration

--- db/migrate/20200904162454_associate_reported_and_threshold_conditions.rb
+++ db/migrate/20200904162454_associate_reported_and_threshold_conditions.rb
@@ -1,8 +1,8 @@
 class AssociateReportedAndThresholdConditions < ActiveRecord::Migration[6.0]
   def change
+    threshold_conditions = ThresholdCondition.all.map {|tc| [tc.threshold_condition_hash, tc.id]}.to_h
     ReportedCondition.all.each do |rc|
-      tc = ThresholdCondition.find_by(threshold_condition_hash: rc.threshold_condition_hash)
-      rc.update!(threshold_condition_id: tc.id)
+      rc.update!(threshold_condition_id: threshold_conditions[rc.threshold_condition_hash])
     end
   end
 end
