21b2a091eb18453d1cae43deea6d611fa7c4350d
Kyle Fagan <kfagan@mitre.org>
2020-09-16 17:57:20 -0400

    Simplify nested if statement. Fix typo. Expand acronym

--- app/jobs/close_patients_job.rb
+++ app/jobs/close_patients_job.rb
@@ -17,11 +17,11 @@ class ClosePatientsJob < ApplicationJob
       patient[:monitoring] = false
       patient[:closed_at] = DateTime.now
 
-      # If the patient was enrolled already past their monitoring period based on their LDE, specify special reason for closure
+      # If the patient was enrolled already past their monitoring period based on their last date of exposure, specify special reason for closure
       if (patient.last_date_of_exposure.beginning_of_day + ADMIN_OPTIONS['monitoring_period_days'].days) < patient.created_at.beginning_of_day
         patient[:monitoring_reason] = 'Enrolled more than 14 days after last date of exposure (system)'
       elsif (patient.last_date_of_exposure.beginning_of_day + ADMIN_OPTIONS['monitoring_period_days'].days) == patient.created_at.beginning_of_day
-        # If the patient was enrolled oon their last day of monitoring based on their LDE, specify special reason for closure
+        # If the patient was enrolled on their last day of monitoring based on their last date of exposure, specify special reason for closure
         patient[:monitoring_reason] = 'Enrolled on last day of monitoring period (system)'
       else
         # Otherwise, normal reason for closure
@@ -29,9 +29,7 @@ class ClosePatientsJob < ApplicationJob
       end
 
       # Send closed email to patient if they are a reporter
-      if patient.save! && patient.email.present?
-        PatientMailer.closed_email(patient).deliver_later if patient.self_reporter_or_proxy?
-      end
+      PatientMailer.closed_email(patient).deliver_later if patient.save! && patient.email.present? && patient.self_reporter_or_proxy?
 
       # History item for automatically closing the record
       History.record_automatically_closed(patient: patient)
