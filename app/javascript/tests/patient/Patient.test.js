2b474937646528471dc644cf7117a3625c8966e8
Sarah G Ober <sgober@mitre.org>
2020-09-23 13:21:00 -0400

    additional detail section tests. updated patient mocks and general cleanup

--- app/javascript/tests/patient/Patient.test.js
+++ app/javascript/tests/patient/Patient.test.js
@@ -9,16 +9,24 @@ import { mockPatient1, mockPatient2, blankMockPatient } from '../mocks/mockPatie
 
 const goToMock = jest.fn();
 const authyToken = "Q1z4yZXLdN+tZod6dBSIlMbZ3yWAUFdY44U06QWffEP76nx1WGMHIz8rYxEUZsl9sspS3ePF2ZNmSue8wFpJGg==";
-const hohTableHeaders = [ 'Name', 'Workflow', 'Monitoring Status', 'Continuous Exposure?' ]
-
-function getWrapper(mockPatient, groupMembers, hideBody) {
-    return shallow(<Patient details={mockPatient} groupMembers={groupMembers} goto={goToMock} hideBody={hideBody}
-        jurisdictionPath="USA, State 1, County 2" authenticity_token={authyToken} />);
-}
+const hohTableHeaders = [ 'Name', 'Workflow', 'Monitoring Status', 'Continuous Exposure?' ];
+const identificationFields = [ 'DOB', 'Age', 'Language', 'State/Local ID', 'CDC ID', 'NNDSS ID', 'Birth Sex', 'Gender Identity', 'Sexual Orientation', 'Race', 'Ethnicity', 'Nationality' ];
+const contactFields = [ 'Phone', 'Preferred Contact Time', 'Type', 'Email', 'Preferred Reporting Method' ];
+const additionalTravelFields = [ 'Type', 'Place', 'Port Of Departure', 'End Date', 'Start Date' ];
+const potentialExposureFields = [ 
+    'CLOSE CONTACT WITH A KNOWN CASE',
+    'MEMBER OF A COMMON EXPOSURE COHORT',
+    'TRAVEL FROM AFFECTED COUNTRY OR AREA',
+    'WAS IN HEALTH CARE FACILITY WITH KNOWN CASES',
+    'LABORATORY PERSONNEL',
+    'HEALTHCARE PERSONNEL',
+    'CREW ON PASSENGER OR CARGO FLIGHT'
+]
 
 describe('Patient', () => {
     it('Properly renders all main components', () => {
-        const wrapper = getWrapper(mockPatient1, [ mockPatient2 ], true);
+        const wrapper = shallow(<Patient details={mockPatient1} groupMembers={[ mockPatient2 ]} goto={goToMock} hideBody={true}
+            jurisdictionPath="USA, State 1, County 2" authenticity_token={authyToken} />);
         expect(wrapper.find('#jurisdiction-path').text()).toEqual('Assigned Jurisdiction: USA, State 1, County 2');
         expect(wrapper.find('#assigned-user').text()).toEqual('Assigned User: 21');
         expect(wrapper.find('#identification').exists()).toBeTruthy();
@@ -29,8 +37,90 @@ describe('Patient', () => {
         expect(wrapper.find('#exposure-case-information').exists()).toBeTruthy();
     });
 
+    it('Properly renders identification section', () => {
+        const wrapper = shallow(<Patient details={mockPatient1} groupMembers={[ ]} hideBody={true}
+            jurisdictionPath="USA, State 1, County 2" authenticity_token={authyToken} />);
+        const section = wrapper.find('#identification');
+        expect(section.find(Row).first().text()).toEqual('Identification: Minnie M Mouse');
+        expect(section.find(Button).length).toEqual(0);
+        identificationFields.forEach(function(field, index) {
+            expect(section.find('.font-weight-normal').at(index).text()).toEqual(field+':');
+        });
+    });
+
+    it('Properly renders contact information section', () => {
+        const wrapper = shallow(<Patient details={mockPatient1} groupMembers={[ ]} hideBody={true}
+            jurisdictionPath="USA, State 1, County 2" authenticity_token={authyToken} />);
+        const section = wrapper.find('#contact-information');
+        expect(section.find(Row).first().text()).toEqual('Contact Information');
+        expect(section.find(Button).length).toEqual(0);
+        contactFields.forEach(function(field, index) {
+            expect(section.find('.font-weight-normal').at(index).text()).toEqual(field+':');
+        });
+    });
+
+    it('Properly renders address section', () => {
+        const wrapper = shallow(<Patient details={mockPatient1} groupMembers={[ ]} hideBody={true}
+            jurisdictionPath="USA, State 1, County 2" authenticity_token={authyToken} />);
+        const section = wrapper.find('#address');
+        expect(section.find(Row).first().text()).toEqual('Address');
+        expect(section.find(Button).length).toEqual(0);
+        expect(section.find('.font-weight-light').at(0).text()).toEqual('1 Hartford Drive');
+        expect(section.find('.font-weight-light').at(1).text()).toEqual('Springfield Connecticut Fairfield 00000-0000');
+    });
+
+    it('Properly renders arrival information section', () => {
+        const wrapper = shallow(<Patient details={mockPatient1} groupMembers={[ ]} hideBody={true}
+            jurisdictionPath="USA, State 1, County 2" authenticity_token={authyToken} />);
+        const section = wrapper.find('#arrival-information');
+        expect(section.find(Row).first().text()).toEqual('Arrival Information');
+        expect(section.find(Button).length).toEqual(0);
+        expect(section.find('h6').at(0).text()).toEqual('DEPARTED');
+        expect(section.find('h6').at(1).text()).toEqual('ARRIVAL');
+
+        // console.log(section.debug())
+        // UPDATE MOCK TO HAVE THESE FIELDS FILLED IN
+    });
+
+    it('Properly renders additional planned travel section', () => {
+        const wrapper = shallow(<Patient details={mockPatient1} groupMembers={[ ]} hideBody={true}
+            jurisdictionPath="USA, State 1, County 2" authenticity_token={authyToken} />);
+        const section = wrapper.find('#additional-planned-travel');
+        expect(section.find(Row).first().text()).toEqual('Additional Planned Travel');
+        expect(section.find(Button).length).toEqual(0);
+        additionalTravelFields.forEach(function(field, index) {
+            expect(section.find('.font-weight-normal').at(index).text()).toEqual(field+':');
+        });
+    });
+
+    it('Properly renders case information section', () => {
+        const wrapper = shallow(<Patient details={mockPatient1} groupMembers={[ ]} hideBody={true}
+            jurisdictionPath="USA, State 1, County 2" authenticity_token={authyToken} />);
+        const section = wrapper.find('#exposure-case-information');
+        expect(section.find(Row).first().text()).toEqual('Case Information');
+        expect(section.find(Button).length).toEqual(0);
+        expect(section.find('.font-weight-light').at(0).text()).toEqual('Symptom Onset: 09/27/2020');
+        expect(section.find('.font-weight-light').at(1).text()).toEqual('Case Status: Suspect');
+
+    });
+
+    it('Properly renders potential exposure information section', () => {
+        const wrapper = shallow(<Patient details={mockPatient2} groupMembers={[ ]} hideBody={true}
+            jurisdictionPath="USA, State 1, County 2" authenticity_token={authyToken} />);
+        const section = wrapper.find('#exposure-case-information');
+        expect(section.find(Row).first().text()).toEqual('Potential Exposure Information');
+        expect(section.find(Button).length).toEqual(0);
+        expect(section.find('h6').text()).toEqual('LAST EXPOSURE');
+        expect(section.find('.font-weight-light').at(0).text()).toEqual(' Mexico'); // fix me
+        expect(section.find('.font-weight-light').at(1).text()).toEqual('09/13/2020');
+        potentialExposureFields.forEach(function(field, index) {
+            expect(section.find('.text-danger').at(index).text().includes(field)).toBeTruthy();
+        });
+    });
+
     it('Properly renders HoH section', () => {
-        const wrapper = getWrapper(mockPatient1, [ mockPatient2, blankMockPatient ], true);
+        const wrapper = shallow(<Patient details={mockPatient1} groupMembers={[ mockPatient2, blankMockPatient ]} goto={goToMock} hideBody={true}
+            jurisdictionPath="USA, State 1, County 2" authenticity_token={authyToken} />);
         expect(wrapper.find('#head-of-household').exists()).toBeTruthy();
         expect(wrapper.find('#head-of-household').find(Row).at(1).text())
             .toEqual('This monitoree is responsible for handling the reporting of the following other monitorees:');
@@ -39,12 +129,13 @@ describe('Patient', () => {
         expect(wrapper.containsMatchingElement(<MoveToHousehold />)).toBeFalsy();
         hohTableHeaders.forEach(function(header, index) {
             expect(wrapper.find('thead th').at(index).text()).toEqual(header);
-        })
+        });
         expect(wrapper.find('tbody tr').length).toEqual(2);
     });
 
     it('Properly renders household member section', () => {
-        const wrapper = getWrapper(mockPatient2, [], true);
+        const wrapper = shallow(<Patient details={mockPatient2} groupMembers={[ ]} goto={goToMock} hideBody={true}
+            jurisdictionPath="USA, State 1, County 2" authenticity_token={authyToken} />);
         expect(wrapper.find('#household-member-not-hoh').exists()).toBeTruthy();
         expect(wrapper.find('#household-member-not-hoh').find(Row).first().text())
             .toEqual('The reporting responsibility for this monitoree is handled by another monitoree. Click here to view that monitoree.');
@@ -55,7 +146,8 @@ describe('Patient', () => {
     });
 
     it('Properly renders single member (not in household) section', () => {
-        const wrapper = getWrapper(mockPatient1, [], true);
+        const wrapper = shallow(<Patient details={mockPatient1} groupMembers={[ ]} goto={goToMock} hideBody={true}
+            jurisdictionPath="USA, State 1, County 2" authenticity_token={authyToken} />);
         expect(wrapper.find('#no-household').exists()).toBeTruthy();
         expect(wrapper.find('#no-household').find(Row).at(1).text()).toEqual('This monitoree is not a member of a household:');
         expect(wrapper.containsMatchingElement(<MoveToHousehold />)).toBeTruthy();
@@ -63,50 +155,29 @@ describe('Patient', () => {
         expect(wrapper.containsMatchingElement(<RemoveFromHousehold />)).toBeFalsy();
     });
 
-    // it('Properly renders identification section', () => {
-
-    // });
-
-    // it('Properly renders contact information section', () => {
-
-    // });
-
-    // it('Properly renders address section', () => {
-
-    // });
-
-    // it('Properly renders arrival information section', () => {
-    // arrival and departure
-    // });
-
-    // it('Properly renders additional planned travel section', () => {
-
-    // });
-
-    // it('Properly renders potential exposure/case information section', () => {
-    // isolation vs exposure
-    // });
-
     it('Properly renders no details message', () => {
-        const blankWrapper = getWrapper();
+        const blankWrapper = shallow(<Patient />);
         expect(blankWrapper.text()).toEqual('No monitoree details to show.');
     });
 
     it('Expands/collapses details with this.props.hideBody', () => {
-        const collapsedWrapper = getWrapper(mockPatient1, [ mockPatient2 ], true);
+        const collapsedWrapper = shallow(<Patient details={mockPatient1} groupMembers={[ mockPatient2 ]} goto={goToMock} hideBody={true}
+            jurisdictionPath="USA, State 1, County 2" authenticity_token={authyToken} />);
         expect(collapsedWrapper.find(Collapse).prop('in')).toBeFalsy();
 
-        const expandedWrapper = getWrapper(mockPatient1, [ mockPatient2 ], false);
+        const expandedWrapper = shallow(<Patient details={mockPatient1} groupMembers={[ mockPatient2 ]} goto={goToMock} hideBody={false}
+            jurisdictionPath="USA, State 1, County 2" authenticity_token={authyToken} />);
         expect(expandedWrapper.find(Collapse).prop('in')).toBeTruthy();
     });
 
     it('Calls props goto method when the edit buttons are clicked', () => {
-        const wrapper = getWrapper(mockPatient1, [ mockPatient2 ], false);
+        const wrapper = shallow(<Patient details={mockPatient1} groupMembers={[ mockPatient2 ]} goto={goToMock} hideBody={false}
+            jurisdictionPath="USA, State 1, County 2" authenticity_token={authyToken} />);
         expect(wrapper.find(Button).length).toEqual(6);
         expect(goToMock).toHaveBeenCalledTimes(0);
         wrapper.find(Button).forEach(function(btn, index) {
             btn.simulate('click');
             expect(goToMock).toHaveBeenCalledTimes(index+1);
-        })
+        });
     });
 });
