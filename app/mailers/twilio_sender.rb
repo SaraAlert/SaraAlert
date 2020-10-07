b7313ad961748f923e45619a40bd5b34ca6d1020
Matt Mayer <mmayer@mitre.org>
2020-10-07 14:41:48 -0400

    Rubocop fixes

--- app/mailers/twilio_sender.rb
+++ app/mailers/twilio_sender.rb
@@ -2,7 +2,7 @@
 
 # TwilioSender: Methods to interact with Twilio REST API
 class TwilioSender
-  @@client = Twilio::REST::Client.new(ENV['TWILLIO_API_ACCOUNT'], ENV['TWILLIO_API_KEY'])
+  @client = Twilio::REST::Client.new(ENV['TWILLIO_API_ACCOUNT'], ENV['TWILLIO_API_KEY'])
   def self.send_sms(patient, contents)
     from = ENV['TWILLIO_MESSAGING_SERVICE_SID'] || ENV['TWILLIO_SENDING_NUMBER']
     begin
@@ -38,15 +38,14 @@ class TwilioSender
   def self.get_responder_from_flow_execution(execution_id)
     begin
       execution = @@client.studio.v1
-      .flows(ENV['TWILLIO_STUDIO_FLOW'])
-      .executions(execution_id)
-      .fetch
+                          .flows(ENV['TWILLIO_STUDIO_FLOW'])
+                          .executions(execution_id)
+                          .fetch
     rescue Twilio::REST::RestError => e
       Rails.logger.warn e.error_message
       return
     end
     phone_number = execution.contact_channel_address
-    return Patient.responder_for_number(phone_number)
+    Patient.responder_for_number(phone_number)
   end
-
 end
