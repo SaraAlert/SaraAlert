eb4a05829943d3bc28c054be15d82ea2f64f5b30
Kyle Fagan <kfagan@mitre.org>
2020-10-08 15:53:02 -0400

    Remove specific concerns and replace with generic concern that takes options

---
+++ app/models/concerns/latest_date_validator.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+class LatestDateValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    return unless options.key?(:date)
+
+    raise TypeError, "#{options[:date]} does not implement required comparison methods" unless options[:date].acts_like?(:date)
+
+    record.errors.add(attribute, "cannot occur before #{options[:date]}") unless valid?(value, options[:date])
+  end
+
+  private
+
+  def  valid?(value, provided)
+    if value.nil?
+      true
+    elsif !value.acts_like?(:date)
+      false
+    else
+      value <= provided
+    end
+  end
+end
