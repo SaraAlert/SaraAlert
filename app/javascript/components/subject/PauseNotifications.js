0b2133f7aef49679a5ecbbe5b0b51ced76b56d68
Sarah G Ober <sgober@mitre.org>
2020-10-12 13:26:22 -0400

    structure for closed and dependent disable and tooltips. always show the pause/resume notification button

--- app/javascript/components/subject/PauseNotifications.js
+++ app/javascript/components/subject/PauseNotifications.js
@@ -12,6 +12,7 @@ class PauseNotifications extends React.Component {
     super(props);
     this.state = {
       loading: false,
+      disableAndDisplayTooltip: props.patient.id !== props.patient.responder_id || !props.patient.monitoring,
     };
   }
 
@@ -39,16 +40,39 @@ class PauseNotifications extends React.Component {
     }
   };
 
+  renderTooltip() {
+    // monitoree record is closed
+    if (!this.props.patient.monitoring) {
+      // pause and resume case
+      const text = 'closed tool tip';
+      return (
+        <ReactTooltip id={`notifications-tooltip`} multiline={true} place="bottom" type="dark" effect="solid" className="tooltip-container mt-3">
+          <span> {text} </span>
+        </ReactTooltip>
+      );
+
+      // monitoree is a HH dependent
+    } else if (this.props.patient.id !== this.props.patient.responder_id) {
+      // pause and resume case
+      const text = 'dependent tool tip';
+      return (
+        <ReactTooltip id={`notifications-tooltip`} multiline={true} place="bottom" type="dark" effect="solid" className="tooltip-container mt-3">
+          <span> {text} </span>
+        </ReactTooltip>
+      );
+    }
+  }
+
   render() {
     return (
       <React.Fragment>
         {!this.props.patient.pause_notifications && (
           <React.Fragment>
-            <span data-for={`${this.props.patient.id}-pause-notifications`} data-tip="">
+            <span data-for="notifications-tooltip" data-tip="">
               <Button
                 id="pause_notifications"
                 className="mr-2"
-                disabled={this.props.patient.id !== this.props.patient.responder_id || this.state.loading}
+                disabled={this.state.disableAndDisplayTooltip || this.state.loading}
                 onClick={() =>
                   this.handleSubmit(
                     "You are about to change this monitoree's notification status to paused. This means that the system will stop sending the monitoree symptom report requests until notifications are resumed by a user."
@@ -62,36 +86,31 @@ class PauseNotifications extends React.Component {
                 )}
               </Button>
             </span>
-            {this.props.patient.id !== this.props.patient.responder_id && (
-              <ReactTooltip
-                id={`${this.props.patient.id}-pause-notifications`}
-                multiline={true}
-                place="bottom"
-                type="dark"
-                effect="solid"
-                className="tooltip-container">
-                <span>INSERT TOOLTIP HERE</span>
-              </ReactTooltip>
-            )}
+            {this.state.disableAndDisplayTooltip && this.renderTooltip()}
           </React.Fragment>
         )}
         {this.props.patient.pause_notifications && (
-          <Button
-            id="pause_notifications"
-            className="mr-2"
-            disabled={this.state.loading}
-            onClick={() =>
-              this.handleSubmit(
-                "You are about to change this monitoree's notification status to resumed. This means that the system will start sending the monitoree symptom report requests unless notifications are paused by a user or the record is closed."
-              )
-            }>
-            <i className="fas fa-play"></i> Resume Notifications
-            {this.state.loading && (
-              <React.Fragment>
-                &nbsp;<span className="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
-              </React.Fragment>
-            )}
-          </Button>
+          <React.Fragment>
+            <span data-for="notifications-tooltip" data-tip="">
+              <Button
+                id="pause_notifications"
+                className="mr-2"
+                disabled={this.state.disableAndDisplayTooltip || this.state.loading}
+                onClick={() =>
+                  this.handleSubmit(
+                    "You are about to change this monitoree's notification status to resumed. This means that the system will start sending the monitoree symptom report requests unless notifications are paused by a user or the record is closed."
+                  )
+                }>
+                <i className="fas fa-play"></i> Resume Notifications
+                {this.state.loading && (
+                  <React.Fragment>
+                    &nbsp;<span className="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+                  </React.Fragment>
+                )}
+              </Button>
+            </span>
+            {this.state.disableAndDisplayTooltip && this.renderTooltip()}
+          </React.Fragment>
         )}
       </React.Fragment>
     );
