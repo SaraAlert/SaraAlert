88cbbcecaa7c461a39218816be9804beaf8dbcc7
Moses, Kylie T <ktmoses@mitre.org>
2020-10-15 15:44:44 -0400

    Cleaning up new header, adding super user permissions, adding new users to demo data.

--- app/lib/roles.rb
+++ app/lib/roles.rb
@@ -2,12 +2,14 @@
 
 # Contains valid Role attribute in the User model and methods to work with them
 module Roles
+  SUPER_USER = 'super_user'
   ADMIN = 'admin'
   ANALYST = 'analyst'
   ENROLLER = 'enroller'
   NONE = 'none'
   PUBLIC_HEALTH = 'public_health'
   PUBLIC_HEALTH_ENROLLER = 'public_health_enroller'
+  CONTACT_TRACER = 'contact_tracer'
 
   def self.all_role_values
     constants.map { |c| const_get(c) }
