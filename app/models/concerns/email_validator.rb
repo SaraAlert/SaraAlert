d3af61ab4c428d2345aa888f4eb4d56428cbd73b
Nick Freiter <nfreiter@mitre.org>
2020-10-18 21:40:16 -0400

    Add date validation to model

--- app/models/concerns/email_validator.rb
+++ app/models/concerns/email_validator.rb
@@ -4,7 +4,7 @@
 class EmailValidator < ActiveModel::EachValidator
   include ValidationHelper
   def validate_each(record, attribute, value)
-    return nil if value.blank?
+    return if value.blank?
     unless ValidEmail2::Address.new(value).valid?
       record.errors.add(attribute, "'#{value}' is not a valid Email Address for '#{VALIDATION[attribute][:label]}'")
     end
