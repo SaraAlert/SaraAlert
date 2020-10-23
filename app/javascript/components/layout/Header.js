c1201c425d4e5f837b640f6c9eee1a9520d9d78d
Moses, Kylie T <ktmoses@mitre.org>
2020-10-23 14:37:53 -0400

    Fixed active tab for isolation workflow, updated assigned jurisdiction tool tip to be dynamic based on role, added back demo warning banner, removed none role option from admin page, updated pause notifications tooltip text, aligned nav link text with nav logo.

--- app/javascript/components/layout/Header.js
+++ app/javascript/components/layout/Header.js
@@ -7,87 +7,107 @@ class Header extends React.Component {
     super(props);
   }
 
+  /**
+   * Finds the "activeKey" for the nav bar based on current path.
+   */
+  getActiveTabKey = () => {
+    // Active tab should still be the tab for the monitoring dashboards when viewing the isolation workflow
+    if (window.location.pathname === `${window.BASE_PATH}/public_health/isolation`) {
+      return `${window.BASE_PATH}/public_health`;
+    }
+    return window.location.pathname;
+  };
+
   render() {
     return (
-      <Navbar bg="primary" variant="dark" expand="lg" className="mb-3">
-        <Navbar.Brand href={this.props.report_mode ? '/' : this.props.root}>
-          Sara Alert<small className="nav-version ml-1">{this.props.version}</small>
-        </Navbar.Brand>
-        {this.props.current_user && (
-          <React.Fragment>
-            <Nav className="mr-auto" activeKey={window.location.pathname}>
-              {this.props.current_user?.can_see_enroller_dashboard_tab && (
-                <Nav.Link className="ml-3" href={`${window.BASE_PATH}/patients`}>
-                  <i className="fas fa-table fa-fw mr-2"></i>Enroller Dashboard
-                </Nav.Link>
-              )}
-              {this.props.current_user?.can_see_monitoring_dashboards_tab && (
-                <Nav.Link className="ml-3" href={`${window.BASE_PATH}/public_health`}>
-                  <i className="fas fa-table fa-fw mr-2"></i>Monitoring Dashboards
-                </Nav.Link>
-              )}
-              {this.props.current_user?.can_see_admin_panel_tab && (
-                <Nav.Link className="ml-3" href={`${window.BASE_PATH}/admin`}>
-                  <i className="fas fa-user-cog fa-fw mr-2"></i>Admin Panel
-                </Nav.Link>
-              )}
-              {this.props.current_user?.can_see_analytics_tab && (
-                <Nav.Link className="ml-3" href={`${window.BASE_PATH}/analytics`}>
-                  <i className="fas fa-chart-pie fa-fw mr-2"></i>Analytics
-                </Nav.Link>
-              )}
-            </Nav>
-            <Form inline className="ml-auto">
-              {!this.props.report_mode && (
-                <React.Fragment>
-                  <Navbar.Text className="text-white px-3">
-                    <i className="fas fa-user fa-fw mr-2"></i>
-                    {this.props.current_user?.email}
-                  </Navbar.Text>
-                  <a className="white-border-right"></a>
-                  <div className="dropdown">
-                    <Nav.Link className="text-white" id="helpMenuButton" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                      <i className="fas fa-question-circle fa-fw"></i>
-                    </Nav.Link>
-                    <div className="dropdown-menu dropdown-menu-right" aria-labelledby="helpMenuButton">
-                      <a className="dropdown-item" href="https://saraalert.org/public-health/guides/" target="_blank" rel="noreferrer">
-                        <i className="fas fa-book fa-fw"></i> User Guides
-                      </a>
-                      <a className="dropdown-item" href="https://virtualcommunities.naccho.org/saraalertforum/home" target="_blank" rel="noreferrer">
-                        <i className="fas fa-comments fa-fw"></i> User Forum
-                      </a>
-                      <a className="dropdown-item" href="https://saraalert.org/contact/" target="_blank" rel="noreferrer">
-                        <i className="fas fa-envelope-open-text fa-fw"></i> Contact Us
-                      </a>
-                    </div>
-                  </div>
-                  <a className="white-border-right"></a>
-                  {this.props.current_user?.is_usa_admin && (
-                    <React.Fragment>
-                      <Nav.Link className="night-nav-link text-white" href="/oauth/applications">
-                        <i className="fas fa-share-alt fa-fw mr-2"></i>API
-                      </Nav.Link>
-                      <a className="white-border-right"></a>
-                    </React.Fragment>
-                  )}
-                  <Nav.Link className="night-nav-link text-white" href={`${window.BASE_PATH}/users/sign_out`} data-method="DELETE">
-                    <i className="fas fa-sign-out-alt fa-fw mr-2"></i>Logout
+      <React.Fragment>
+        <Navbar bg="primary" variant="dark" expand="lg" className={this.props.show_demo_warning ? '' : 'mb-3'}>
+          <Navbar.Brand href={this.props.report_mode ? '/' : this.props.root}>
+            Sara Alert<small className="nav-version ml-1">{this.props.version}</small>
+          </Navbar.Brand>
+          {this.props.current_user && (
+            <React.Fragment>
+              <Nav className="pt-2 mr-auto" activeKey={this.getActiveTabKey()}>
+                {this.props.current_user?.can_see_enroller_dashboard_tab && (
+                  <Nav.Link className="ml-3" href={`${window.BASE_PATH}/patients`}>
+                    <i className="fas fa-table fa-fw mr-2"></i>Enroller Dashboard
                   </Nav.Link>
-                </React.Fragment>
-              )}
-            </Form>
-          </React.Fragment>
+                )}
+                {this.props.current_user?.can_see_monitoring_dashboards_tab && (
+                  <Nav.Link className="ml-3" href={`${window.BASE_PATH}/public_health`}>
+                    <i className="fas fa-table fa-fw mr-2"></i>Monitoring Dashboards
+                  </Nav.Link>
+                )}
+                {this.props.current_user?.can_see_admin_panel_tab && (
+                  <Nav.Link className="ml-3" href={`${window.BASE_PATH}/admin`}>
+                    <i className="fas fa-user-cog fa-fw mr-2"></i>Admin Panel
+                  </Nav.Link>
+                )}
+                {this.props.current_user?.can_see_analytics_tab && (
+                  <Nav.Link className="ml-3" href={`${window.BASE_PATH}/analytics`}>
+                    <i className="fas fa-chart-pie fa-fw mr-2"></i>Analytics
+                  </Nav.Link>
+                )}
+              </Nav>
+              <Form inline className="ml-auto">
+                {!this.props.report_mode && (
+                  <React.Fragment>
+                    <Navbar.Text className="text-white px-3">
+                      <i className="fas fa-user fa-fw mr-2"></i>
+                      {this.props.current_user?.email}
+                    </Navbar.Text>
+                    <a className="white-border-right"></a>
+                    <div className="dropdown">
+                      <Nav.Link className="text-white" id="helpMenuButton" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <i className="fas fa-question-circle fa-fw"></i>
+                      </Nav.Link>
+                      <div className="dropdown-menu dropdown-menu-right" aria-labelledby="helpMenuButton">
+                        <a className="dropdown-item" href="https://saraalert.org/public-health/guides/" target="_blank" rel="noreferrer">
+                          <i className="fas fa-book fa-fw"></i> User Guides
+                        </a>
+                        <a className="dropdown-item" href="https://virtualcommunities.naccho.org/saraalertforum/home" target="_blank" rel="noreferrer">
+                          <i className="fas fa-comments fa-fw"></i> User Forum
+                        </a>
+                        <a className="dropdown-item" href="https://saraalert.org/contact/" target="_blank" rel="noreferrer">
+                          <i className="fas fa-envelope-open-text fa-fw"></i> Contact Us
+                        </a>
+                      </div>
+                    </div>
+                    <a className="white-border-right"></a>
+                    {this.props.current_user?.is_usa_admin && (
+                      <React.Fragment>
+                        <Nav.Link className="night-nav-link text-white" href="/oauth/applications">
+                          <i className="fas fa-share-alt fa-fw mr-2"></i>API
+                        </Nav.Link>
+                        <a className="white-border-right"></a>
+                      </React.Fragment>
+                    )}
+                    <Nav.Link className="night-nav-link text-white" href={`${window.BASE_PATH}/users/sign_out`} data-method="DELETE">
+                      <i className="fas fa-sign-out-alt fa-fw mr-2"></i>Logout
+                    </Nav.Link>
+                  </React.Fragment>
+                )}
+              </Form>
+            </React.Fragment>
+          )}
+        </Navbar>
+        {this.props.show_demo_warning && (
+          <Navbar bg="warning" variant="dark" expand="lg" className="mb-3">
+            This system is for demonstration use only, please do not provide any personal identifying information other than business contact data that may be
+            required for testing.
+          </Navbar>
         )}
-      </Navbar>
+      </React.Fragment>
     );
   }
 }
 
 Header.propTypes = {
-  root: PropTypes.string,
-  current_user: PropTypes.object,
   report_mode: PropTypes.bool,
   version: PropTypes.string,
+  show_demo_warning: PropTypes.bool,
+  root: PropTypes.string,
+  current_user: PropTypes.object,
 };
 
 export default Header;
