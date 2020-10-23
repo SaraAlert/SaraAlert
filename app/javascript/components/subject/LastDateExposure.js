a84f61a61fb4b8b326a824bb117d9f9822f21e60
Wong, Tim <twong@mitre.org>
2020-10-23 12:10:53 -0400

    fix lde language

--- app/javascript/components/subject/LastDateExposure.js
+++ app/javascript/components/subject/LastDateExposure.js
@@ -187,10 +187,10 @@ class LastDateExposure extends React.Component {
         {this.state.showLastDateOfExposureModal &&
           this.createModal(
             'Last Date of Exposure',
-            `Are you sure you want to ${this.state.last_date_of_exposure ? 'modify' : 'clear'} the Last Date of Exposure to ${
-              this.state.last_date_of_exposure ? moment(this.state.last_date_of_exposure).format('MM/DD/YYYY') : 'blank'
-            }? The Last Date of Exposure will be updated and Continuous Exposure will be turned ${
-              this.state.last_date_of_exposure ? 'OFF' : 'ON'
+            `Are you sure you want to ${this.state.last_date_of_exposure ? 'modify' : 'clear'} the Last Date of Exposure${
+              this.state.last_date_of_exposure ? ` to ${moment(this.state.last_date_of_exposure).format('MM/DD/YYYY')}` : ''
+            }? The Last Date of Exposure will be updated ${this.state.last_date_of_exposure ? '' : 'to blank '}${
+              this.props.patient.monitoring ? `and Continuous Exposure will be turned ${this.state.last_date_of_exposure ? 'OFF' : 'ON'}` : ''
             } for the selected record${this.props.has_group_members ? '(s):' : '.'}`,
             this.closeModal,
             this.submit
