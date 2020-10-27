81e7f0cfebd2deea728aaeb2dfc174e88190bcf8
Kyle Fagan <kfagan@mitre.org>
2020-10-27 16:58:48 -0400

    Show validation errors for labs when importing patients

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -112,7 +112,7 @@ class PatientsController < ApplicationController
     end
 
     # Add patient details that were collected from the form
-    patient = Patient.new(params[:patient].permit(*allowed_params))
+    patient = Patient.new(allowed_params)
 
     # Default to copying *required address into monitored address if monitored address is nil
     if patient.monitored_address_line_1.nil? || patient.monitored_address_state.nil? ||
@@ -166,19 +166,6 @@ class PatientsController < ApplicationController
     if params[:cc_id].present?
       close_contact = CloseContact.where(patient_id: current_user.viewable_patients).where(id: params.permit(:cc_id)[:cc_id])&.first
       close_contact.update(enrolled_id: patient.id)
-    end
-
-    # Create laboratories for patient if included in import
-    unless params.dig(:patient, :laboratories).nil?
-      params[:patient][:laboratories].each do |lab|
-        laboratory = Laboratory.new
-        laboratory.lab_type = lab[:lab_type]
-        laboratory.specimen_collection = lab[:specimen_collection]
-        laboratory.report = lab[:report]
-        laboratory.result = lab[:result]
-        laboratory.patient = patient
-        laboratory.save
-      end
     end
 
     render(json: patient) && return
@@ -515,104 +502,110 @@ class PatientsController < ApplicationController
 
   # Parameters allowed for saving to database
   def allowed_params
-    %i[
-      user_defined_id_statelocal
-      user_defined_id_cdc
-      user_defined_id_nndss
-      first_name
-      middle_name
-      last_name
-      date_of_birth
-      age
-      sex
-      white
-      black_or_african_american
-      american_indian_or_alaska_native
-      asian
-      native_hawaiian_or_other_pacific_islander
-      ethnicity
-      primary_language
-      secondary_language
-      interpretation_required
-      nationality
-      address_line_1
-      foreign_address_line_1
-      address_city
-      address_state
-      address_line_2
-      address_zip
-      address_county
-      monitored_address_line_1
-      monitored_address_city
-      monitored_address_state
-      monitored_address_line_2
-      monitored_address_zip
-      monitored_address_county
-      foreign_address_city
-      foreign_address_country
-      foreign_address_line_2
-      foreign_address_zip
-      foreign_address_line_3
-      foreign_address_state
-      foreign_monitored_address_line_1
-      foreign_monitored_address_city
-      foreign_monitored_address_state
-      foreign_monitored_address_line_2
-      foreign_monitored_address_zip
-      foreign_monitored_address_county
-      primary_telephone
-      primary_telephone_type
-      secondary_telephone
-      secondary_telephone_type
-      email
-      preferred_contact_method
-      preferred_contact_time
-      port_of_origin
-      source_of_report
-      source_of_report_specify
-      flight_or_vessel_number
-      flight_or_vessel_carrier
-      port_of_entry_into_usa
-      travel_related_notes
-      additional_planned_travel_type
-      additional_planned_travel_destination
-      additional_planned_travel_destination_state
-      additional_planned_travel_destination_country
-      additional_planned_travel_port_of_departure
-      date_of_departure
-      date_of_arrival
-      additional_planned_travel_start_date
-      additional_planned_travel_end_date
-      additional_planned_travel_related_notes
-      last_date_of_exposure
-      potential_exposure_location
-      potential_exposure_country
-      contact_of_known_case
-      contact_of_known_case_id
-      travel_to_affected_country_or_area
-      was_in_health_care_facility_with_known_cases
-      was_in_health_care_facility_with_known_cases_facility_name
-      laboratory_personnel
-      laboratory_personnel_facility_name
-      healthcare_personnel
-      healthcare_personnel_facility_name
-      exposure_notes
-      crew_on_passenger_or_cargo_flight
-      monitoring_plan
-      exposure_risk_assessment
-      member_of_a_common_exposure_cohort
-      member_of_a_common_exposure_cohort_type
-      isolation
-      jurisdiction_id
-      assigned_user
-      symptom_onset
-      extended_isolation
-      case_status
-      continuous_exposure
-      gender_identity
-      sexual_orientation
-      user_defined_symptom_onset
-    ]
+    params.require(:patient).permit(
+      :user_defined_id_statelocal,
+      :user_defined_id_cdc,
+      :user_defined_id_nndss,
+      :first_name,
+      :middle_name,
+      :last_name,
+      :date_of_birth,
+      :age,
+      :sex,
+      :white,
+      :black_or_african_american,
+      :american_indian_or_alaska_native,
+      :asian,
+      :native_hawaiian_or_other_pacific_islander,
+      :ethnicity,
+      :primary_language,
+      :secondary_language,
+      :interpretation_required,
+      :nationality,
+      :address_line_1,
+      :foreign_address_line_1,
+      :address_city,
+      :address_state,
+      :address_line_2,
+      :address_zip,
+      :address_county,
+      :monitored_address_line_1,
+      :monitored_address_city,
+      :monitored_address_state,
+      :monitored_address_line_2,
+      :monitored_address_zip,
+      :monitored_address_county,
+      :foreign_address_city,
+      :foreign_address_country,
+      :foreign_address_line_2,
+      :foreign_address_zip,
+      :foreign_address_line_3,
+      :foreign_address_state,
+      :foreign_monitored_address_line_1,
+      :foreign_monitored_address_city,
+      :foreign_monitored_address_state,
+      :foreign_monitored_address_line_2,
+      :foreign_monitored_address_zip,
+      :foreign_monitored_address_county,
+      :primary_telephone,
+      :primary_telephone_type,
+      :secondary_telephone,
+      :secondary_telephone_type,
+      :email,
+      :preferred_contact_method,
+      :preferred_contact_time,
+      :port_of_origin,
+      :source_of_report,
+      :source_of_report_specify,
+      :flight_or_vessel_number,
+      :flight_or_vessel_carrier,
+      :port_of_entry_into_usa,
+      :travel_related_notes,
+      :additional_planned_travel_type,
+      :additional_planned_travel_destination,
+      :additional_planned_travel_destination_state,
+      :additional_planned_travel_destination_country,
+      :additional_planned_travel_port_of_departure,
+      :date_of_departure,
+      :date_of_arrival,
+      :additional_planned_travel_start_date,
+      :additional_planned_travel_end_date,
+      :additional_planned_travel_related_notes,
+      :last_date_of_exposure,
+      :potential_exposure_location,
+      :potential_exposure_country,
+      :contact_of_known_case,
+      :contact_of_known_case_id,
+      :travel_to_affected_country_or_area,
+      :was_in_health_care_facility_with_known_cases,
+      :was_in_health_care_facility_with_known_cases_facility_name,
+      :laboratory_personnel,
+      :laboratory_personnel_facility_name,
+      :healthcare_personnel,
+      :healthcare_personnel_facility_name,
+      :exposure_notes,
+      :crew_on_passenger_or_cargo_flight,
+      :monitoring_plan,
+      :exposure_risk_assessment,
+      :member_of_a_common_exposure_cohort,
+      :member_of_a_common_exposure_cohort_type,
+      :isolation,
+      :jurisdiction_id,
+      :assigned_user,
+      :symptom_onset,
+      :extended_isolation,
+      :case_status,
+      :continuous_exposure,
+      :gender_identity,
+      :sexual_orientation,
+      :user_defined_symptom_onset,
+      laboratories_attributes: [
+        :lab_type,
+        :specimen_collection,
+        :report,
+        :result
+      ]
+    )
   end
 
   # Fields that should be copied over from parent to group member for easier form completion
