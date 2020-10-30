ecb47c4de634cc7aad8335710b979976fd234435
ashankland <ashankland@mitre.org>
2020-10-30 16:36:50 -0400

    Revert "SARA-883 histories filter (#440)"
    This reverts commit 9e35fcb6cfb9ff646f55fcd3eebeda9f13f2880b.

--- app/javascript/components/history/HistoryComponent.js
+++ app/javascript/components/history/HistoryComponent.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import _ from 'lodash';
 import { PropTypes } from 'prop-types';
 import { Card, Row } from 'react-bootstrap';
 import Pagination from 'jw-react-pagination';
@@ -17,7 +16,7 @@ class HistoryComponent extends React.Component {
     this.state = {
       loading: false,
       comment: '',
-      filters: { typeFilters: [], creatorFilters: [] },
+      selectedFilters: [],
       filteredHistories: this.props.histories,
       pageOfHistories: [],
     };
@@ -54,14 +53,9 @@ class HistoryComponent extends React.Component {
 
   filterHistories = () => {
     let filteredHistories = [...this.props.histories];
-    if (this.state.filters.typeFilters.length !== 0) {
+    if (this.state.selectedFilters.length !== 0) {
       filteredHistories = filteredHistories.filter(history => {
-        return this.state.filters.typeFilters.includes(history.history_type);
-      });
-    }
-    if (this.state.filters.creatorFilters.length !== 0) {
-      filteredHistories = filteredHistories.filter(history => {
-        return this.state.filters.creatorFilters.includes(history.created_by);
+        return this.state.selectedFilters.includes(history.history_type);
       });
     }
     this.setState({
@@ -69,16 +63,10 @@ class HistoryComponent extends React.Component {
     });
   };
 
-  handleFilterChange = (inputValue, filterCategory) => {
-    let filters = this.state.filters;
+  handleFilterChange = inputValue => {
     let selectedFilters = [];
     if (Array.isArray(inputValue) && inputValue.length) {
       selectedFilters = inputValue.map(x => x.value);
-    }
-    if (filterCategory == 'History Type') {
-      filters.typeFilters = selectedFilters;
-    } else if (filterCategory == 'History Creator') {
-      filters.creatorFilters = selectedFilters;
     }
     this.setState(
       {
@@ -89,10 +77,6 @@ class HistoryComponent extends React.Component {
       }
     );
   };
-
-  handleTypeFilterChange = inputValue => this.handleFilterChange(inputValue, 'History Type');
-
-  handleCreatorFilterChange = inputValue => this.handleFilterChange(inputValue, 'History Creator');
 
   render() {
     const historiesArray = this.state.pageOfHistories.map(history => <History key={history.id} history={history} />);
@@ -117,14 +101,6 @@ class HistoryComponent extends React.Component {
         ],
       },
     ];
-    const historyCreators = [
-      {
-        label: 'History Creator',
-        options: _.uniq(this.props.histories.map(x => x.created_by)).map(x => {
-          return { value: x, label: x };
-        }),
-      },
-    ];
 
     return (
       <React.Fragment>
@@ -142,30 +118,16 @@ class HistoryComponent extends React.Component {
               <Select
                 closeMenuOnSelect={false}
                 isMulti
-                name="Creator Filters"
-                options={historyCreators}
-                className="basic-multi-select w-25 pl-1"
-                classNamePrefix="select"
-                placeholder="Filter by Creator"
-                theme={theme => ({
-                  ...theme,
-                  borderRadius: 0,
-                })}
-                onChange={this.handleCreatorFilterChange}
-              />
-              <Select
-                closeMenuOnSelect={false}
-                isMulti
                 name="Filters"
                 options={filterOptions}
-                className="basic-multi-select w-25 pl-2"
+                className="basic-multi-select w-25"
                 classNamePrefix="select"
-                placeholder="Filter by Type"
+                placeholder="Filters"
                 theme={theme => ({
                   ...theme,
                   borderRadius: 0,
                 })}
-                onChange={this.handleTypeFilterChange}
+                onChange={this.handleFilterChange}
               />
             </Row>
             {historiesArray}
