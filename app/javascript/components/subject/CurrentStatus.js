977e4741bc9f1b0da45db5c9afda1c719fdbfcf0
Sarah G Ober <sgober@mitre.org>
2020-11-02 13:29:52 -0500

    Initial current status commit

--- app/javascript/components/subject/CurrentStatus.js
+++ app/javascript/components/subject/CurrentStatus.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { PropTypes } from 'prop-types';
-
-import InfoTooltip from '../util/InfoTooltip';
+import { Badge } from 'react-bootstrap';
+import ReactTooltip from 'react-tooltip';
 import EligibilityTooltip from '../util/EligibilityTooltip';
 
 class CurrentStatus extends React.Component {
@@ -11,64 +11,92 @@ class CurrentStatus extends React.Component {
 
   generateStatus(status) {
     if (status === 'exposure_symptomatic') {
-      return <span className="text-danger">symptomatic</span>;
+      return <Badge variant="danger">symptomatic</Badge>;
     } else if (status === 'exposure_asymptomatic') {
-      return <span className="text-success">asymptomatic</span>;
+      return <Badge variant="success">asymptomatic</Badge>;
     } else if (status === 'exposure_non_reporting') {
-      return <span className="text-warning">non-reporting</span>;
+      return <Badge variant="warning">non-reporting</Badge>;
     } else if (status === 'exposure_under_investigation') {
-      return <span className="text-dark">PUI</span>;
+      return <Badge variant="dark">PUI</Badge>;
     } else if (status === 'purged') {
-      return <span className="text-muted">purged</span>;
+      // FIX ME
+      return <Badge variant="muted">purged</Badge>;
     } else if (status === 'closed') {
-      return <span className="text-secondary">not currently being monitored</span>;
+      return <Badge variant="secondary">not currently being monitored</Badge>;
     } else if (status === 'isolation_requiring_review') {
-      return <span className="text-danger">requires review</span>;
+      return <Badge variant="danger">requires review</Badge>;
     } else if (status === 'isolation_symp_non_test_based') {
-      return <span className="text-danger">requires review (symptomatic non test based)</span>;
+      return (
+        <Badge variant="danger" data-for={`symptomatic-non-test-based`} data-tip="">
+          requires review (symptomatic non test based)
+        </Badge>
+      );
     } else if (status === 'isolation_asymp_non_test_based') {
-      return <span className="text-danger">requires review (asymptomatic non test based)</span>;
+      return (
+        <Badge variant="danger" data-for={`aymptomatic-non-test-based`} data-tip="">
+          requires review (asymptomatic non test based)
+        </Badge>
+      );
     } else if (status === 'isolation_test_based') {
-      return <span className="text-danger">requires review (test based)</span>;
+      return (
+        <Badge variant="danger" data-for={`test-based`} data-tip="">
+          requires review (test based)
+        </Badge>
+      );
     } else if (status === 'isolation_non_reporting') {
-      return <span className="text-warning">non-reporting</span>;
+      return <Badge variant="warning">non-reporting</Badge>;
     } else if (status === 'isolation_reporting') {
-      return <span className="text-success">reporting</span>;
+      return <Badge variant="success">reporting</Badge>;
     } else {
       return <span>unknown</span>;
     }
   }
 
-  generateInfoHover(status) {
+  generateStatusTooltip(status) {
     if (status === 'isolation_symp_non_test_based') {
-      return <InfoTooltip tooltipTextKey="symptomaticNonTestBased" location="right"></InfoTooltip>;
+      return (
+        <ReactTooltip id={`symptomatic-non-test-based`} multiline={true} place="top" type="dark" effect="solid" className="tooltip-container">
+          <span>
+            At least 10 days have passed since the symptom onset date and at least 24 hours have passed since the case last reported “Yes” for fever or use of
+            fever-reducing medicine to the system. The system does not collect information on severity of symptoms. Public health will need to validate if other
+            symptoms have improved.
+          </span>
+        </ReactTooltip>
+      );
     } else if (status === 'isolation_asymp_non_test_based') {
-      return <InfoTooltip tooltipTextKey="asymptomaticNonTestBased" location="right"></InfoTooltip>;
+      return (
+        <ReactTooltip id={`asymptomatic-non-test-based`} multiline={true} place="top" type="dark" effect="solid" className="tooltip-container">
+          <span>
+            At least 10 days have passed since the specimen collection date of a positive laboratory test and the monitoree has never reported symptoms.
+          </span>
+        </ReactTooltip>
+      );
     } else if (status === 'isolation_test_based') {
-      return <InfoTooltip tooltipTextKey="testBased" location="right"></InfoTooltip>;
+      return (
+        <ReactTooltip id={`test-based`} multiline={true} place="top" type="dark" effect="solid" className="tooltip-container">
+          <span>
+            Two negative laboratory results have been documented and at least 24 hours have passed since the case last reported “Yes” for fever or use of
+            fever-reducing medicine to the system. The system does not validate the type of test, time between specimen collection, or if the tests were
+            consecutive. Public health will need to validate that the test results meet the latest guidance prior to discontinuing isolation. The system does
+            not collect information on severity of symptoms. Public health will also need to validate if other symptoms have improved.
+          </span>
+        </ReactTooltip>
+      );
     } else {
       return null;
     }
   }
 
-  generateReportEligibility() {
-    return (
-      <React.Fragment>
-        <span>. This {this.props.isolation ? "case's" : "monitoree's"} notification status is&nbsp;</span>
-        <EligibilityTooltip report_eligibility={this.props.report_eligibility} id={`eltt`} inline={true} />.
-      </React.Fragment>
-    );
-  }
-
   render() {
     return (
       <React.Fragment>
-        <h1 className="display-6 pb-3">
-          This monitoree is in the <u>{this.props.isolation ? 'isolation' : 'exposure'}</u> workflow, and their current status is{' '}
-          <b>{this.generateStatus(this.props.status)}</b>
-          {this.generateInfoHover(this.props.status)}
-          {this.generateReportEligibility()}
-        </h1>
+        <h4 className="pb-3">
+          <b>
+            {this.props.isolation ? 'Isolation' : 'Exposure'} Workflow: {this.generateStatus(this.props.status)}
+            {this.generateStatusTooltip(this.props.status)}
+            {'  |  '}Notification status is <EligibilityTooltip report_eligibility={this.props.report_eligibility} id={`eltt`} inline={true} />
+          </b>
+        </h4>
       </React.Fragment>
     );
   }
