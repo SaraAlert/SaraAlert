ced2141fbbbf841790447bb25a7ecf024da94b7a
Justin Kufro <jkufro@mitre.org>
2020-11-03 10:29:36 -0500

    pull query out of view into patients controller

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -6,6 +6,7 @@ class PatientsController < ApplicationController
 
   # Enroller view to see enrolled subjects and button to enroll new subjects
   def index
+    @enrolled_patients = current_user.enrolled_patients.eager_load(:jurisdiction)
     redirect_to(root_url) && return unless current_user.can_create_patient?
   end
 
