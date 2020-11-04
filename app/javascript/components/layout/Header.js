53fff714c96e6401ddd8827b091d5038ed824a62
Moses, Kylie T <ktmoses@mitre.org>
2020-11-04 13:15:21 -0500

    Aligning tabs with logo without increasing navbar height.

--- app/javascript/components/layout/Header.js
+++ app/javascript/components/layout/Header.js
@@ -29,64 +29,60 @@ class Header extends React.Component {
             <React.Fragment>
               <Nav className="pt-2 mr-auto" activeKey={this.getActiveTabKey()}>
                 {this.props.current_user?.can_see_enroller_dashboard_tab && (
-                  <Nav.Link className="ml-3" href={`${window.BASE_PATH}/patients`}>
+                  <Nav.Link className="py-0 ml-3" href={`${window.BASE_PATH}/patients`}>
                     <i className="fas fa-table fa-fw mr-2"></i>Enroller Dashboard
                   </Nav.Link>
                 )}
                 {this.props.current_user?.can_see_monitoring_dashboards_tab && (
-                  <Nav.Link className="ml-3" href={`${window.BASE_PATH}/public_health`}>
+                  <Nav.Link className="py-0 ml-3" href={`${window.BASE_PATH}/public_health`}>
                     <i className="fas fa-table fa-fw mr-2"></i>Monitoring Dashboards
                   </Nav.Link>
                 )}
                 {this.props.current_user?.can_see_admin_panel_tab && (
-                  <Nav.Link className="ml-3" href={`${window.BASE_PATH}/admin`}>
+                  <Nav.Link className="py-0 ml-3" href={`${window.BASE_PATH}/admin`}>
                     <i className="fas fa-user-cog fa-fw mr-2"></i>Admin Panel
                   </Nav.Link>
                 )}
                 {this.props.current_user?.can_see_analytics_tab && (
-                  <Nav.Link className="ml-3" href={`${window.BASE_PATH}/analytics`}>
+                  <Nav.Link className="py-0 ml-3" href={`${window.BASE_PATH}/analytics`}>
                     <i className="fas fa-chart-pie fa-fw mr-2"></i>Analytics
                   </Nav.Link>
                 )}
               </Nav>
-              <Form inline className="ml-auto">
-                {!this.props.report_mode && (
+              <Form inline className="pt-2 ml-auto">
+                <Navbar.Text className="text-white py-0 px-3">
+                  <i className="fas fa-user fa-fw mr-2"></i>
+                  {this.props.current_user?.email}
+                </Navbar.Text>
+                <a className="white-border-right"></a>
+                <div className="dropdown">
+                  <Nav.Link className="text-white py-0" id="helpMenuButton" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <i className="fas fa-question-circle fa-fw"></i>
+                  </Nav.Link>
+                  <div className="dropdown-menu dropdown-menu-right" aria-labelledby="helpMenuButton">
+                    <a className="dropdown-item" href="https://saraalert.org/public-health/guides/" target="_blank" rel="noreferrer">
+                      <i className="fas fa-book fa-fw"></i> User Guides
+                    </a>
+                    <a className="dropdown-item" href="https://virtualcommunities.naccho.org/saraalertforum/home" target="_blank" rel="noreferrer">
+                      <i className="fas fa-comments fa-fw"></i> User Forum
+                    </a>
+                    <a className="dropdown-item" href="https://saraalert.org/contact/" target="_blank" rel="noreferrer">
+                      <i className="fas fa-envelope-open-text fa-fw"></i> Contact Us
+                    </a>
+                  </div>
+                </div>
+                <a className="white-border-right"></a>
+                {this.props.current_user?.is_usa_admin && (
                   <React.Fragment>
-                    <Navbar.Text className="text-white px-3">
-                      <i className="fas fa-user fa-fw mr-2"></i>
-                      {this.props.current_user?.email}
-                    </Navbar.Text>
-                    <a className="white-border-right"></a>
-                    <div className="dropdown">
-                      <Nav.Link className="text-white" id="helpMenuButton" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <i className="fas fa-question-circle fa-fw"></i>
-                      </Nav.Link>
-                      <div className="dropdown-menu dropdown-menu-right" aria-labelledby="helpMenuButton">
-                        <a className="dropdown-item" href="https://saraalert.org/public-health/guides/" target="_blank" rel="noreferrer">
-                          <i className="fas fa-book fa-fw"></i> User Guides
-                        </a>
-                        <a className="dropdown-item" href="https://virtualcommunities.naccho.org/saraalertforum/home" target="_blank" rel="noreferrer">
-                          <i className="fas fa-comments fa-fw"></i> User Forum
-                        </a>
-                        <a className="dropdown-item" href="https://saraalert.org/contact/" target="_blank" rel="noreferrer">
-                          <i className="fas fa-envelope-open-text fa-fw"></i> Contact Us
-                        </a>
-                      </div>
-                    </div>
-                    <a className="white-border-right"></a>
-                    {this.props.current_user?.is_usa_admin && (
-                      <React.Fragment>
-                        <Nav.Link className="night-nav-link text-white" href="/oauth/applications">
-                          <i className="fas fa-share-alt fa-fw mr-2"></i>API
-                        </Nav.Link>
-                        <a className="white-border-right"></a>
-                      </React.Fragment>
-                    )}
-                    <Nav.Link className="night-nav-link text-white" href={`${window.BASE_PATH}/users/sign_out`} data-method="DELETE">
-                      <i className="fas fa-sign-out-alt fa-fw mr-2"></i>Logout
+                    <Nav.Link className="text-white py-0" href="/oauth/applications">
+                      <i className="fas fa-share-alt fa-fw mr-2"></i>API
                     </Nav.Link>
+                    <a className="white-border-right"></a>
                   </React.Fragment>
                 )}
+                <Nav.Link className="text-white py-0" href={`${window.BASE_PATH}/users/sign_out`} data-method="DELETE">
+                  <i className="fas fa-sign-out-alt fa-fw mr-2"></i>Logout
+                </Nav.Link>
               </Form>
             </React.Fragment>
           )}
