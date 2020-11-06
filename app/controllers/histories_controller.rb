4caf4ab0b9b8625d1fd029cf30750261b59312b9
Wong, Tim <twong@mitre.org>
2020-11-06 09:18:00 -0500

    brakeman

--- app/controllers/histories_controller.rb
+++ app/controllers/histories_controller.rb
@@ -15,8 +15,8 @@ class HistoriesController < ApplicationController
 
     # Increment number of contact attempts if applicable
     if params.permit(:type)[:type] == 'Contact Attempt'
-      patient = Patient.find(params.permit(:patient_id)[:patient_id])
-      patient.update(contact_attempts: patient.contact_attempts + 1)
+      patient = current_user.patients.find(params.permit(:patient_id)[:patient_id])
+      patient&.update(contact_attempts: patient.contact_attempts + 1)
     end
 
     redirect_back fallback_location: root_path
