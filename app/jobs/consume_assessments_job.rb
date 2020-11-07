9a319403ebf577769431e7e44f9fd0a1faa2e2de
Adam M. Holmes <aholmes@mitre.org>
2020-11-07 12:58:52 -0500

    Utilize RPOPLPUSH for split arch bridge with a queue

--- app/jobs/consume_assessments_job.rb
+++ app/jobs/consume_assessments_job.rb
@@ -1,19 +1,25 @@
 # frozen_string_literal: true
 
 require 'redis'
+require 'redis-queue'
 
 # ConsumeAssessmentsJob: Pulls assessments created in the split instance and saves them
 class ConsumeAssessmentsJob < ApplicationJob
   queue_as :default
 
   def perform
-    Rails.application.config.redis.subscribe 'reports' do |on|
-      on.message do |_channel, msg|
-        # message = SaraSchema::Validator.validate(:assessment, JSON.parse(msg))
+    queue = Redis::Queue.new('q_bridge', 'bp_q_bridge', redis: Rails.application.config.redis)
+
+    while (msg = queue.pop)
+      begin
         message = JSON.parse(msg)&.slice('threshold_condition_hash', 'reported_symptoms_array',
                                          'patient_submission_token', 'experiencing_symptoms', 'response_status')
 
-        Rails.logger.info 'ConsumeAssessmentsJob: skipping nil message...' && next if message.nil?
+        # Invalid message
+        if message.nil?
+          Rails.logger.info 'ConsumeAssessmentsJob: skipping nil message...'
+          next
+        end
 
         patient = Patient.where(purged: false).find_by(submission_token: message['patient_submission_token'])
 
@@ -23,12 +29,16 @@ class ConsumeAssessmentsJob < ApplicationJob
           patient = Patient.find_by(submission_token: patient_lookup[:new_submission_token]) unless patient_lookup.nil?
         end
 
-        Rails.logger.info "ConsumeAssessmentsJob: skipping nil patient (token #{message['patient_submission_token']})..." && next if patient.nil?
+        # Failed to find patient
+        if patient.nil?
+          Rails.logger.info "ConsumeAssessmentsJob: skipping nil patient (token: #{message['patient_submission_token']})..."
+          next
+        end
 
         # Prevent duplicate patient assessment spam
         # Only check for latest assessment if there is one
         if !patient.latest_assessment.nil? && (patient.latest_assessment.created_at > ADMIN_OPTIONS['reporting_limit'].minutes.ago)
-          Rails.logger.info "ConsumeAssessmentsJob: skipping duplicate assessment (patient #{patient.id})..."
+          Rails.logger.info "ConsumeAssessmentsJob: skipping duplicate assessment (patient: #{patient.id})..."
           next
         end
 
@@ -46,18 +56,18 @@ class ConsumeAssessmentsJob < ApplicationJob
                                                                               of household and nobody answered the phone.")
           end
 
-          next
+          queue.commit
         when 'no_answer_sms'
           # No need to wipe out last_assessment_reminder_sent so that another sms will be sent because the sms studio flow is kept open for 18hrs
           History.contact_attempt(patient: patient, comment: "Sara Alert texted this monitoree's primary telephone \
-                                                             number #{patient.primary_telephone} during their preferred \
-                                                             contact time, but did not receive a response.")
+                                                              number #{patient.primary_telephone} during their preferred \
+                                                              contact time, but did not receive a response.")
           unless dependents.blank?
             create_contact_attempt_history_for_dependents(dependents, "Sara Alert texted this monitoree's head of \
                                                                               household and did not receive a response.")
           end
 
-          next
+          queue.commit
         when 'error_voice'
           # If there was an error in completeing the call, nil out the last_reminder_sent field so the system will try calling again
           patient.update(last_assessment_reminder_sent: nil)
@@ -68,29 +78,34 @@ class ConsumeAssessmentsJob < ApplicationJob
                                                                               to this monitoree's head of household.")
           end
 
-          next
+          queue.commit
         when 'error_sms'
           # If there was an error sending an SMS, nil out the last_reminder_sent field so the system will try calling again
           patient.update(last_assessment_reminder_sent: nil)
           History.contact_attempt(patient: patient, comment: "Sara Alert was unable to send an SMS to this monitoree's \
-                                                             primary telephone number #{patient.primary_telephone}.")
+                                                              primary telephone number #{patient.primary_telephone}.")
           unless dependents.blank?
             create_contact_attempt_history_for_dependents(dependents, "Sara Alert was unable to send an SMS to \
                                                                               this monitoree's head of household.")
           end
 
-          next
+          queue.commit
         end
 
         threshold_condition = ThresholdCondition.where(type: 'ThresholdCondition').find_by(threshold_condition_hash: message['threshold_condition_hash'])
-        Rails.logger.info "ConsumeAssessmentsJob: skipping nil threshold (patient #{patient.id})..." && next unless threshold_condition
+
+        # Invalid threshold condition hash
+        if threshold_condition.nil?
+          Rails.logger.info "ConsumeAssessmentsJob: skipping nil threshold (patient: #{patient.id}, hash: #{message['threshold_condition_hash']})..."
+          next
+        end
 
         if message['reported_symptoms_array']
           typed_reported_symptoms = Condition.build_symptoms(message['reported_symptoms_array'])
           reported_condition = ReportedCondition.new(symptoms: typed_reported_symptoms, threshold_condition_hash: message['threshold_condition_hash'])
           assessment = Assessment.new(reported_condition: reported_condition, patient: patient, who_reported: 'Monitoree')
           assessment.symptomatic = assessment.symptomatic? || message['experiencing_symptoms']
-          assessment.save
+          queue.commit if assessment.save
         else
           # If message['reported_symptoms_array'] is not populated then this assessment came in through
           # a generic channel ie: SMS where monitorees are asked YES/NO if they are experiencing symptoms
@@ -111,11 +126,11 @@ class ConsumeAssessmentsJob < ApplicationJob
             # that they reported for themselves, else we are creating an assessment for the dependent and
             # that means that it was the proxy who reported for them
             assessment.who_reported = patient.submission_token == dependent.submission_token ? 'Monitoree' : 'Proxy'
-            assessment.save
+            queue.commit if assessment.save
           end
         end
       rescue JSON::ParserError
-        next
+        Rails.logger.info "ConsumeAssessmentsJob: skipping invalid message..." && next
       end
     end
   rescue Redis::ConnectionError, Redis::CannotConnectError => e
