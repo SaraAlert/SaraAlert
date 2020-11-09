91222966b7d9adfb17297f731ccc6c3b431c4aa8
Justin Kufro <jkufro@mitre.org>
2020-11-09 11:16:35 -0500

    empty test file for assessments controller test

---
+++ test/controllers/assessments_controller_test.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'test_case'
+
+class AssessmentsControllerTest < ActionController::TestCase
+  def setup; end
+
+  def teardown; end
+
+  test 'nothing' do
+
+  end
+end
