16a12a776b196211a32ea8806e76287da56908cf
ashankland <ashankland@mitre.org>
2020-11-10 13:54:33 -0500

    first pass, still gotta capitalize

--- app/javascript/components/layout/Header.js
+++ app/javascript/components/layout/Header.js
@@ -7,6 +7,10 @@ class Header extends React.Component {
     super(props);
   }
 
+  getUserRoleIcon = role => {
+    return role;
+  };
+
   /**
    * Finds the "activeKey" for the nav bar based on current path.
    */
@@ -52,8 +56,17 @@ class Header extends React.Component {
               <Form inline className="ml-auto">
                 <Navbar.Text className="text-white py-0 px-3">
                   <i className="fas fa-user fa-fw mr-2"></i>
-                  {this.props.current_user?.email}
+                  {this.props.current_user?.email} ({this.props.current_user?.role.split('_').join(' ')})
                 </Navbar.Text>
+                <span className="fas-stack fa-3x">
+                  <i className="fas fa-user fa-fw mr-2 fa-stack-2x"></i>
+                  <strong className="fa-fw mr-2 fa-stack-1x calendar-text">27</strong>
+                </span>
+
+                {/* <a className="white-border-right"></a>
+                <Navbar.Text className="text-white py-0 px-3">
+                  {this.props.current_user?.role.split('_').join(' ')}
+                </Navbar.Text> */}
                 <a className="white-border-right"></a>
                 <div className="dropdown">
                   <Nav.Link className="text-white py-0" id="helpMenuButton" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
