f3f0d519525c81931a390625dbe5797c4430562d
Kyle Fagan <kfagan@mitre.org>
2020-11-10 16:41:49 -0500

    Update worker with latest logic. Update log messages to be a bit more specific

--- app/workers/consume_assessments_worker.rb
+++ app/workers/consume_assessments_worker.rb
@@ -11,19 +11,30 @@ class ConsumeAssessmentsWorker
                                      'patient_submission_token',
                                      'experiencing_symptoms',
                                      'response_status')
-
-    if message.nil?
-      Rails.logger.info 'ConsumeAssessmentsJob: skipping nil message...'
+    if message.empty?
+      Rails.logger.info('ConsumeAssessmentsJob: No valid fields found in message. Skipping.')
       return
     end
 
-    patient = Patient.find_by(submission_token: message['patient_submission_token'])
+    patient = Patient.where(purged: false).find_by(submission_token: message['patient_submission_token'])
 
-    return if patient.nil?
+    # Perform patient lookup for old submission tokens
+    if patient.nil?
+      patient_lookup = PatientLookup.find_by(old_submission_token: message['patient_submission_token'])
+      patient = Patient.find_by(submission_token: patient_lookup[:new_submission_token]) unless patient_lookup.nil?
+    end
+
+    if patient.nil?
+      Rails.logger.info("ConsumeAssessmentsJob: No patient found with submission_token: #{message['patient_submission_token']}")
+      return
+    end
 
     # Prevent duplicate patient assessment spam
     # Only check for latest assessment if there is one
-    return if !patient.latest_assessment.nil? && (patient.latest_assessment.created_at > ADMIN_OPTIONS['reporting_limit'].minutes.ago)
+    if !patient.latest_assessment.nil? && (patient.latest_assessment.created_at > ADMIN_OPTIONS['reporting_limit'].minutes.ago)
+      Rails.logger.info("ConsumeAssessmentsJob: Skipping duplicate assessment (patient: #{patient.id})")
+      return
+    end
 
     # Get list of dependents excluding the patient itself.
     dependents = patient.dependents_exclude_self
@@ -75,8 +86,14 @@ class ConsumeAssessmentsWorker
       return
     end
 
-    threshold_condition = ThresholdCondition.where(threshold_condition_hash: message['threshold_condition_hash']).first
-    return unless threshold_condition
+    threshold_condition = ThresholdCondition.where(type: 'ThresholdCondition').find_by(threshold_condition_hash: message['threshold_condition_hash'])
+
+    # Invalid threshold_condition_hash
+    if threshold_condition.nil?
+      Rails.logger.info("ConsumeAssessmentsJob: No ThresholdCondition found (patient: #{patient.id},
+                        threshold_condition_hash: #{message['threshold_condition_hash']})")
+      return
+    end
 
     if message['reported_symptoms_array']
       typed_reported_symptoms = Condition.build_symptoms(message['reported_symptoms_array'])
@@ -87,7 +104,7 @@ class ConsumeAssessmentsWorker
     else
       # If message['reported_symptoms_array'] is not populated then this assessment came in through
       # a generic channel ie: SMS where monitorees are asked YES/NO if they are experiencing symptoms
-      patient.active_dependents.uniq.each do |pat|
+      patient.active_dependents.uniq.each do |dependent|
         typed_reported_symptoms = if message['experiencing_symptoms']
                                     # Remove values so that the values will appear as blank in a symptomatic report
                                     # this will indicate that the person needs to be reached out to to get the actual values
@@ -96,22 +113,20 @@ class ConsumeAssessmentsWorker
                                     # The person is not experiencing symptoms, we can infer that the bool symptoms are the opposite
                                     # of the threshold values that represent symptomatic
                                     threshold_condition.clone_symptoms_negate_bool_values
-                                    reported_condition = ReportedCondition.new(symptoms: typed_reported_symptoms,
-                                                                               threshold_condition_hash: message['threshold_condition_hash'])
-                                    assessment = Assessment.new(reported_condition: reported_condition, patient: pat)
-                                    assessment.symptomatic = assessment.symptomatic? || message['experiencing_symptoms']
-                                    # If current user in the collection of patient + patient dependents is the patient, then that means
-                                    # that they reported for themselves, else we are creating an assessment for the dependent and
-                                    # that means that it was the proxy who reported for them
-                                    assessment.who_reported = if message['patient_submission_token'] == pat.submission_token
-                                                                'Monitoree'
-                                                              else
-                                                                'Proxy'
-                                                              end
                                   end
+        reported_condition = ReportedCondition.new(symptoms: typed_reported_symptoms, threshold_condition_hash: message['threshold_condition_hash'])
+        assessment = Assessment.new(reported_condition: reported_condition, patient: dependent)
+        assessment.symptomatic = assessment.symptomatic? || message['experiencing_symptoms']
+        # If current user in the collection of patient + patient dependents is the patient, then that means
+        # that they reported for themselves, else we are creating an assessment for the dependent and
+        # that means that it was the proxy who reported for them
+        assessment.who_reported = patient.submission_token == dependent.submission_token ? 'Monitoree' : 'Proxy'
         assessment.save
       end
     end
+  rescue JSON::ParserError
+    # Do not reproduce entire message here. There may be sensitive data in the message.
+    Rails.logger.info('ConsumeAssessmentsJob: Skipping invalid message.')
   end
 
   private
--- lib/tasks/reports.rake
+++ lib/tasks/reports.rake
@@ -6,22 +6,15 @@ require 'redis-queue'
 namespace :reports do
   desc "Receive and Process Reports"
   task receive_and_process_reports: :environment do
-    tries = 0
+    Rails.logger.info('Starting the ConsumeAssessments task')
     queue = Redis::Queue.new('q_bridge', 'bp_q_bridge', redis: Rails.application.config.redis)
 
     while(msg = queue.pop)
-      ConsumeAssessmentsWorker.perform_now(msg)
+      ConsumeAssessmentsWorker.perform_async(msg)
     end
   rescue Redis::ConnectionError, Redis::CannotConnectError => e
-    Rails.logger.info "ConsumeAssessmentsJob: Redis::ConnectionError (#{e}), retrying..."
-    if tries < 3
-      tries += 1
-      sleep(1)
-      retry
-    else
-      Rails.logger.info "ConsumeAssessmentsJob: Redis connection error > 3 times, cancelling queuing of this message."
-      Rails.logger.info "Dropped Message: #{msg}"
-      tries = 0
-    end
+    Rails.logger.info("ConsumeAssessmentsJob: Redis::ConnectionError (#{e}), retrying")
+    sleep(1)
+    retry
   end
 end
