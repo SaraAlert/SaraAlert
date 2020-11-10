8f80356258b3aa75394aca1856139eab644a03a3
Matt Mayer <mmayer@mitre.org>
2020-11-10 08:39:19 -0500

    Change has_blocked_sms to just blocked_sms

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -589,7 +589,7 @@ class Patient < ApplicationRecord
   end
 
   # Determine if this patient's phone number has blocked communication with SaraAlert
-  def has_blocked_sms
+  def blocked_sms
     BlockedNumber.exists?(phone_number: primary_telephone)
   end
 
