169a872c9021e802af796b89790b0b3366e5606f
Matt Mayer <mmayer@mitre.org>
2020-11-13 14:20:10 -0500

    Add null false to blocked numbers phone number

--- db/migrate/20201020194506_create_blocked_numbers.rb
+++ db/migrate/20201020194506_create_blocked_numbers.rb
@@ -1,7 +1,7 @@
 class CreateBlockedNumbers < ActiveRecord::Migration[6.0]
   def change
     create_table :blocked_numbers do |t|
-      t.string :phone_number
+      t.string :phone_number, null: false
       t.index [:phone_number], name: "index_blocked_phone_number"
     end
   end
