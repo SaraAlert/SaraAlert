bb4d205ae21dc39037756420afeaaf21ce5419b1
Sarah G Ober <sgober@mitre.org>
2020-11-16 11:15:57 -0500

    fix broken tests

--- app/javascript/components/patient/Patient.js
+++ app/javascript/components/patient/Patient.js
@@ -13,7 +13,7 @@ class Patient extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      showCaseInfo: props.details.isolation || (!props.details.isolation && props.details.exposure_notes),
+      showCaseInfo: props?.details?.isolation || (!props?.details?.isolation && props?.details?.exposure_notes),
       expandNotes: false,
     };
   }
@@ -337,7 +337,7 @@ class Patient extends React.Component {
                     <React.Fragment>
                       <Col>
                         <Row>
-                          <Col className="text-truncate">
+                          <Col className="text-truncate departed-col">
                             <Row className="py-1">
                               <Col>
                                 <b>DEPARTED</b>
@@ -357,7 +357,7 @@ class Patient extends React.Component {
                               </Col>
                             </Row>
                           </Col>
-                          <Col className="text-truncate">
+                          <Col className="text-truncate arrival-col">
                             <Row className="py-1">
                               <Col>
                                 <b>ARRIVAL</b>
@@ -379,7 +379,7 @@ class Patient extends React.Component {
                             </Row>
                           </Col>
                         </Row>
-                        <Row>
+                        <Row className="carrier-row">
                           <Col className="text-truncate pt-1">
                             <Row>
                               <Col>
@@ -456,12 +456,6 @@ class Patient extends React.Component {
                                 ? this.props.details.additional_planned_travel_port_of_departure
                                 : '--'
                             }`}</span>
-                          </Col>
-                        </Row>
-                        <Row>
-                          <Col>
-                            <b>Flight or Vessel Number:</b>{' '}
-                            <span>{this.props.details.flight_or_vessel_number && `${this.props.details.flight_or_vessel_number}`}</span>
                           </Col>
                         </Row>
                         <Row>
