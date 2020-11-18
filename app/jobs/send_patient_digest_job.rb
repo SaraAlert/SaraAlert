379a6b61de1d2973f321721fbe504e69523e48e1
ashankland <ashankland@mitre.org>
2020-11-18 17:05:14 -0500

    fixed static analysis

--- app/jobs/send_patient_digest_job.rb
+++ app/jobs/send_patient_digest_job.rb
@@ -2,22 +2,20 @@
 
 # SendPatientDigestJob: sends assessment reminder to patients
 class SendPatientDigestJob < ApplicationJob
-    queue_as :mailers
-  
-    def perform(*_args)
-        jurisdiction_ids = []
-        jurisdiction_lists = {}
-        jurisdiction_addressees = {}
-        Jurisdiction.find_each do |jur|
-            if jur.send_digest
-                jurisdiction_ids << jur.id
-                jurisdiction_patients = jur.all_patients.recently_symptomatic
-                jurisdiction_addressees = User.where(jurisdiction_id: jur.id, role: ["super_user", "public_health", "public_health_enroller"])
-                jurisdiction_addressees.each do |user|
-                    eligible = jurisdiction_patients.count
-                    UserMailer.send_patient_digest_job_email(jurisdiction_patients, user, eligible).deliver_now
-                end
-            end
+  queue_as :mailers
+
+  def perform(*_args)
+    jurisdiction_lists = {}
+    jurisdiction_addressees = {}
+    Jurisdiction.find_each do |jur|
+      if jur.send_digest
+        jurisdiction_patients = jur.all_patients.recently_symptomatic
+        jurisdiction_addressees = User.where(jurisdiction_id: jur.id, role: ["super_user", "public_health", "public_health_enroller"])
+        jurisdiction_addressees.each do |user|
+          eligible = jurisdiction_patients.count
+          UserMailer.send_patient_digest_job_email(jurisdiction_patients, user, eligible).deliver_now
         end
+      end
+    end
   end
 end
