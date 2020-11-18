abd23847a0c7e7fd6d622ba5deecd123581777a5
ashankland <ashankland@mitre.org>
2020-11-18 17:25:16 -0500

    a few more static analysis issues

--- app/jobs/send_patient_digest_job.rb
+++ app/jobs/send_patient_digest_job.rb
@@ -5,12 +5,11 @@ class SendPatientDigestJob < ApplicationJob
   queue_as :mailers
 
   def perform(*_args)
-    jurisdiction_lists = {}
     jurisdiction_addressees = {}
     Jurisdiction.find_each do |jur|
       if jur.send_digest
         jurisdiction_patients = jur.all_patients.recently_symptomatic
-        jurisdiction_addressees = User.where(jurisdiction_id: jur.id, role: ["super_user", "public_health", "public_health_enroller"])
+        jurisdiction_addressees = User.where(jurisdiction_id: jur.id, role: ['super_user', 'public_health', 'public_health_enroller'])
         jurisdiction_addressees.each do |user|
           eligible = jurisdiction_patients.count
           UserMailer.send_patient_digest_job_email(jurisdiction_patients, user, eligible).deliver_now
