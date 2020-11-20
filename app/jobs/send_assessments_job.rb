efc1fd1bf8081499d165c31942efb375c35da586
Kyle Fagan <kfagan@mitre.org>
2020-11-20 11:55:49 -0500

    Reduce query in send assessements job to use size after gathering all reminder eligible

--- app/jobs/send_assessments_job.rb
+++ app/jobs/send_assessments_job.rb
@@ -5,15 +5,15 @@ class SendAssessmentsJob < ApplicationJob
   queue_as :mailers
 
   def perform(*_args)
-    eligible = Patient.reminder_eligible.count
+    eligible = Patient.reminder_eligible
     sent = []
     not_sent = []
-    Patient.reminder_eligible.find_each do |patient|
+    eligible.each do |patient|
       sent << { id: patient.id, method: patient.preferred_contact_method } if patient.send_assessment
     rescue StandardError => e
       not_sent << { id: patient.id, method: patient.preferred_contact_method, reason: e.message }
       next
     end
-    UserMailer.assessment_job_email(sent, not_sent, eligible).deliver_now
+    UserMailer.assessment_job_email(sent, not_sent, eligible.size).deliver_now
   end
 end
