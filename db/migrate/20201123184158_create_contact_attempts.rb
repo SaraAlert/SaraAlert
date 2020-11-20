7c4ad97cce4ffdd8e3e29bf6bb7cd2978d00af13
Wong, Tim <twong@mitre.org>
2020-11-20 15:09:36 -0500

    generate system vs. manual contact attempts

--- db/migrate/20201123184158_create_contact_attempts.rb
+++ db/migrate/20201123184158_create_contact_attempts.rb
@@ -14,26 +14,28 @@ class CreateContactAttempts < ActiveRecord::Migration[6.0]
     execute <<-SQL.squish
       INSERT INTO contact_attempts (patient_id, user_id, successful, note, created_at, updated_at)
       SELECT histories.patient_id as patient_id,
-            users.id as user_id,
-            histories.comment NOT LIKE '%unsuccessful contact attempt%' as successful,
-            SUBSTRING_INDEX(histories.comment, 'uccessful contact attempt. Note: ', -1) as comment,
-            histories.created_at as created_at,
-            histories.updated_at as updated_at
+             users.id as user_id,
+             histories.comment NOT LIKE '%unsuccessful contact attempt%' as successful,
+             SUBSTRING_INDEX(histories.comment, 'uccessful contact attempt. Note: ', -1) as comment,
+             histories.created_at as created_at,
+             histories.updated_at as updated_at
       FROM histories
       LEFT JOIN users
       ON histories.created_by = users.email
       WHERE history_type = 'Contact Attempt'
+      AND created_by <> 'Sara Alert System'
       UNION
       SELECT histories.patient_id as patient_id,
-            users.id as user_id,
-            histories.comment NOT LIKE '%unsuccessful contact attempt%' as successful,
-            SUBSTRING_INDEX(histories.comment, 'uccessful contact attempt. Note: ', -1) as comment,
-            histories.created_at as created_at,
-            histories.updated_at as updated_at
+             users.id as user_id,
+             histories.comment NOT LIKE '%unsuccessful contact attempt%' as successful,
+             SUBSTRING_INDEX(histories.comment, 'uccessful contact attempt. Note: ', -1) as comment,
+             histories.created_at as created_at,
+             histories.updated_at as updated_at
       FROM users
       RIGHT JOIN histories
       ON histories.created_by = users.email
       WHERE history_type = 'Contact Attempt'
+      AND created_by <> 'Sara Alert System'
     SQL
   end
 
