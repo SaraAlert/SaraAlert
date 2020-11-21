048276f770a6f44c1f31d9dd7357b5f1351901b6
Justin Kufro <jkufro@mitre.org>
2020-11-21 08:37:35 -0500

    new tests for reminder eligible

--- test/models/patient_notification_eligibilty_test.rb
+++ test/models/patient_notification_eligibilty_test.rb
@@ -34,7 +34,7 @@ class PatientNotificationEligibilityTest < ActiveSupport::TestCase
       attributes = p.attributes.slice(
         'id', 'responder_id', 'head_of_household', 'preferred_contact_method', 'pause_notifications',
         'isolation', 'continuous_exposure', 'last_date_of_exposure', 'created_at',
-        'monitoring', 'closed_at', 'last_assessment_reminder_sent', 'purged'
+        'monitoring', 'closed_at', 'last_assessment_reminder_sent', 'latest_assessment_at', 'purged'
       )
       result += "\n#{attributes}\n"
     end
@@ -125,6 +125,27 @@ class PatientNotificationEligibilityTest < ActiveSupport::TestCase
       assert_ineligible(dependent)
       dependent.destroy
     end
+  end
+
+  test 'MySQL date compare works as expected' do
+    # This test is just a sanity check to verify that casting to a DATE in MySQL
+    # effectively gives us the beginning of the day and is considered less than
+    # one second past the beginning of the day
+    dt = Time.now.getlocal('-00:00').beginning_of_day + 1.second
+    query = ActiveRecord::Base.connection.raw_connection.prepare("SELECT Date(?) < ?")
+    results = query.execute(dt, dt)
+    assert_equal 1, results.first.first
+    query.close
+
+    query = ActiveRecord::Base.connection.raw_connection.prepare("SELECT Date(?) > ?")
+    results = query.execute(dt, dt)
+    assert_equal 0, results.first.first
+    query.close
+
+    query = ActiveRecord::Base.connection.raw_connection.prepare("SELECT Date(?) != ?")
+    results = query.execute(dt, dt)
+    assert_equal 1, results.first.first
+    query.close
   end
 
   test 'ignored dependent eligibility fields' do
@@ -231,7 +252,7 @@ class PatientNotificationEligibilityTest < ActiveSupport::TestCase
           :assessment,
           patient: patient,
           symptomatic: false,
-          created_at: Time.now.getlocal('-04:00').yesterday.end_of_day
+          created_at: 1.day.ago.end_of_day
         )
         assert_eligible(patient)
         # Creating an assessment from today SHOULD affect eligibility
@@ -239,7 +260,7 @@ class PatientNotificationEligibilityTest < ActiveSupport::TestCase
           :assessment,
           patient: patient,
           symptomatic: false,
-          created_at: Time.now.getlocal('-04:00').beginning_of_day
+          created_at: Time.now.beginning_of_day
         )
         assert_ineligible(patient)
       end
@@ -388,7 +409,53 @@ class PatientNotificationEligibilityTest < ActiveSupport::TestCase
     assert_eligible(patient)
   end
 
-  test 'configuring reporting period affects eligibility' do
+  test 'Patients in differing timezones are eligible when expected' do
+    [
+      { monitored_address_state: 'Florida'},
+      { monitored_address_state: 'Colorado'},
+      { monitored_address_state: nil, address_state: 'California' },
+      {}
+    ].each do |patient_params|
+      patient = create(
+        :patient,
+        {
+          preferred_contact_method: 'E-mailed Web Link',
+          continuous_exposure: true
+        }.merge(patient_params)
+      )
+      assert_eligible(patient)
+      create(
+        :assessment,
+        patient: patient,
+        # converting to UTC because these DB times are assumed to be saved in UTC
+        created_at: Time.now.getlocal(patient.address_timezone_offset).yesterday.end_of_day.utc
+      )
+      assert_eligible(patient)
+      create(
+        :assessment,
+        patient: patient,
+        created_at: Time.now.getlocal(patient.address_timezone_offset).beginning_of_day.utc
+      )
+      assert_ineligible(patient)
+    end
+  end
 
+  test 'configuring reporting period affects eligibility' do
+    # patient was created more than 24 hours ago
+    patient = create(
+      :patient,
+      preferred_contact_method: 'E-mailed Web Link',
+      monitoring: true,
+      purged: false,
+      isolation: false,
+      created_at: 4.days.ago,
+      continuous_exposure: true
+    )
+    assert_eligible(patient)
+    ADMIN_OPTIONS['reporting_period_minutes'] = 1440 * 7  # 1 week
+    create(:assessment, patient: patient, symptomatic: false, created_at: 7.days.ago.end_of_day)
+    assert_eligible(patient)
+    create(:assessment, patient: patient, symptomatic: false, created_at: 6.days.ago.beginning_of_day)
+    assert_ineligible(patient)
   end
 end
