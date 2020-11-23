8f36581f65e57f0886f787e6b8ccc1d9f94725be
Justin Kufro <jkufro@mitre.org>
2020-11-23 09:39:21 -0500

    when creating time_zone_offset column, update all existing patients

--- db/migrate/20201121134354_add_time_zone_offset_to_patients.rb
+++ db/migrate/20201121134354_add_time_zone_offset_to_patients.rb
@@ -1,5 +1,12 @@
 class AddTimeZoneOffsetToPatients < ActiveRecord::Migration[6.0]
-  def change
+  def up
     add_column :patients, :time_zone_offset, :string
+    # -04:00 was being used before and we expect it to be updated
+    # to the proper value the next time each patient is saved.
+    Patient.update_all(time_zone_offset: '-04:00')
+  end
+
+  def down
+    remove_column :patients, :time_zone_offset
   end
 end
