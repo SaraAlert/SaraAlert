2740184722e7aea31d07ff61200a2a0e33e0ce5d
David Connolly <dpconnolly@mitre.org>
2020-11-23 15:02:57 -0500

    This commit fixes rebase issues and other git issues that were encountered.

--- db/migrate/20201116134817_add_status_to_monitoree_counts.rb
+++ db/migrate/20201116134817_add_status_to_monitoree_counts.rb
@@ -1,4 +1,9 @@
 class AddStatusToMonitoreeCounts < ActiveRecord::Migration[6.0]
-  def change
+  def up
+    add_column :monitoree_counts, :status, :string, default: 'Missing'
+  end
+
+  def down
+    remove_column :monitoree_counts, :status, :string
   end
 end
