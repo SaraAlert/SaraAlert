7501a4799b8b63a6e5aa400856bb756f5a688083
Adam M. Holmes <aholmes@mitre.org>
2020-11-24 17:04:48 -0500

    Adjust role model

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,16 +1,5 @@
 # frozen_string_literal: true
 
-# Role: role model
+# Role
 class Role < ApplicationRecord
-  has_and_belongs_to_many :users, join_table: :users_roles
-
-  belongs_to :resource,
-             polymorphic: true,
-             optional: true
-
-  validates :resource_type,
-            inclusion: { in: Rolify.resource_types },
-            allow_nil: true
-
-  scopify
 end
