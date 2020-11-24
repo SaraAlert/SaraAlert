2ffe5c6ff63af8801060f5127f356ec5384f6869
Matt Mayer <mmayer@mitre.org>
2020-11-24 13:11:25 -0500

    Max_retries_sms will no longer result in another assessment being sent during the reporting period

--- app/jobs/consume_assessments_job.rb
+++ app/jobs/consume_assessments_job.rb
@@ -103,8 +103,6 @@ class ConsumeAssessmentsJob < ApplicationJob
           next
         when 'max_retries_sms'
           # Maximum amount of SMS response retries reached
-          # nil out the last_reminder_sent field so the system will try sending another SMS assessment
-          patient.update(last_assessment_reminder_sent: nil)
           History.contact_attempt(patient: patient, comment: "The system could not record a response because the monitoree exceeded the maximum number
              of daily report SMS response retries via primary telephone number #{patient.primary_telephone}.")
           unless dependents.blank?
