e95c5bf7f46d44d496b7065c831c5d70a82580ed
Becky Hack <rhack@mitre.org>
2020-12-01 08:44:43 -0500

    Separating audits to display one row per change

--- app/javascript/components/admin/AuditModal.js
+++ app/javascript/components/admin/AuditModal.js
@@ -45,84 +45,84 @@ class AuditModal extends React.Component {
     return ts.isValid() ? ts.tz(moment.tz.guess()).format('MM/DD/YYYY HH:mm z') : '';
   };
 
-  renderChange = change => {
+  renderChange = (change, change_details) => {
     return (
       <React.Fragment>
-        {change.jurisdiction_id && (
+        {change === 'jurisdiction_id' && (
           <React.Fragment>
             <b>Jurisdiction:</b>&nbsp;
-            {_.invert(this.props.jurisdiction_paths)[change.jurisdiction_id[0]]}
+            {_.invert(this.props.jurisdiction_paths)[change_details[0]]}
             <i className="mx-2 fas fa-arrow-right"></i>
-            {_.invert(this.props.jurisdiction_paths)[change.jurisdiction_id[1]]}
+            {_.invert(this.props.jurisdiction_paths)[change_details[1]]}
           </React.Fragment>
         )}
-        {change.role && (
+        {change === 'role' && (
           <React.Fragment>
             <b>Role:</b>&nbsp;
-            {_.startCase(change.role[0])}
+            {_.startCase(change_details[0])}
             <i className="mx-2 fas fa-arrow-right"></i>
-            {_.startCase(change.role[1])}
+            {_.startCase(change_details[1])}
           </React.Fragment>
         )}
-        {change.locked_at && (
+        {change === 'locked_at' && (
           <React.Fragment>
             <b>Status:</b>&nbsp;
-            {change.locked_at[0] && (
+            {change_details[0] && (
               <span>
                 Locked<i className="mx-2 fas fa-arrow-right"></i>Unlocked
               </span>
             )}
-            {!change.locked_at[0] && (
+            {!change_details[0] && (
               <span>
                 Unlocked<i className="mx-2 fas fa-arrow-right"></i>Locked
               </span>
             )}
           </React.Fragment>
         )}
-        {change.api_enabled && (
+        {change === 'api_enabled' && (
           <React.Fragment>
             <b>API Enabled:</b>&nbsp;
-            {change.api_enabled[0] && (
+            {change_details[0] && (
               <span>
                 Yes<i className="mx-2 fas fa-arrow-right"></i>No
               </span>
             )}
-            {!change.api_enabled[0] && (
+            {!change_details[0] && (
               <span>
                 No<i className="mx-2 fas fa-arrow-right"></i>Yes
               </span>
             )}
           </React.Fragment>
         )}
-        {change.email && (
+        {change === 'email' && (
           <React.Fragment>
             <b>Email:</b>&nbsp;
-            {change.email[0]}
+            {change_details[0]}
             <i className="mx-2 fas fa-arrow-right"></i>
-            {change.email[1]}
+            {change_details[1]}
           </React.Fragment>
         )}
-        {change.authy_enabled && (
+        {change === 'authy_enabled' && (
           <React.Fragment>
             <b>2FA Enabled:</b>&nbsp;
-            {change.authy_enabled[0] && (
+            {change_details[0] && (
               <span>
                 Yes<i className="mx-2 fas fa-arrow-right"></i>No
               </span>
             )}
-            {!change.authy_enabled[0] && (
+            {!change_details[0] && (
               <span>
                 No<i className="mx-2 fas fa-arrow-right"></i>Yes
               </span>
             )}
           </React.Fragment>
         )}
-        {change.force_password_change && (
+        {change === 'force_password_change' && (
           <React.Fragment>
             <b>Password Changed/Reset</b>
           </React.Fragment>
         )}
-        {change.last_sign_in_with_authy && (
+        {change === 'last_sign_in_with_authy' && (
           <React.Fragment>
             <b>User Signed In</b>
           </React.Fragment>
@@ -135,7 +135,7 @@ class AuditModal extends React.Component {
     return (
       <tr key={`${index}${this.props.user.id}ae`}>
         <td>{event.user}</td>
-        <td>{this.renderChange(event.change)}</td>
+        <td>{this.renderChange(event.change, event.change_details)}</td>
         <td>{this.formatTimestamp(event.when)}</td>
       </tr>
     );
@@ -146,9 +146,10 @@ class AuditModal extends React.Component {
       <React.Fragment>
         <Modal show={this.props.show} onHide={this.hide} dialogClassName="modal-am" aria-labelledby="contained-modal-title-vcenter" centered>
           <Modal.Header closeButton>
-            <Modal.Title>Audit Events for {this.props.user.email}</Modal.Title>
+            <Modal.Title>Audit Events</Modal.Title>
           </Modal.Header>
           <Modal.Body>
+            <b>User: </b> {this.props.user.email}
             {this.state.loading && (
               <div className="text-center">
                 <Spinner variant="secondary" animation="border" size="lg" />
@@ -157,9 +158,9 @@ class AuditModal extends React.Component {
             <Table striped bordered size="sm">
               <thead>
                 <tr>
-                  <th>Who</th>
-                  <th>What</th>
-                  <th>When</th>
+                  <th>Triggered By</th>
+                  <th>Action</th>
+                  <th>Timestamp</th>
                 </tr>
               </thead>
               <tbody>
