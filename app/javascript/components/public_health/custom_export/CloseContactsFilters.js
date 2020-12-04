0dc1db2b872d315bfa7239bc5efb19806800e7e9
Wong, Tim <twong@mitre.org>
2020-12-03 23:56:44 -0800

    uat feedback

--- app/javascript/components/public_health/custom_export/CloseContactsFilters.js
+++ app/javascript/components/public_health/custom_export/CloseContactsFilters.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { PropTypes } from 'prop-types';
 import { Button, Col, Form, InputGroup, Row } from 'react-bootstrap';
 
+import Select from 'react-select';
 import moment from 'moment';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
@@ -14,89 +15,90 @@ class CloseContactsFilters extends React.Component {
 
   render() {
     return (
-      <Form.Group className="mb-0">
-        <Form.Label className="nav-input-label m-1">Select Close Contacts:</Form.Label>
-        <Row className="px-3">
-          <Col md={24} className="my-1 px-1">
-            <InputGroup size="sm">
-              <InputGroup.Prepend>
-                <InputGroup.Text className="rounded-0">
-                  <FontAwesomeIcon icon="plus-square" />
-                  <span className="ml-1">Enrolled</span>
-                </InputGroup.Text>
-              </InputGroup.Prepend>
-              <Button
-                size="sm"
-                variant={this.props.query?.enrolled === undefined ? 'primary' : 'outline-secondary'}
-                style={{ outline: 'none', boxShadow: 'none' }}
-                onClick={() => this.props.onQueryChange('enrolled', undefined)}
-                disabled={this.props.disabled}>
-                All
-              </Button>
-              <Button
-                size="sm"
-                variant={this.props.query?.enrolled === true ? 'primary' : 'outline-secondary'}
-                style={{ outline: 'none', boxShadow: 'none' }}
-                onClick={() => this.props.onQueryChange('enrolled', true)}
-                disabled={this.props.disabled}>
-                Yes
-              </Button>
-              <Button
-                size="sm"
-                variant={this.props.query?.enrolled === false ? 'primary' : 'outline-secondary'}
-                style={{ outline: 'none', boxShadow: 'none' }}
-                onClick={() => this.props.onQueryChange('enrolled', false)}
-                disabled={this.props.disabled}>
-                No
-              </Button>
-            </InputGroup>
-          </Col>
-          <Col lg={12} className="my-1 px-1">
-            <InputGroup size="sm">
-              <InputGroup.Prepend>
-                <InputGroup.Text className="rounded-0">
-                  <FontAwesomeIcon icon="calendar-alt" />
-                  <span className="ml-1">Created After</span>
-                </InputGroup.Text>
-              </InputGroup.Prepend>
-              <DateInput
-                date={this.props.query?.created_at_after}
-                minDate={'2020-01-01'}
-                maxDate={moment()
-                  .add(30, 'days')
-                  .format('YYYY-MM-DD')}
-                onChange={date => this.props.onQueryChange('created_at_after', date)}
-                placement="bottom"
-                isClearable
-                customClass="form-control-sm"
-                disabled={this.props.disabled}
-              />
-            </InputGroup>
-          </Col>
-          <Col lg={12} className="my-1 px-1">
-            <InputGroup size="sm">
-              <InputGroup.Prepend>
-                <InputGroup.Text className="rounded-0">
-                  <FontAwesomeIcon icon="calendar-alt" />
-                  <span className="ml-1">Created Before</span>
-                </InputGroup.Text>
-              </InputGroup.Prepend>
-              <DateInput
-                date={this.props.query?.created_at_before}
-                minDate={'2020-01-01'}
-                maxDate={moment()
-                  .add(30, 'days')
-                  .format('YYYY-MM-DD')}
-                onChange={date => this.props.onQueryChange('created_at_before', date)}
-                placement="bottom"
-                isClearable
-                customClass="form-control-sm"
-                disabled={this.props.disabled}
-              />
-            </InputGroup>
-          </Col>
-        </Row>
-      </Form.Group>
+      <Select placeholder="Filter close contacts by..." />
+      // <Form.Group className="mb-0">
+      //   <Form.Label className="nav-input-label m-1">Select Close Contacts:</Form.Label>
+      //   <Row className="px-3">
+      //     <Col md={24} className="my-1 px-1">
+      //       <InputGroup size="sm">
+      //         <InputGroup.Prepend>
+      //           <InputGroup.Text className="rounded-0">
+      //             <FontAwesomeIcon icon="plus-square" />
+      //             <span className="ml-1">Enrolled</span>
+      //           </InputGroup.Text>
+      //         </InputGroup.Prepend>
+      //         <Button
+      //           size="sm"
+      //           variant={this.props.query?.enrolled === undefined ? 'primary' : 'outline-secondary'}
+      //           style={{ outline: 'none', boxShadow: 'none' }}
+      //           onClick={() => this.props.onQueryChange('enrolled', undefined)}
+      //           disabled={this.props.disabled}>
+      //           All
+      //         </Button>
+      //         <Button
+      //           size="sm"
+      //           variant={this.props.query?.enrolled === true ? 'primary' : 'outline-secondary'}
+      //           style={{ outline: 'none', boxShadow: 'none' }}
+      //           onClick={() => this.props.onQueryChange('enrolled', true)}
+      //           disabled={this.props.disabled}>
+      //           Yes
+      //         </Button>
+      //         <Button
+      //           size="sm"
+      //           variant={this.props.query?.enrolled === false ? 'primary' : 'outline-secondary'}
+      //           style={{ outline: 'none', boxShadow: 'none' }}
+      //           onClick={() => this.props.onQueryChange('enrolled', false)}
+      //           disabled={this.props.disabled}>
+      //           No
+      //         </Button>
+      //       </InputGroup>
+      //     </Col>
+      //     <Col lg={12} className="my-1 px-1">
+      //       <InputGroup size="sm">
+      //         <InputGroup.Prepend>
+      //           <InputGroup.Text className="rounded-0">
+      //             <FontAwesomeIcon icon="calendar-alt" />
+      //             <span className="ml-1">Created After</span>
+      //           </InputGroup.Text>
+      //         </InputGroup.Prepend>
+      //         <DateInput
+      //           date={this.props.query?.created_at_after}
+      //           minDate={'2020-01-01'}
+      //           maxDate={moment()
+      //             .add(30, 'days')
+      //             .format('YYYY-MM-DD')}
+      //           onChange={date => this.props.onQueryChange('created_at_after', date)}
+      //           placement="bottom"
+      //           isClearable
+      //           customClass="form-control-sm"
+      //           disabled={this.props.disabled}
+      //         />
+      //       </InputGroup>
+      //     </Col>
+      //     <Col lg={12} className="my-1 px-1">
+      //       <InputGroup size="sm">
+      //         <InputGroup.Prepend>
+      //           <InputGroup.Text className="rounded-0">
+      //             <FontAwesomeIcon icon="calendar-alt" />
+      //             <span className="ml-1">Created Before</span>
+      //           </InputGroup.Text>
+      //         </InputGroup.Prepend>
+      //         <DateInput
+      //           date={this.props.query?.created_at_before}
+      //           minDate={'2020-01-01'}
+      //           maxDate={moment()
+      //             .add(30, 'days')
+      //             .format('YYYY-MM-DD')}
+      //           onChange={date => this.props.onQueryChange('created_at_before', date)}
+      //           placement="bottom"
+      //           isClearable
+      //           customClass="form-control-sm"
+      //           disabled={this.props.disabled}
+      //         />
+      //       </InputGroup>
+      //     </Col>
+      //   </Row>
+      // </Form.Group>
     );
   }
 }
--- app/javascript/components/public_health/custom_export/LaboratoriesFilters.js
+++ app/javascript/components/public_health/custom_export/LaboratoriesFilters.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { PropTypes } from 'prop-types';
 import { Col, Form, InputGroup, Row } from 'react-bootstrap';
 
+import Select from 'react-select';
 import moment from 'moment';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
@@ -14,133 +15,134 @@ class LaboratoriesFilters extends React.Component {
 
   render() {
     return (
-      <Form.Group className="mb-0">
-        <Form.Label className="nav-input-label m-1">Select Lab Results:</Form.Label>
-        <Row className="px-3">
-          <Col md={12} className="my-1 px-1">
-            <InputGroup size="sm">
-              <InputGroup.Prepend>
-                <InputGroup.Text className="rounded-0">
-                  <FontAwesomeIcon icon="vial" />
-                  <span className="ml-1">Type</span>
-                </InputGroup.Text>
-              </InputGroup.Prepend>
-              <Form.Control
-                autoComplete="off"
-                size="sm"
-                value={this.props.query?.lab_type || ''}
-                onChange={event => this.props.onQueryChange('lab_type', event?.target?.value)}
-                disabled={this.props.disabled}
-              />
-            </InputGroup>
-          </Col>
-          <Col md={12} className="my-1 px-1">
-            <InputGroup size="sm">
-              <InputGroup.Prepend>
-                <InputGroup.Text className="rounded-0">
-                  <FontAwesomeIcon icon="poll" />
-                  <span className="ml-1">Result</span>
-                </InputGroup.Text>
-              </InputGroup.Prepend>
-              <Form.Control
-                autoComplete="off"
-                size="sm"
-                value={this.props.query?.result || ''}
-                onChange={event => this.props.onQueryChange('result', event?.target?.value)}
-                disabled={this.props.disabled}
-              />
-            </InputGroup>
-          </Col>
-          <Col lg={12} className="my-1 px-1">
-            <InputGroup size="sm">
-              <InputGroup.Prepend>
-                <InputGroup.Text className="rounded-0">
-                  <FontAwesomeIcon icon="calendar-alt" />
-                  <span className="ml-1">Collected After</span>
-                </InputGroup.Text>
-              </InputGroup.Prepend>
-              <DateInput
-                date={this.props.query?.specimen_collection_after}
-                minDate={'2020-01-01'}
-                maxDate={moment()
-                  .add(30, 'days')
-                  .format('YYYY-MM-DD')}
-                onChange={date => this.props.onQueryChange('specimen_collection_after', date)}
-                placement="bottom"
-                isClearable
-                customClass="form-control-sm"
-                disabled={this.props.disabled}
-              />
-            </InputGroup>
-          </Col>
-          <Col lg={12} className="my-1 px-1">
-            <InputGroup size="sm">
-              <InputGroup.Prepend>
-                <InputGroup.Text className="rounded-0">
-                  <FontAwesomeIcon icon="calendar-alt" />
-                  <span className="ml-1">Collected Before</span>
-                </InputGroup.Text>
-              </InputGroup.Prepend>
-              <DateInput
-                date={this.props.query?.specimen_collection_before}
-                minDate={'2020-01-01'}
-                maxDate={moment()
-                  .add(30, 'days')
-                  .format('YYYY-MM-DD')}
-                onChange={date => this.props.onQueryChange('specimen_collection_before', date)}
-                placement="bottom"
-                isClearable
-                customClass="form-control-sm"
-                disabled={this.props.disabled}
-              />
-            </InputGroup>
-          </Col>
-          <Col lg={12} className="my-1 px-1">
-            <InputGroup size="sm">
-              <InputGroup.Prepend>
-                <InputGroup.Text className="rounded-0">
-                  <FontAwesomeIcon icon="calendar-alt" />
-                  <span className="ml-1">Reported After</span>
-                </InputGroup.Text>
-              </InputGroup.Prepend>
-              <DateInput
-                date={this.props.query?.report_after}
-                minDate={'2020-01-01'}
-                maxDate={moment()
-                  .add(30, 'days')
-                  .format('YYYY-MM-DD')}
-                onChange={date => this.props.onQueryChange('report_after', date)}
-                placement="bottom"
-                isClearable
-                customClass="form-control-sm"
-                disabled={this.props.disabled}
-              />
-            </InputGroup>
-          </Col>
-          <Col lg={12} className="my-1 px-1">
-            <InputGroup size="sm">
-              <InputGroup.Prepend>
-                <InputGroup.Text className="rounded-0">
-                  <FontAwesomeIcon icon="calendar-alt" />
-                  <span className="ml-1">Reported Before</span>
-                </InputGroup.Text>
-              </InputGroup.Prepend>
-              <DateInput
-                date={this.props.query?.report_before}
-                minDate={'2020-01-01'}
-                maxDate={moment()
-                  .add(30, 'days')
-                  .format('YYYY-MM-DD')}
-                onChange={date => this.props.onQueryChange('report_before', date)}
-                placement="bottom"
-                isClearable
-                customClass="form-control-sm"
-                disabled={this.props.disabled}
-              />
-            </InputGroup>
-          </Col>
-        </Row>
-      </Form.Group>
+      <Select placeholder="Filter lab results by..." />
+      // <Form.Group className="mb-0">
+      //   <Form.Label className="nav-input-label m-1">Select Lab Results:</Form.Label>
+      //   <Row className="px-3">
+      //     <Col md={12} className="my-1 px-1">
+      //       <InputGroup size="sm">
+      //         <InputGroup.Prepend>
+      //           <InputGroup.Text className="rounded-0">
+      //             <FontAwesomeIcon icon="vial" />
+      //             <span className="ml-1">Type</span>
+      //           </InputGroup.Text>
+      //         </InputGroup.Prepend>
+      //         <Form.Control
+      //           autoComplete="off"
+      //           size="sm"
+      //           value={this.props.query?.lab_type || ''}
+      //           onChange={event => this.props.onQueryChange('lab_type', event?.target?.value)}
+      //           disabled={this.props.disabled}
+      //         />
+      //       </InputGroup>
+      //     </Col>
+      //     <Col md={12} className="my-1 px-1">
+      //       <InputGroup size="sm">
+      //         <InputGroup.Prepend>
+      //           <InputGroup.Text className="rounded-0">
+      //             <FontAwesomeIcon icon="poll" />
+      //             <span className="ml-1">Result</span>
+      //           </InputGroup.Text>
+      //         </InputGroup.Prepend>
+      //         <Form.Control
+      //           autoComplete="off"
+      //           size="sm"
+      //           value={this.props.query?.result || ''}
+      //           onChange={event => this.props.onQueryChange('result', event?.target?.value)}
+      //           disabled={this.props.disabled}
+      //         />
+      //       </InputGroup>
+      //     </Col>
+      //     <Col lg={12} className="my-1 px-1">
+      //       <InputGroup size="sm">
+      //         <InputGroup.Prepend>
+      //           <InputGroup.Text className="rounded-0">
+      //             <FontAwesomeIcon icon="calendar-alt" />
+      //             <span className="ml-1">Collected After</span>
+      //           </InputGroup.Text>
+      //         </InputGroup.Prepend>
+      //         <DateInput
+      //           date={this.props.query?.specimen_collection_after}
+      //           minDate={'2020-01-01'}
+      //           maxDate={moment()
+      //             .add(30, 'days')
+      //             .format('YYYY-MM-DD')}
+      //           onChange={date => this.props.onQueryChange('specimen_collection_after', date)}
+      //           placement="bottom"
+      //           isClearable
+      //           customClass="form-control-sm"
+      //           disabled={this.props.disabled}
+      //         />
+      //       </InputGroup>
+      //     </Col>
+      //     <Col lg={12} className="my-1 px-1">
+      //       <InputGroup size="sm">
+      //         <InputGroup.Prepend>
+      //           <InputGroup.Text className="rounded-0">
+      //             <FontAwesomeIcon icon="calendar-alt" />
+      //             <span className="ml-1">Collected Before</span>
+      //           </InputGroup.Text>
+      //         </InputGroup.Prepend>
+      //         <DateInput
+      //           date={this.props.query?.specimen_collection_before}
+      //           minDate={'2020-01-01'}
+      //           maxDate={moment()
+      //             .add(30, 'days')
+      //             .format('YYYY-MM-DD')}
+      //           onChange={date => this.props.onQueryChange('specimen_collection_before', date)}
+      //           placement="bottom"
+      //           isClearable
+      //           customClass="form-control-sm"
+      //           disabled={this.props.disabled}
+      //         />
+      //       </InputGroup>
+      //     </Col>
+      //     <Col lg={12} className="my-1 px-1">
+      //       <InputGroup size="sm">
+      //         <InputGroup.Prepend>
+      //           <InputGroup.Text className="rounded-0">
+      //             <FontAwesomeIcon icon="calendar-alt" />
+      //             <span className="ml-1">Reported After</span>
+      //           </InputGroup.Text>
+      //         </InputGroup.Prepend>
+      //         <DateInput
+      //           date={this.props.query?.report_after}
+      //           minDate={'2020-01-01'}
+      //           maxDate={moment()
+      //             .add(30, 'days')
+      //             .format('YYYY-MM-DD')}
+      //           onChange={date => this.props.onQueryChange('report_after', date)}
+      //           placement="bottom"
+      //           isClearable
+      //           customClass="form-control-sm"
+      //           disabled={this.props.disabled}
+      //         />
+      //       </InputGroup>
+      //     </Col>
+      //     <Col lg={12} className="my-1 px-1">
+      //       <InputGroup size="sm">
+      //         <InputGroup.Prepend>
+      //           <InputGroup.Text className="rounded-0">
+      //             <FontAwesomeIcon icon="calendar-alt" />
+      //             <span className="ml-1">Reported Before</span>
+      //           </InputGroup.Text>
+      //         </InputGroup.Prepend>
+      //         <DateInput
+      //           date={this.props.query?.report_before}
+      //           minDate={'2020-01-01'}
+      //           maxDate={moment()
+      //             .add(30, 'days')
+      //             .format('YYYY-MM-DD')}
+      //           onChange={date => this.props.onQueryChange('report_before', date)}
+      //           placement="bottom"
+      //           isClearable
+      //           customClass="form-control-sm"
+      //           disabled={this.props.disabled}
+      //         />
+      //       </InputGroup>
+      //     </Col>
+      //   </Row>
+      // </Form.Group>
     );
   }
 }
