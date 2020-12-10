01e4f8b4fb1568d93c145d8d39c92221d1959503
Adam M. Holmes <aholmes@mitre.org>
2020-12-10 15:13:54 -0500

    Tweak digest job

--- app/jobs/send_patient_digest_job.rb
+++ app/jobs/send_patient_digest_job.rb
@@ -6,9 +6,7 @@ class SendPatientDigestJob < ApplicationJob
 
   def perform(*_args)
     # Loop over jurisdictions
-    Jurisdiction.find_each do |jur|
-      next unless jur.send_digest
-
+    Jurisdiction.where(send_digest: true) do |jur|
       # Grab patients who reported symtomatic in the last hour
       patients = jur.all_patients.recently_symptomatic
 
@@ -17,9 +15,9 @@ class SendPatientDigestJob < ApplicationJob
 
       # Grab users who need an email
       users = User.where(jurisdiction_id: jur.id, role: %w[super_user public_health public_health_enroller])
-      users.find_each do |user|
+      users.each do |user|
         # Send email to this user
-        UserMailer.send_patient_digest_job_email(patients, user).deliver_now
+        UserMailer.send_patient_digest_job_email(patients, user).deliver_later
       end
     end
   end
