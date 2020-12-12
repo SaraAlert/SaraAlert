467062f4a66af38e95831f15c935fb5955f2691b
Nick Freiter <nfreiter@mitre.org>
2020-12-12 13:19:24 -0500

    Refactor phone number validation out of import_controller

--- app/models/concerns/phone_number_validator.rb
+++ app/models/concerns/phone_number_validator.rb
@@ -6,7 +6,7 @@ class PhoneNumberValidator < ActiveModel::EachValidator
     return if value.blank?
 
     phone = Phonelib.parse(value, 'US')
-    return unless phone.national(false).nil? || phone.national(false).length != 10
+    return unless phone.e164.blank? || phone.e164.sub(/^\+1/, '').length != 10
 
     record.errors.add(attribute, 'is not a valid phone number')
   end
