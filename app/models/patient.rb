f0f8f23348c9ab5fa2e069703f5f8d89556423c7
Nick Freiter <nfreiter@mitre.org>
2020-12-14 10:33:48 -0500

    Refactor assigned user validation out of import_controller

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -77,7 +77,11 @@ class Patient < ApplicationRecord
 
   validates :email, email: true
 
-  validates :assigned_user, numericality: { only_integer: true, allow_nil: true, greater_than: 0, less_than_or_equal_to: 999_999 }
+  validates :assigned_user, numericality: { only_integer: true,
+                                            allow_nil: true,
+                                            greater_than: 0,
+                                            less_than_or_equal_to: 999_999,
+                                            message: 'is not valid, acceptable values are numbers between 1-9999' }
 
   validates_with PrimaryContactValidator
 
