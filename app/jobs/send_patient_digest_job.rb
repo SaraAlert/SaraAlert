3d0c293faa3ee6d53d24236cad6c0ac83f1bf82f
Moses, Kylie T <ktmoses@mitre.org>
2020-12-15 14:15:20 -0500

    Rubocop

--- app/jobs/send_patient_digest_job.rb
+++ app/jobs/send_patient_digest_job.rb
@@ -21,7 +21,7 @@ class SendPatientDigestJob < ApplicationJob
       users.each do |user|
         # Send email to this user
         UserMailer.send_patient_digest_job_email(patients, user).deliver_later
-        sent << {id: user.id, jur_id: jur.id, user_jur_id: user.jurisdiction_id}
+        sent << { id: user.id, jur_id: jur.id, user_jur_id: user.jurisdiction_id }
       end
     end
 
