3b57076267710d71dbdcc08f3f3a59bd761e1b24
Kyle Fagan <kfagan@mitre.org>
2020-12-16 14:40:38 -0500

    Add redis cache store configuration to log errors to Sentry and connect to a separate redis instance

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,7 +54,12 @@ Rails.application.configure do
   config.log_tags = [ :request_id ]
 
   # Use a different cache store in production.
-  config.cache_store = :redis_cache_store
+  config.cache_store = :redis_cache_store, {
+    url: ENV['REDIS_CACHE_URL']
+    error_handler: -> (method:, returning:, exception:) {
+      Raven.capture_exception(exception, level: 'warning', tags: { method: method, returning: returning })
+    }
+  }
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
