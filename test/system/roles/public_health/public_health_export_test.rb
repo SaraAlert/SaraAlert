308037702cc075a2d59cad5eb8dee487376f07ec
Moses, Kylie T <ktmoses@mitre.org>
2021-01-03 14:22:42 -0500

    Created ENV variables for export batch sizes, separated out export and import system tests, added system tests for export batching.
    Rubocop and removing unused variable.
    
    Fixing workflow after changing test names.

---
+++ test/system/roles/public_health/public_health_export_test.rb
@@ -0,0 +1,97 @@
+# frozen_string_literal: true
+
+require 'application_system_test_case'
+
+SimpleCov.command_name 'SystemTestCasePublicHealthExport'
+
+require_relative 'public_health_test_helper'
+require_relative '../../lib/system_test_utils'
+
+class PublicHealthImportExportTest < ApplicationSystemTestCase
+  @@public_health_test_helper = PublicHealthTestHelper.new(nil)
+  @@system_test_utils = SystemTestUtils.new(nil)
+
+  test 'export line list csv (exposure)' do
+    @@public_health_test_helper.export_csv_linelist('locals2c3_epi', :exposure, :cancel)
+    @@public_health_test_helper.export_csv_linelist('state1_epi', :exposure, :export)
+  end
+
+  test 'export line list csv (isolation)' do
+    @@public_health_test_helper.export_csv_linelist('locals2c4_epi', :isolation, :cancel)
+    @@public_health_test_helper.export_csv_linelist('state1_epi_enroller', :isolation, :export)
+  end
+
+  test 'export sara alert format (exposure)' do
+    @@public_health_test_helper.export_sara_alert_format('locals1c1_epi', :exposure, :cancel)
+    @@public_health_test_helper.export_sara_alert_format('state1_epi_enroller', :exposure, :export)
+  end
+
+  test 'export sara alert format (isolation)' do
+    @@public_health_test_helper.export_sara_alert_format('locals2c3_epi', :isolation, :cancel)
+    @@public_health_test_helper.export_sara_alert_format('state1_epi', :isolation, :export)
+  end
+
+  test 'export full history purge-eligible monitorees' do
+    @@public_health_test_helper.export_full_history_patients('state1_epi_enroller', :isolation, :cancel, :purgeable)
+    @@public_health_test_helper.export_full_history_patients('state1_epi', :exposure, :export, :purgeable)
+  end
+
+  test 'export full history all monitorees' do
+    @@public_health_test_helper.export_full_history_patients('locals1c1_epi', :exposure, :cancel, :all)
+    @@public_health_test_helper.export_full_history_patients('state1_epi', :isolation, :export, :all)
+  end
+
+  test 'export full history single monitoree' do
+    @@public_health_test_helper.export_full_history_patient('locals2c4_epi', 'patient_10')
+  end
+
+  # ---- Test the same exports but with smaller batching so that the batchig functionality can be tested ----
+
+  test 'export line list csv (exposure) with batching' do
+    ENV['EXPORT_OUTER_BATCH_SIZE'] = '10'
+    ENV['EXPORT_INNER_BATCH_SIZE'] = '2'
+    @@public_health_test_helper.export_csv_linelist('locals2c3_epi', :exposure, :cancel)
+    @@public_health_test_helper.export_csv_linelist('state1_epi', :exposure, :export)
+  end
+
+  test 'export line list csv (isolation) with batching' do
+    ENV['EXPORT_OUTER_BATCH_SIZE'] = '10'
+    ENV['EXPORT_INNER_BATCH_SIZE'] = '2'
+    @@public_health_test_helper.export_csv_linelist('locals2c4_epi', :isolation, :cancel)
+    @@public_health_test_helper.export_csv_linelist('state1_epi_enroller', :isolation, :export)
+  end
+
+  test 'export sara alert format (exposure) with batching' do
+    ENV['EXPORT_OUTER_BATCH_SIZE'] = '10'
+    ENV['EXPORT_INNER_BATCH_SIZE'] = '2'
+    @@public_health_test_helper.export_sara_alert_format('locals1c1_epi', :exposure, :cancel)
+    @@public_health_test_helper.export_sara_alert_format('state1_epi_enroller', :exposure, :export)
+  end
+
+  test 'export sara alert format (isolation) with batching' do
+    ENV['EXPORT_OUTER_BATCH_SIZE'] = '10'
+    ENV['EXPORT_INNER_BATCH_SIZE'] = '2'
+    @@public_health_test_helper.export_sara_alert_format('locals2c3_epi', :isolation, :cancel)
+    @@public_health_test_helper.export_sara_alert_format('state1_epi', :isolation, :export)
+  end
+
+  test 'export full history purge-eligible monitorees with batching' do
+    ENV['EXPORT_OUTER_BATCH_SIZE'] = '10'
+    ENV['EXPORT_INNER_BATCH_SIZE'] = '2'
+    @@public_health_test_helper.export_full_history_patients('state1_epi_enroller', :isolation, :cancel, :purgeable)
+    @@public_health_test_helper.export_full_history_patients('state1_epi', :exposure, :export, :purgeable)
+  end
+
+  test 'export full history all monitorees with batching' do
+    ENV['EXPORT_OUTER_BATCH_SIZE'] = '10'
+    ENV['EXPORT_INNER_BATCH_SIZE'] = '2'
+    @@public_health_test_helper.export_full_history_patients('locals1c1_epi', :exposure, :cancel, :all)
+    @@public_health_test_helper.export_full_history_patients('state1_epi', :isolation, :export, :all)
+  end
+
+  test 'export full history single monitoree with batching' do
+    ENV['EXPORT_OUTER_BATCH_SIZE'] = '10'
+    ENV['EXPORT_INNER_BATCH_SIZE'] = '2'
+    @@public_health_test_helper.export_full_history_patient('locals2c4_epi', 'patient_10')
+  end
+end
