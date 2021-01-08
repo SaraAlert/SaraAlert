e2f933f66230dd6b237acd0901263c3b54301f8d
Moses, Kylie T <ktmoses@mitre.org>
2021-01-07 21:32:58 -0500

    Adding back reorder before batching to guarantee order. Testing monitoree details in custom export system tests.

--- test/system/roles/public_health/public_health_custom_export_test.rb
+++ test/system/roles/public_health/public_health_custom_export_test.rb
@@ -8,36 +8,67 @@ require_relative 'public_health_test_helper'
 require_relative '../../lib/system_test_utils'
 
 class PublicHealthCustomExportTest < ApplicationSystemTestCase
+  include ImportExport
+
   @@public_health_test_helper = PublicHealthTestHelper.new(nil)
   @@system_test_utils = SystemTestUtils.new(nil)
+
+  def setup
+    # Reset ENV variables and reload export job file which has constants dependent on these ENV variables
+    ENV['EXPORT_OUTER_BATCH_SIZE'] = '10_000'
+    ENV['EXPORT_INNER_BATCH_SIZE'] = '500'
+    load 'app/jobs/export_job.rb'
+  end
+
+  # Recursively grabs all the "leaf" field options in custom export
+  def get_all_field_options(options)
+    leaf_values = []
+    options.each do |option|
+      if option[:children].present?
+        leaf_values.concat(get_all_field_options(option[:children]))
+      else
+        leaf_values << option[:value].to_sym
+      end
+    end
+    leaf_values
+  end
+
+  # Gets all the patient field options for custom export.
+  def all_custom_export_patient_fields
+    # NOTE: Doesn't use PATIENT_FIELD_NAMES as that contains options not available to custom export
+    options_tree = ImportExport::PATIENTS_EXPORT_OPTIONS
+    get_all_field_options(options_tree[:nodes])
+  end
 
   test 'export xlsx format with all monitorees with all data types with all fields' do
     settings = {
       records: :all,
-      elements: {
-        patients: {
-          checked: ['Monitoree Details']
-        },
-        assessments: {
-          checked: ['Reports']
-        },
-        laboratories: {
-          checked: ['Lab Results']
-        },
-        close_contacts: {
-          checked: ['Close Contacts']
-        },
-        transfers: {
-          checked: ['Transfers']
-        },
-        histories: {
-          checked: ['History']
-        }
-      },
       name: 'Excel export all',
       format: :xlsx,
       actions: %i[save export],
-      confirm: :start
+      confirm: :start,
+      data: {
+        patients: {
+          selected: ['Monitoree Details'],
+          checked: all_custom_export_patient_fields,
+          query: { 'workflow' => 'all', 'tab' => 'all', 'jurisdiction' => 1, 'scope' => 'all', 'user' => nil, 'search' => '', 'tz_offset' => 300 }
+        },
+        assessments: {
+          selected: ['Reports']
+        },
+        laboratories: {
+          selected: ['Lab Results']
+        },
+        close_contacts: {
+          selected: ['Close Contacts']
+        },
+        transfers: {
+          selected: ['Transfers']
+        },
+        histories: {
+          selected: ['History']
+        }
+      }
     }
     @@public_health_test_helper.export_custom('state1_epi', settings)
   end
