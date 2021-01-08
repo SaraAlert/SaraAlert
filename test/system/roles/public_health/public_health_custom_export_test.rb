09ca7724840db5d23dc55677b1b9ef087be80b23
Moses, Kylie T <ktmoses@mitre.org>
2021-01-08 16:45:02 -0500

    Fixing and updating custom export system tests.

--- test/system/roles/public_health/public_health_custom_export_test.rb
+++ test/system/roles/public_health/public_health_custom_export_test.rb
@@ -20,6 +20,16 @@ class PublicHealthCustomExportTest < ApplicationSystemTestCase
     load 'app/jobs/export_job.rb'
   end
 
+  def teardown
+    # Reset ENV variables and reload export job file which has constants dependent on these ENV variables
+    ENV['EXPORT_OUTER_BATCH_SIZE'] = nil
+    ENV['EXPORT_INNER_BATCH_SIZE'] = nil
+    load 'app/jobs/export_job.rb'
+
+    # Remove any files downloaded for each test
+    FileUtils.rm_rf(Rails.root.join('tmp/downloads'))
+  end
+
   # Recursively grabs all the "leaf" field options in custom export
   def get_all_field_options(options)
     leaf_values = []
@@ -40,6 +50,103 @@ class PublicHealthCustomExportTest < ApplicationSystemTestCase
     get_all_field_options(options_tree[:nodes])
   end
 
+  # Gets all the assessment field options for custom export.
+  def all_custom_export_assessment_fields
+    options_tree = ImportExport::ASSESSMENTS_EXPORT_OPTIONS
+    get_all_field_options(options_tree[:nodes])
+  end
+
+  # TODO: Tests for custom query option
+  # TODO: Tests for some options not being checked
+
+  test 'export xlsx format with monitorees on current exposure symptomatic dashboard with all data types with all fields' do
+    settings = {
+      records: :current,
+      workflow: :exposure,
+      tab: :symptomatic,
+      name: 'Excel export all',
+      format: :xlsx,
+      actions: %i[save export],
+      confirm: :start,
+      data: {
+        patients: {
+          selected: ['Monitoree Details'],
+          checked: all_custom_export_patient_fields,
+          query: {
+            workflow: 'exposure',
+            tab: 'symptomatic',
+            jurisdiction: 2,
+            scope: 'all',
+            user: nil,
+            search: '',
+            tz_offset: 300
+          }
+        },
+        assessments: {
+          selected: ['Reports'],
+          checked: all_custom_export_assessment_fields
+        },
+        laboratories: {
+          selected: ['Lab Results']
+        },
+        close_contacts: {
+          selected: ['Close Contacts']
+        },
+        transfers: {
+          selected: ['Transfers']
+        },
+        histories: {
+          selected: ['History']
+        }
+      }
+    }
+    @@public_health_test_helper.export_custom('state1_epi', settings)
+  end
+
+  test 'export xlsx format with monitorees on current isolation requires review dashboard with all data types with all fields' do
+    settings = {
+      records: :current,
+      workflow: :isolation,
+      tab: :requiring_review,
+      name: 'Excel export all',
+      format: :xlsx,
+      actions: %i[save export],
+      confirm: :start,
+      data: {
+        patients: {
+          selected: ['Monitoree Details'],
+          checked: all_custom_export_patient_fields,
+          query: {
+            workflow: 'isolation',
+            tab: 'requiring_review',
+            jurisdiction: 2,
+            scope: 'all',
+            user: nil,
+            search: '',
+            tz_offset: 300
+          }
+        },
+        assessments: {
+          selected: ['Reports'],
+          checked: all_custom_export_assessment_fields
+        },
+        laboratories: {
+          selected: ['Lab Results']
+        },
+        close_contacts: {
+          selected: ['Close Contacts']
+        },
+        transfers: {
+          selected: ['Transfers']
+        },
+        histories: {
+          selected: ['History']
+        }
+      }
+    }
+    @@public_health_test_helper.export_custom('state1_epi', settings)
+  end
+
   test 'export xlsx format with all monitorees with all data types with all fields' do
     settings = {
       records: :all,
@@ -51,10 +158,163 @@ class PublicHealthCustomExportTest < ApplicationSystemTestCase
         patients: {
           selected: ['Monitoree Details'],
           checked: all_custom_export_patient_fields,
-          query: { 'workflow' => 'all', 'tab' => 'all', 'jurisdiction' => 1, 'scope' => 'all', 'user' => nil, 'search' => '', 'tz_offset' => 300 }
+          query: {
+            workflow: 'all',
+            tab: 'all',
+            jurisdiction: 1,
+            scope: 'all',
+            user: nil,
+            search: '',
+            tz_offset: 300
+          }
         },
         assessments: {
-          selected: ['Reports']
+          selected: ['Reports'],
+          checked: all_custom_export_assessment_fields
+        },
+        laboratories: {
+          selected: ['Lab Results']
+        },
+        close_contacts: {
+          selected: ['Close Contacts']
+        },
+        transfers: {
+          selected: ['Transfers']
+        },
+        histories: {
+          selected: ['History']
+        }
+      }
+    }
+    @@public_health_test_helper.export_custom('state1_epi', settings)
+  end
+
+  # ---- Test the same exports but with smaller batching so that the batching functionality can be tested ----
+
+  test 'export xlsx format with monitorees on current exposure symptomatic dashboard with all data types with all fields with batching' do
+    ENV['EXPORT_OUTER_BATCH_SIZE'] = '10'
+    ENV['EXPORT_INNER_BATCH_SIZE'] = '2'
+    load 'app/jobs/export_job.rb'
+
+    settings = {
+      records: :current,
+      workflow: :exposure,
+      tab: :symptomatic,
+      name: 'Excel export all',
+      format: :xlsx,
+      actions: %i[save export],
+      confirm: :start,
+      data: {
+        patients: {
+          selected: ['Monitoree Details'],
+          checked: all_custom_export_patient_fields,
+          query: {
+            workflow: 'exposure',
+            tab: 'symptomatic',
+            jurisdiction: 2,
+            scope: 'all',
+            user: nil,
+            search: '',
+            tz_offset: 300
+          }
+        },
+        assessments: {
+          selected: ['Reports'],
+          checked: all_custom_export_assessment_fields
+        },
+        laboratories: {
+          selected: ['Lab Results']
+        },
+        close_contacts: {
+          selected: ['Close Contacts']
+        },
+        transfers: {
+          selected: ['Transfers']
+        },
+        histories: {
+          selected: ['History']
+        }
+      }
+    }
+    @@public_health_test_helper.export_custom('state1_epi', settings)
+  end
+
+  test 'export xlsx format with monitorees on current isolation requires review dashboard with all data types with all fields with batching' do
+    ENV['EXPORT_OUTER_BATCH_SIZE'] = '10'
+    ENV['EXPORT_INNER_BATCH_SIZE'] = '2'
+    load 'app/jobs/export_job.rb'
+
+    settings = {
+      records: :current,
+      workflow: :isolation,
+      tab: :requiring_review,
+      name: 'Excel export all',
+      format: :xlsx,
+      actions: %i[save export],
+      confirm: :start,
+      data: {
+        patients: {
+          selected: ['Monitoree Details'],
+          checked: all_custom_export_patient_fields,
+          query: {
+            workflow: 'isolation',
+            tab: 'requiring_review',
+            jurisdiction: 2,
+            scope: 'all',
+            user: nil,
+            search: '',
+            tz_offset: 300
+          }
+        },
+        assessments: {
+          selected: ['Reports'],
+          checked: all_custom_export_assessment_fields
+        },
+        laboratories: {
+          selected: ['Lab Results']
+        },
+        close_contacts: {
+          selected: ['Close Contacts']
+        },
+        transfers: {
+          selected: ['Transfers']
+        },
+        histories: {
+          selected: ['History']
+        }
+      }
+    }
+    @@public_health_test_helper.export_custom('state1_epi', settings)
+  end
+
+  test 'export xlsx format with all monitorees with all data types with all fields with batching' do
+    ENV['EXPORT_OUTER_BATCH_SIZE'] = '10'
+    ENV['EXPORT_INNER_BATCH_SIZE'] = '2'
+    load 'app/jobs/export_job.rb'
+
+    settings = {
+      records: :all,
+      name: 'Excel export all',
+      format: :xlsx,
+      actions: %i[save export],
+      confirm: :start,
+      data: {
+        patients: {
+          selected: ['Monitoree Details'],
+          checked: all_custom_export_patient_fields,
+          query: {
+            workflow: 'all',
+            tab: 'all',
+            jurisdiction: 1,
+            scope: 'all',
+            user: nil,
+            search: '',
+            tz_offset: 300
+          }
+        },
+        assessments: {
+          selected: ['Reports'],
+          checked: all_custom_export_assessment_fields
         },
         laboratories: {
           selected: ['Lab Results']
