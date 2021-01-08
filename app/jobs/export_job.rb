fa184cb6582aeaf30eae71271c47c0c6b81da983
Moses, Kylie T <ktmoses@mitre.org>
2021-01-07 19:10:24 -0500

    Removing now unnecessary reorder call.

--- app/jobs/export_job.rb
+++ app/jobs/export_job.rb
@@ -29,10 +29,8 @@ class ExportJob < ApplicationJob
 
     patients = patients_by_query(user, data.dig(:patients, :query) || {})
 
-    # NOTE: The reorder here clears out any other sorting that may have been added to this query as
-    # it should just be sorting by ID when getting batches. in_batches appears to NOT sort within batches,
-    # so explicit ordering on ID is also done deeper down.
-    patients.reorder('').in_batches(of: OUTER_BATCH_SIZE).each_with_index do |patients_group, index|
+    # NOTE: in_batches appears to NOT sort within batches, so explicit ordering on ID is also done deeper down.
+    patients.in_batches(of: OUTER_BATCH_SIZE).each_with_index do |patients_group, index|
       files = write_export_data_to_files(config, patients_group, index, INNER_BATCH_SIZE)
       lookups.concat(create_lookups(config, files))
     end
