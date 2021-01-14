eb899596ad7c3062fd13696647e92594b4c0f87c
David Connolly <dpconnolly@mitre.org>
2021-01-14 16:16:27 -0500

    refactors the PR to create one master list of monitoring_reasons

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -10,23 +10,6 @@ class Patient < ApplicationRecord
   include ActiveModel::Validations
   include FhirHelper
 
-  MONITORING_REASONS = [
-    'Completed Monitoring',
-    'Meets criteria to shorten quarantine',
-    'Does not meet criteria for monitoring',
-    'Meets Case Definition',
-    'Lost to follow-up during monitoring period',
-    'Lost to follow-up (contact never established)',
-    'Transferred to another jurisdiction',
-    'Person Under Investigation (PUI)',
-    'Case confirmed',
-    'Past monitoring period',
-    'Meets criteria to discontinue isolation',
-    'Deceased',
-    'Duplicate',
-    'Other'
-  ].freeze
-
   columns.each do |column|
     case column.type
     when :text
