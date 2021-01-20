a748ac62fa28c7ec90ad182bdf59d15e97b990ad
Justin Kufro <jkufro@mitre.org>
2021-01-20 12:17:21 -0700

    ensure csrf_token exists before requesting assigned_users

--- script/jmx/public_health_dashboard_jmx_test.rb
+++ script/jmx/public_health_dashboard_jmx_test.rb
@@ -15,11 +15,10 @@ test do
 
   cookies
 
-  threads count: 30 do
+  threads count: 10, loops: 1 do
     think_time 500, 3000
 
     visit name: '01_SA_visit_sign_in', url: '/users/sign_in' do
-      assert 'contains': 'Log In'
       # <input type="hidden" name="authenticity_token" value="[FILTERED]">
       extract regex: 'input type="hidden" name="authenticity_token" value="(.+?)"', name: 'authenticity_token', match_number: 1
     end
@@ -33,31 +32,30 @@ test do
                'user[password]': password,
                authenticity_token: '${authenticity_token}'
              } do
-        assert 'contains': 'epi_enroller_all@example.com (Public Health Enroller)'
         think_time 500, 1000
       end
     end
 
-    loops count: 50 do
+    loops count: 20 do
       visit name: '03_SA_dashboard', url: '/public_health' do
-        assert 'contains': 'epi_enroller_all@example.com (Public Health Enroller)'
         think_time 500, 1000
         # <meta name="csrf-token" content="[FILTERED]">
         extract regex: 'meta name="csrf-token" content="(.+?)"', name: 'csrf_token', match_number: 1
       end
 
-      # {"query"=>{"jurisdiction"=>1, "scope"=>"all", "workflow"=>"exposure", "tab"=>"all"}, "jurisdiction"=>{}}
-      submit name: '04_SA_assigned_users',
-             url: '/jurisdictions/assigned_users',
-             fill_in: {
-               'query[jurisdiction]': 1,
-               'query[scope]': 'all',
-               'query[workflow]': 'exposure',
-               'query[tab]': 'all',
-               authenticity_token: '${csrf_token}'
-             } do
-        assert 'contains': 'epi_enroller_all@example.com (Public Health Enroller)'
-        think_time 500, 1000
+      exists 'csrf_token' do
+        # {"query"=>{"jurisdiction"=>1, "scope"=>"all", "workflow"=>"exposure", "tab"=>"all"}, "jurisdiction"=>{}}
+        submit name: '04_SA_assigned_users',
+               url: '/jurisdictions/assigned_users',
+               fill_in: {
+                 'query[jurisdiction]': 1,
+                 'query[scope]': 'all',
+                 'query[workflow]': 'exposure',
+                 'query[tab]': 'all',
+                 authenticity_token: '${csrf_token}'
+               } do
+          think_time 500, 1000
+        end
       end
     end
   end
@@ -71,4 +69,4 @@ test do
 
   # end.run(path: '/usr/local/bin/', gui: false)
   # end.jmx(file: 'tmp/public_health_dashboard_jmx_test.jmx')
-end.run(path: '/usr/local/bin/', gui: true)
+end.run(path: '/usr/local/bin/', gui: false)
