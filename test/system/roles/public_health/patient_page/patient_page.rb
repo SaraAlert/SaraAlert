693de0a27df37fcb995e8389e002172f7f4554c3
Moses, Kylie T <ktmoses@mitre.org>
2021-01-20 16:09:41 -0500

    Added basic system tests for Move to Household functionality.

--- test/system/roles/public_health/patient_page/patient_page.rb
+++ test/system/roles/public_health/patient_page/patient_page.rb
@@ -9,6 +9,9 @@ class PublicHealthPatientPage < ApplicationSystemTestCase
   @@public_health_patient_page_verifier = PublicHealthPatientPageVerifier.new(nil)
   @@system_test_utils = SystemTestUtils.new(nil)
 
+  USERS = @@system_test_utils.users
+  PATIENTS = @@system_test_utils.patients
+
   def view_patients_details_and_reports(jurisdiction_id)
     monitorees = Jurisdiction.find(jurisdiction_id).all_patients
     click_on 'All Monitorees'
@@ -20,5 +23,47 @@ class PublicHealthPatientPage < ApplicationSystemTestCase
     monitorees.where(isolation: true).where(monitoring: true).each do |patient|
       @@public_health_patient_page_verifier.verify_patient_details_and_reports(patient, 'isolation')
     end
+  end
+
+  def move_to_household(user_label, patient_label, target_hoh_label)
+    search_in_move_to_household_modal(user_label, patient_label, target_hoh_label)
+    select_in_move_to_household_modal(user_label, patient_label, target_hoh_label)
+    # TODO: Pagination and sorting tests.
+  end
+
+  def search_in_move_to_household_modal(_user_label, _patient_label, target_hoh_label)
+    first_name = PATIENTS[target_hoh_label]['first_name']
+    last_name = PATIENTS[target_hoh_label]['last_name']
+    user_defined_id_statelocal = PATIENTS[target_hoh_label]['user_defined_id_statelocal']
+
+    click_on 'Move To Household'
+
+    # Searching by last name
+    fill_in 'search', with: last_name
+    assert page.find('tbody').has_link?("#{last_name}, #{first_name}")
+    assert page.find('tbody').has_button?('Select', count: 1)
+
+    # Searching by last name
+    fill_in 'search', with: first_name
+    assert page.find('tbody').has_link?("#{last_name}, #{first_name}")
+    assert page.find('tbody').has_button?('Select', count: 1)
+
+    # Searching by State/Local ID
+    fill_in 'search', with: user_defined_id_statelocal
+    assert page.find('tbody').has_text?(user_defined_id_statelocal.to_s)
+    assert page.find('tbody').has_button?('Select', count: 1)
+
+    click_on 'Cancel'
+  end
+
+  def select_in_move_to_household_modal(_user_label, _patient_label, target_hoh_label)
+    new_responder_id = PATIENTS[target_hoh_label]['id']
+
+    click_on 'Move To Household'
+    fill_in 'search', with: PATIENTS[target_hoh_label]['last_name']
+    assert page.has_button?('Select', count: 1)
+    click_on 'Select'
+
+    assert page.has_link?('Click here to view that monitoree', href: "/patients/#{new_responder_id}")
   end
 end
