1e79c4413682cf58b87fa364b76cef0cd7edcfcd
Justin Kufro <jkufro@mitre.org>
2021-01-21 12:11:13 -0700

    renaming and draft CI benchmarking

---
+++ script/run_jmeter_tests.sh
@@ -0,0 +1,20 @@
+
+DISEASE_TRAKKER_DATABASE_NAME='1m_disease_trakker_development' \
+bundle exec rails s &
+
+sleep 15
+
+for script in $(ls script/jmeter/); do
+  echo "Executing JMeter test: $script"
+  ruby script/jmeter/$script
+  sleep 5
+done
+
+if [ -f "tmp/pids/server.pid" ]; then
+    echo "Killing rails server with PID $(cat tmp/pids/server.pid)"
+    # -INT does not terminate the server
+    # kill -INT $(cat tmp/pids/server.pid)
+    kill -9 $(cat tmp/pids/server.pid)
+fi
+
+ruby script/jmx_assertion.rb
