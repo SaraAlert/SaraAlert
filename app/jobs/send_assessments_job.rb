0db9488491a808697f526fa45ae1c0ce8dcadb2f
Justin Kufro <jkufro@mitre.org>
2021-01-22 15:06:37 -0700

    Performance improvements to the send_assessments_job

--- app/jobs/send_assessments_job.rb
+++ app/jobs/send_assessments_job.rb
@@ -5,15 +5,70 @@ class SendAssessmentsJob < ApplicationJob
   queue_as :mailers
 
   def perform(*_args)
-    eligible = Patient.reminder_eligible.count
-    sent = []
-    not_sent = []
-    Patient.reminder_eligible.find_each do |patient|
-      sent << { id: patient.id, method: patient.preferred_contact_method } if patient.send_assessment
-    rescue StandardError => e
-      not_sent << { id: patient.id, method: patient.preferred_contact_method, reason: e.message }
-      next
+    patient_ids = Patient.reminder_eligible.pluck(:id)
+    eligible = patient_ids.size
+
+    # Check what the max thread pool is for the DB since that is the limiting factor
+    # on how many threads can be used in the job. Subtracting 2 from the pool
+    # size to be on the safer side of exhausting the connection pool.
+    total_threads = [1, ActiveRecord::Base.connection_pool.stat[:size] - 2].max
+
+    # [1, x].max here avoids an exception when there are 0 eligible patients
+    slice_size = [1, patient_ids.size / total_threads].max
+
+    threads = patient_ids.each_slice(slice_size).map do |patient_batch|
+      Thread.new { perform_batch(patient_batch) }
     end
-    UserMailer.assessment_job_email(sent, not_sent, eligible).deliver_now
+    threads.each(&:join)
+    results = combine_batch_results(threads.map(&:value))
+    UserMailer.assessment_job_email(results[:sent], results[:not_sent], eligible).deliver_now
+  end
+
+  private
+
+  def perform_batch(patient_batch)
+    sent = Hash.new(0)
+    not_sent = []
+    Patient.select(
+      :id,
+      :email,
+      :preferred_contact_method,
+      :monitored_address_state,
+      :address_state,
+      :last_assessment_reminder_sent,
+      :preferred_contact_time,
+      :last_date_of_exposure,
+      :created_at,
+      :monitoring,
+      :isolation,
+      :continuous_exposure
+    ).where(id: patient_batch).find_in_batches(batch_size: 15_000) do |group|
+      group.each do |patient|
+        if patient.send_assessment
+          sent[:count] += 1
+          sent[patient.preferred_contact_method] += 1
+        end
+      rescue StandardError => e
+        not_sent << { id: patient.id, method: patient.preferred_contact_method, reason: e.message }
+      end
+    end
+    {
+      sent: sent,
+      not_sent: not_sent
+    }
+  end
+
+  def combine_batch_results(batch_results)
+    results = {
+      sent: Hash.new(0),
+      not_sent: []
+    }
+    batch_results.each do |batch_result|
+      batch_result[:sent].each do |key, value|
+        results[:sent][key] += value
+      end
+      results[:not_sent] += batch_result[:not_sent]
+    end
+    results
   end
 end
---
+++ test/jobs/send_assessments_job_test.rb
@@ -0,0 +1,106 @@
+# frozen_string_literal: true
+
+require 'test_case'
+
+class SendAssessmentsJobTest < ActiveSupport::TestCase
+  def setup
+    Timecop.freeze(Time.now.in_time_zone('Eastern Time (US & Canada)').change(hour: 10).utc)
+    Patient.delete_all
+    ADMIN_OPTIONS['job_run_email'] = 'test@test.com'
+    ActionMailer::Base.deliveries.clear
+  end
+
+  def teardown; end
+
+  def default_days_ago(days)
+    (Time.now.in_time_zone('Eastern Time (US & Canada)') - days.days)
+  end
+
+  test 'send assessments job to zero patients' do
+    email = SendAssessmentsJob.perform_now
+    email_body = email.parts.first.body.to_s.gsub("\n", ' ')
+
+    assert_not ActionMailer::Base.deliveries.empty?
+    assert_includes(email_body, 'Total eligible for notifications at runtime: 0')
+    assert_includes(email_body, 'Sent during this job run: 0')
+    assert_includes(email_body, 'Not sent during this job run (due to exceptions): 0')
+  end
+
+  test 'send assessments job to various patient configurations' do
+    # Patients that should NOT be sent assessments or be counted in
+    # any of the counts found in the summary email
+    [
+      { purged: true },
+      { pause_notifications: true },
+      { preferred_contact_method: 'Unknown' },
+      { preferred_contact_method: 'Opt-out' },
+      { preferred_contact_method: '' },
+      { preferred_contact_method: nil },
+      { monitoring: false },
+      { last_assessment_reminder_sent: Time.now }
+    ].each do |ineligible_params|
+      create(
+        :patient,
+        {
+          email: 'example@example.com',
+          preferred_contact_method: 'E-mailed Web Link',
+          preferred_contact_time: 'Morning',
+          last_date_of_exposure: 1.day.ago,
+          submission_token: SecureRandom.urlsafe_base64[0, 10]
+        }.merge(ineligible_params)
+      )
+    end
+
+    # Patients that are expected to be returned by the scope but are not
+    # expected to be sent assessments
+    # (due to any logic in the Patient#send_assessment method)
+    #
+    # NOTE: Expect to need to move items from this list to the ineligible list
+    #       above as logic is pulled out of send_assessment and into reminder_eligible
+    scope_not_sent = [
+      { last_date_of_exposure: nil, created_at: 50.days.ago },
+      { last_date_of_exposure: 50.days.ago, created_at: 50.days.ago },
+      { latest_assessment_at: Time.now },
+      { preferred_contact_time: 'Evening' }
+    ].map do |ineligible_params|
+      create(
+        :patient,
+        {
+          email: 'example@example.com',
+          preferred_contact_method: 'E-mailed Web Link',
+          preferred_contact_time: 'Morning',
+          last_date_of_exposure: 1.day.ago,
+          submission_token: SecureRandom.urlsafe_base64[0, 10]
+        }.merge(ineligible_params)
+      )
+    end
+
+    # Patients that should be sent assessments
+    eligible_patients = [
+      { isolation: true },
+      { preferred_contact_method: 'SMS Texted Weblink', continuous_exposure: true },
+      { preferred_contact_method: 'Telephone call', last_date_of_exposure: nil, created_at: default_days_ago(5) },
+      { preferred_contact_method: 'SMS Text-message', last_date_of_exposure: default_days_ago(5), created_at: default_days_ago(5) },
+      { last_date_of_exposure: default_days_ago(11), created_at: default_days_ago(20) }
+    ].map do |eligible_params|
+      create(
+        :patient,
+        {
+          email: 'example@example.com',
+          preferred_contact_method: 'E-mailed Web Link',
+          preferred_contact_time: 'Morning',
+          last_date_of_exposure: 1.day.ago,
+          submission_token: SecureRandom.urlsafe_base64[0, 10]
+        }.merge(eligible_params)
+      )
+    end
+
+    email = SendAssessmentsJob.perform_now
+    email_body = email.parts.first.body.to_s.gsub("\n", ' ')
+
+    assert_not ActionMailer::Base.deliveries.empty?
+    assert_includes(email_body, "Total eligible for notifications at runtime: #{eligible_patients.size + scope_not_sent.size}")
+    assert_includes(email_body, "Sent during this job run: #{eligible_patients.size}")
+    assert_includes(email_body, 'Not sent during this job run (due to exceptions): 0')
+  end
+end
