085e94c8d618fca97f9c6ec9b7f099c006d0f10e
Wong, Tim <twong@mitre.org>
2021-01-26 14:57:51 -0500

    gray out unselected race options if refused to answer or unknown is selected
    rubocop

--- app/models/concerns/race_validator.rb
+++ app/models/concerns/race_validator.rb
@@ -4,9 +4,11 @@
 class RaceValidator < ActiveModel::Validator
   def validate(record)
     # If value is unsuccessfully typecast to a date, it will be nil, so validate on the value before cast
-    has_non_exclusive_race = record.white || record.black_or_african_american || record.american_indian_or_alaska_native|| record.asian || record.native_hawaiian_or_other_pacific_islander || record.race_other
-    if has_non_exclusive_race ? (record.race_unknown || record.race_refused_to_answer) : (record.race_unknown && race_refused_to_answer)
-      record.errors.add(record.race_unknown ? :race_unknown : :race_refused_to_answer, "cannot be true if any other race parameters are also true")
-    end
+    has_non_exclusive_race = record.white || record.black_or_african_american || record.american_indian_or_alaska_native ||
+                             record.asian || record.native_hawaiian_or_other_pacific_islander || record.race_other
+
+    return unless has_non_exclusive_race ? (record.race_unknown || record.race_refused_to_answer) : (record.race_unknown && record.race_refused_to_answer)
+
+    record.errors.add(record.race_unknown ? :race_unknown : :race_refused_to_answer, 'cannot be true if any other race parameters are also true')
   end
 end
