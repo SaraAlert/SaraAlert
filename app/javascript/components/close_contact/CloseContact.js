f714939bd32b60110b18b78838350e6ed7d75f1c
David Connolly <dpconnolly@mitre.org>
2021-01-28 10:22:13 -0500

    This commit adds a responsive column system for the CloseContacts Modal. It also changes the requirements for the required fields in that CloseContact modal.
    fixed up tests due to new structure of closecontacts modal

--- app/javascript/components/close_contact/CloseContact.js
+++ app/javascript/components/close_contact/CloseContact.js
@@ -8,6 +8,7 @@ import libphonenumber from 'google-libphonenumber';
 import DateInput from '../util/DateInput';
 import moment from 'moment';
 import InfoTooltip from '../util/InfoTooltip';
+import ReactTooltip from 'react-tooltip';
 
 const PNF = libphonenumber.PhoneNumberFormat;
 const phoneUtil = libphonenumber.PhoneNumberUtil.getInstance();
@@ -21,6 +22,7 @@ class CloseContact extends React.Component {
     super(props);
     this.state = {
       showModal: false,
+      disableCreate: true,
       loading: false,
       errors: {},
       first_name: this.props.close_contact.first_name || '',
@@ -47,6 +49,8 @@ class CloseContact extends React.Component {
       // (because the rest of this method hasnt had time to fire, and change 'show' to false)
       // When they click cancel, we want to null out all of the fields
       newState = {
+        disableCreate: true,
+        errors: {},
         first_name: this.props.close_contact.first_name || '',
         last_name: this.props.close_contact.last_name || '',
         primary_telephone: this.props.close_contact.primary_telephone || '',
@@ -66,6 +70,12 @@ class CloseContact extends React.Component {
 
   handleChange = event => {
     let value;
+    let disableCreate = false;
+    const possiblyRequiredFields = ['first_name', 'last_name', 'primary_telephone', 'email'];
+    if (possiblyRequiredFields.includes(event.target.id)) {
+      // This checks that at least one of the possiblyRequiredFields is set to a truthy value
+      disableCreate = !possiblyRequiredFields.some(field => (field === event.target.id ? !!event.target.value : !!this.state[`${field}`]));
+    }
     if (event?.target?.id && event.target.id === 'assigned_user') {
       if (isNaN(event.target.value) || parseInt(event.target.value) > 999999) return;
       // trim() call included since there is a bug with yup validation for numbers that allows whitespace entry
@@ -75,7 +85,7 @@ class CloseContact extends React.Component {
     } else {
       value = event.target.value;
     }
-    this.setState({ [event.target.id]: value });
+    this.setState({ [event.target.id]: value, disableCreate });
   };
 
   contactAttempt = async () => {
@@ -132,137 +142,151 @@ class CloseContact extends React.Component {
           <h1 className="sr-only">{title}</h1>
           <Modal.Title>{title}</Modal.Title>
         </Modal.Header>
-        <Modal.Body>
-          <Form>
-            <Row>
-              <Form.Group as={Col} controlId="first_name">
-                <Form.Label className="nav-input-label">First Name {schema?.fields?.first_name?._exclusive?.required && '*'} </Form.Label>
-                <Form.Control size="lg" className="form-square" value={this.state.first_name || ''} onChange={this.handleChange} />
-                <Form.Control.Feedback className="d-block" type="invalid">
-                  {this.state.errors['first_name']}
-                </Form.Control.Feedback>
-              </Form.Group>
-            </Row>
-            <Row>
-              <Form.Group as={Col} controlId="last_name">
-                <Form.Label className="nav-input-label">Last Name {schema?.fields?.last_name?._exclusive?.required && '*'} </Form.Label>
-                <Form.Control size="lg" className="form-square" value={this.state.last_name || ''} onChange={this.handleChange} />
-                <Form.Control.Feedback className="d-block" type="invalid">
-                  {this.state.errors['last_name']}
-                </Form.Control.Feedback>
-              </Form.Group>
-            </Row>
-            <Row>
-              <Form.Group as={Col} controlId="primary_telephone">
-                <Form.Label className="nav-input-label">Phone Number {schema?.fields?.primary_telephone?._exclusive?.required && '*'}</Form.Label>
-                <PhoneInput
-                  id="primary_telephone"
-                  className="form-square"
-                  value={this.state.primary_telephone}
-                  onChange={this.handleChange}
-                  isInvalid={!!this.state.errors['primary_telephone']}
-                />
-                <Form.Control.Feedback className="d-block" type="invalid">
-                  {this.state.errors['primary_telephone']}
-                </Form.Control.Feedback>
-              </Form.Group>
-            </Row>
-            <Row>
-              <Form.Group as={Col} controlId="email">
-                <Form.Label className="nav-input-label">Email {schema?.fields?.email?._exclusive?.required && '*'} </Form.Label>
-                <Form.Control size="lg" className="form-square" value={this.state.email || ''} onChange={this.handleChange} />
-                <Form.Control.Feedback className="d-block" type="invalid">
-                  {this.state.errors['email']}
-                </Form.Control.Feedback>
-              </Form.Group>
-            </Row>
-            <Row>
-              <Form.Group as={Col} controlId="last_date_of_exposure">
-                <Form.Label className="nav-input-label">Last Date of Exposure {schema?.fields?.last_date_of_exposure?._exclusive?.required && '*'}</Form.Label>
-                <DateInput
-                  id="last_date_of_exposure"
-                  date={this.state.last_date_of_exposure}
-                  minDate={'2020-01-01'}
-                  maxDate={moment()
-                    .add(30, 'days')
-                    .format('YYYY-MM-DD')}
-                  onChange={this.handleDateChange}
-                  placement="top"
-                  isInvalid={!!this.state.errors['last_date_of_exposure']}
-                  customClass="form-control-lg"
-                  ariaLabel="Last Date of Exposure Input"
-                />
-                <Form.Control.Feedback className="d-block" type="invalid">
-                  {this.state.errors['last_date_of_exposure']}
-                </Form.Control.Feedback>
-              </Form.Group>
-            </Row>
-            <Row>
-              <Form.Group as={Col} className="mb-2 pt-2" controlId="assigned_user">
-                <Form.Label className="nav-input-label">
-                  Assigned User {schema?.fields?.assigned_user?._exclusive?.required && '*'}
-                  <InfoTooltip tooltipTextKey="assignedUser" location="top"></InfoTooltip>
-                </Form.Label>
-                <Form.Control
-                  isInvalid={this.state.errors['assigned_user']}
-                  as="input"
-                  list="assigned_users"
-                  autoComplete="off"
-                  size="lg"
-                  className="d-block"
-                  onChange={this.handleChange}
-                  value={this.state.assigned_user || ''}
-                />
-                <datalist id="assigned_users">
-                  {this.props.assigned_users?.map(num => {
-                    return (
-                      <option value={num} key={num}>
-                        {num}
-                      </option>
-                    );
-                  })}
-                </datalist>
-                <Form.Control.Feedback className="d-block" type="invalid">
-                  {this.state.errors['assigned_user']}
-                </Form.Control.Feedback>
-              </Form.Group>
-            </Row>
-            <Row>
-              <Form.Group as={Col}>
-                <Form.Label htmlFor="notes" className="nav-input-label">
-                  Notes
-                  {schema?.fields?.notes?._exclusive?.required && '*'}
-                </Form.Label>
-                <Form.Control
-                  id="notes"
-                  as="textarea"
-                  rows="5"
-                  className="form-square"
-                  value={this.state.notes || ''}
-                  placeholder={this.closeContactNotePlaceholder}
-                  maxLength="2000"
-                  onChange={this.handleChange}
-                />
-                <Form.Label className="notes-character-limit"> {2000 - this.state.notes.length} characters remaining </Form.Label>
-                <Form.Control.Feedback className="d-block" type="invalid">
-                  {this.state.errors['notes']}
-                </Form.Control.Feedback>
-              </Form.Group>
-            </Row>
-          </Form>
+        <Modal.Body className="px-5">
+          <Row className="mt-3">
+            <Form.Group as={Col} lg="12" controlId="first_name">
+              <Form.Label className="nav-input-label">First Name {schema?.fields?.first_name?._exclusive?.required && '*'} </Form.Label>
+              <Form.Control size="lg" className="form-square" value={this.state.first_name || ''} onChange={this.handleChange} />
+              <Form.Control.Feedback className="d-block" type="invalid">
+                {this.state.errors['first_name']}
+              </Form.Control.Feedback>
+            </Form.Group>
+            <Form.Group as={Col} lg="12" controlId="last_name">
+              <Form.Label className="nav-input-label">Last Name {schema?.fields?.last_name?._exclusive?.required && '*'} </Form.Label>
+              <Form.Control size="lg" className="form-square" value={this.state.last_name || ''} onChange={this.handleChange} />
+              <Form.Control.Feedback className="d-block" type="invalid">
+                {this.state.errors['last_name']}
+              </Form.Control.Feedback>
+            </Form.Group>
+          </Row>
+          <Row>
+            <Form.Group as={Col} controlId="primary_telephone">
+              <Form.Label className="nav-input-label">Phone Number {schema?.fields?.primary_telephone?._exclusive?.required && '*'}</Form.Label>
+              <PhoneInput
+                id="primary_telephone"
+                className="form-square"
+                value={this.state.primary_telephone}
+                onChange={this.handleChange}
+                isInvalid={!!this.state.errors['primary_telephone']}
+              />
+              <Form.Control.Feedback className="d-block" type="invalid">
+                {this.state.errors['primary_telephone']}
+              </Form.Control.Feedback>
+            </Form.Group>
+            <Form.Group as={Col} controlId="email">
+              <Form.Label className="nav-input-label">Email {schema?.fields?.email?._exclusive?.required && '*'} </Form.Label>
+              <Form.Control size="lg" className="form-square" value={this.state.email || ''} onChange={this.handleChange} />
+              <Form.Control.Feedback className="d-block" type="invalid">
+                {this.state.errors['email']}
+              </Form.Control.Feedback>
+            </Form.Group>
+          </Row>
+          <hr></hr>
+          <Row>
+            <Form.Group as={Col} controlId="last_date_of_exposure">
+              <Form.Label className="nav-input-label">Last Date of Exposure {schema?.fields?.last_date_of_exposure?._exclusive?.required && '*'}</Form.Label>
+              <DateInput
+                id="last_date_of_exposure"
+                date={this.state.last_date_of_exposure}
+                minDate={'2020-01-01'}
+                maxDate={moment()
+                  .add(30, 'days')
+                  .format('YYYY-MM-DD')}
+                onChange={this.handleDateChange}
+                placement="top"
+                isInvalid={!!this.state.errors['last_date_of_exposure']}
+                customClass="form-control-lg"
+                ariaLabel="Last Date of Exposure Input"
+              />
+              <Form.Control.Feedback className="d-block" type="invalid">
+                {this.state.errors['last_date_of_exposure']}
+              </Form.Control.Feedback>
+            </Form.Group>
+            <Form.Group as={Col} controlId="assigned_user">
+              <Form.Label className="nav-input-label">
+                Assigned User {schema?.fields?.assigned_user?._exclusive?.required && '*'}
+                <InfoTooltip tooltipTextKey="assignedUser" location="top"></InfoTooltip>
+              </Form.Label>
+              <Form.Control
+                isInvalid={this.state.errors['assigned_user']}
+                as="input"
+                list="assigned_users"
+                autoComplete="off"
+                size="lg"
+                className="d-block"
+                onChange={this.handleChange}
+                value={this.state.assigned_user || ''}
+              />
+              <datalist id="assigned_users">
+                {this.props.assigned_users?.map(num => {
+                  return (
+                    <option value={num} key={num}>
+                      {num}
+                    </option>
+                  );
+                })}
+              </datalist>
+              <Form.Control.Feedback className="d-block" type="invalid">
+                {this.state.errors['assigned_user']}
+              </Form.Control.Feedback>
+            </Form.Group>
+          </Row>
+          <Row>
+            <Form.Group as={Col}>
+              <Form.Label htmlFor="notes" className="nav-input-label">
+                Notes
+                {schema?.fields?.notes?._exclusive?.required && '*'}
+              </Form.Label>
+              <Form.Control
+                id="notes"
+                as="textarea"
+                rows="5"
+                className="form-square"
+                value={this.state.notes || ''}
+                placeholder={this.closeContactNotePlaceholder}
+                maxLength="2000"
+                onChange={this.handleChange}
+              />
+              <Form.Label className="notes-character-limit"> {2000 - this.state.notes.length} characters remaining </Form.Label>
+              <Form.Control.Feedback className="d-block" type="invalid">
+                {this.state.errors['notes']}
+              </Form.Control.Feedback>
+            </Form.Group>
+          </Row>
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary btn-square" onClick={toggle}>
             Cancel
           </Button>
-          <Button
-            variant="primary btn-square"
-            onClick={submit}
-            disabled={
-              !(this.state.first_name || this.state.last_name || this.state.email || this.state.primary_telephone || this.state.notes) || this.state.loading
-            }>
-            {this.props.close_contact.id ? 'Update' : 'Create'}
+          <Button variant="primary btn-square" onClick={submit} disabled={this.state.disableCreate || this.state.loading}>
+            <span data-for="create-tooltip" data-tip="" className="ml-1">
+              {this.props.close_contact.id ? 'Update' : 'Create'}
+            </span>
           </Button>
+          {/* Typically we pair the ReactTooltip up directly next to the mount point. However, due to the disabled attribute on the button */}
+          {/* above, this Tooltip should be placed outside the parent component (to prevent unwanted parent opacity settings from being inherited) */}
+          {/* This does not impact component functionality at all. */}
+          <ReactTooltip
+            id="create-tooltip"
+            multiline={true}
+            place="top"
+            type="dark"
+            effect="solid"
+            offset={{ left: 113 }}
+            arrowColor="#ffffff00"
+            className="tooltip-container text-left"
+            disable={!this.state.disableCreate}>
+            <div>
+              {' '}
+              Please enter at least one of the following:
+              <ul className="m-0 pl-3">
+                <li>First Name</li>
+                <li>Last Name</li>
+                <li>Phone Number</li>
+                <li>Email</li>
+              </ul>
+            </div>
+          </ReactTooltip>
         </Modal.Footer>
       </Modal>
     );
