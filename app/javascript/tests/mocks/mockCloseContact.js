4bb6bdce13b3d6f57f6c18e2650e2c1ddd25f6b3
David Connolly <dpconnolly@mitre.org>
2021-01-29 10:01:13 -0500

    Added new tests
    Reverted center text to left-justified text for consistency
    
    Reverted logic for submit button based off team review

--- app/javascript/tests/mocks/mockCloseContact.js
+++ app/javascript/tests/mocks/mockCloseContact.js
@@ -1,6 +1,6 @@
 const mockCloseContact1 = {
   id: null,
-  patient_id: 4,
+  patient_id: null,
   first_name: null,
   last_name: null,
   primary_telephone: null,
@@ -18,7 +18,7 @@ const mockCloseContact1 = {
 }
 
 const mockCloseContact2 = {
-  id: 1,
+  id: 2,
   patient_id: 4,
   first_name: "Captain",
   last_name: "Rogers",
@@ -35,9 +35,27 @@ const mockCloseContact2 = {
   user_defined_id_cdc: null,
   user_defined_id_nndss: null
 }
-
+const mockCloseContact3 = {
+  id: 2,
+  patient_id: null,
+  first_name: "Thor",
+  last_name: "Odinson",
+  primary_telephone: "+15555150256",
+  email: null,
+  notes: "Because that's what superheros do",
+  enrolled_id: null,
+  contact_attempts: null,
+  created_at: "2021-01-06 17:53:58.000000000 +0000",
+  updated_at: "2021-01-06 17:53:58.000000000 +0000",
+  last_date_of_exposure: null,
+  assigned_user: null,
+  user_defined_id_statelocal: null,
+  user_defined_id_cdc: null,
+  user_defined_id_nndss: null
+}
 
 export {
   mockCloseContact1,
   mockCloseContact2,
+  mockCloseContact3,
 };
