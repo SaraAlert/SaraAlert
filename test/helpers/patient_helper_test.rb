2f1f55ea235d042c33fe6c4a065504e99580330e
Kyle Fagan <kfagan@mitre.org>
2021-02-15 09:53:58 -0800

    time_zone_offset_for_state now correctly provides the zone offset

--- test/helpers/patient_helper_test.rb
+++ test/helpers/patient_helper_test.rb
@@ -13,4 +13,14 @@ class PatientHelperTest < ActiveSupport::TestCase
     assert_equal('New Hampshire', test_subject.address_state)
     assert_equal('New Hampshire', test_subject.additional_planned_travel_destination_state)
   end
+
+  test 'time zone offset for state ' do
+    # DST starts 2nd Sunday in March so pick a date in May to ensure DST.
+    Timecop.freeze(Time.local(2008, 5, 1, 12, 0, 0))
+    assert_equal('-04:00', time_zone_offset_for_state('Massachusetts'))
+    # ST
+    Timecop.freeze(Time.local(2008, 2, 1, 12, 0, 0))
+    assert_equal('-05:00', time_zone_offset_for_state('Massachusetts'))
+    Timecop.return
+  end
 end
