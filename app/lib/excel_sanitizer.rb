97aba62cfac52c40562b3f648880ba752c285ab1
Kyle Fagan <kfagan@mitre.org>
2021-02-18 12:59:57 -0800

    Add Excel sanitization method which will remove = characters from the beginning of string and text fields before exporting to Excel to prevent formula and display related errors

---
+++ app/lib/excel_sanitizer.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+# String manipulation of data fields that are to be exported
+# Not intended for to protect against any threat vector through Excel
+# Intent is to reduce errors when viewing exported files
+module ExcelSanitizer
+  # Removes = from the beginning of a string to avoid "#NAME!" and
+  # "There is a problem with a formula in this file" errors
+  def remove_formula_start(string)
+    return string unless string&.starts_with?('=') || string&.starts_with?(' ==')
+
+    remove_formula_start(string[1..])
+  end
+end
