e904112b63747f231fb99c0e6bd6ce38e29c765a
David Connolly <dpconnolly@mitre.org>
2021-03-01 16:27:43 -0500

    Updates EnrollerAnalytics to use the same styling as the Epi Analytics
    Moves Enroller's SystemStatistics and YourStatistics to common component
    
    Removes old and unused Analytics files
    
    Fixes Analytics Component naming
    
    Adds Tests for the Enroller Analytics
    
    Adds spacing for the EnrollerAnalytics on smaller viewports

--- app/javascript/components/analytics/EnrollerAnalytics.js
+++ app/javascript/components/analytics/EnrollerAnalytics.js
@@ -2,8 +2,7 @@ import React from 'react';
 import { PropTypes } from 'prop-types';
 import { Row, Col } from 'react-bootstrap';
 
-import SystemStatistics from './widgets/SystemStatistics';
-import YourStatistics from './widgets/YourStatistics';
+import EnrollerStatistics from './widgets/EnrollerStatistics';
 
 class EnrollerAnalytics extends React.Component {
   constructor(props) {
@@ -16,11 +15,23 @@ class EnrollerAnalytics extends React.Component {
         <h1 className="sr-only">Analytics</h1>
         <div className="mx-2">
           <Row className="pt-4">
-            <Col md="12">
-              <SystemStatistics stats={this.props.stats} />
+            <Col lg="12">
+              <EnrollerStatistics
+                title="System Statistics"
+                total_monitorees={this.props.stats.system_subjects}
+                new_monitorees={this.props.stats.system_subjects_last_24}
+                total_reports={this.props.stats.system_assessments}
+                new_reports={this.props.stats.system_assessments_last_24}
+              />
             </Col>
-            <Col md="12">
-              <YourStatistics stats={this.props.stats} />
+            <Col lg="12" className="my-4 my-lg-0">
+              <EnrollerStatistics
+                title="Your Statistics"
+                total_monitorees={this.props.stats.user_subjects}
+                new_monitorees={this.props.stats.user_subjects_last_24}
+                total_reports={this.props.stats.user_assessments}
+                new_reports={this.props.stats.user_assessments_last_24}
+              />
             </Col>
           </Row>
         </div>
---
+++ app/javascript/tests/analytics/EnrollerAnalytics.test.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { shallow } from 'enzyme';
+import EnrollerStatistics from '../../components/analytics/widgets/EnrollerStatistics';
+
+import EnrollerAnalytics from '../../components/analytics/EnrollerAnalytics.js'
+import { mockUser1 } from '../mocks/mockUsers'
+import { mockEnrollerStatistics1 } from '../mocks/mockEnrollerStatistics'
+
+function getShallowWrapper() {
+  return shallow(<EnrollerAnalytics user={mockUser1} stats={mockEnrollerStatistics1}/>);
+}
+
+afterEach(() => {
+  jest.clearAllMocks();
+});
+
+describe('EnrollerAnalytics', () => {
+  it('Properly Renders Both EnrollerStatistics Cards correctly', () => {
+    const eaWrapper = getShallowWrapper();
+    expect(eaWrapper.find('Fragment').find('Row').find('Col').at(0).containsMatchingElement(<EnrollerStatistics />)).toBeTruthy();
+    expect(eaWrapper.find(EnrollerStatistics).at(0).prop('title')).toEqual('System Statistics');
+    expect(eaWrapper.find(EnrollerStatistics).at(0).prop('total_monitorees')).toEqual(mockEnrollerStatistics1.system_subjects);
+    expect(eaWrapper.find(EnrollerStatistics).at(0).prop('new_monitorees')).toEqual(mockEnrollerStatistics1.system_subjects_last_24);
+    expect(eaWrapper.find(EnrollerStatistics).at(0).prop('total_reports')).toEqual(mockEnrollerStatistics1.system_assessments);
+    expect(eaWrapper.find(EnrollerStatistics).at(0).prop('new_reports')).toEqual(mockEnrollerStatistics1.system_assessments_last_24);
+
+    expect(eaWrapper.find('Fragment').find('Row').find('Col').at(1).containsMatchingElement(<EnrollerStatistics />)).toBeTruthy();
+    expect(eaWrapper.find(EnrollerStatistics).at(1).prop('title')).toEqual('Your Statistics');
+    expect(eaWrapper.find(EnrollerStatistics).at(1).prop('total_monitorees')).toEqual(mockEnrollerStatistics1.user_subjects);
+    expect(eaWrapper.find(EnrollerStatistics).at(1).prop('new_monitorees')).toEqual(mockEnrollerStatistics1.user_subjects_last_24);
+    expect(eaWrapper.find(EnrollerStatistics).at(1).prop('total_reports')).toEqual(mockEnrollerStatistics1.user_assessments);
+    expect(eaWrapper.find(EnrollerStatistics).at(1).prop('new_reports')).toEqual(mockEnrollerStatistics1.user_assessments_last_24);
+
+  });
+});
