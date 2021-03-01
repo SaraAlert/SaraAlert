d1ba3563885728f4879be80b841601633606a1ef
Moses, Kylie T <ktmoses@mitre.org>
2021-03-01 12:41:24 -0500

    Updating sara alert import template download link to download served file rather than file at Github.

--- app/javascript/components/public_health/PublicHealthHeader.js
+++ app/javascript/components/public_health/PublicHealthHeader.js
@@ -126,8 +126,7 @@ class PublicHealthHeader extends React.Component {
         <Modal.Body>
           {this.state.importType === 'saf' && (
             <div className="mb-3">
-              <a href="https://github.com/SaraAlert/SaraAlert/blob/master/public/Sara%20Alert%20Import%20Format.xlsx?raw=true">Download formatting guidance</a>{' '}
-              (Updated 3/23/2021)
+              <a href="Sara%20Alert%20Import%20Format.xlsx?raw=true">Download formatting guidance</a> (Updated 3/23/2021)
             </div>
           )}
           <Form inline>
