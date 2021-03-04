b07debcb5f6a9442adb2efd5e33d9e11171c0702
ashankland <ashankland@mitre.org>
2021-03-03 21:21:45 -0500

    added landing page, needs review for style

--- config/routes.rb
+++ config/routes.rb
@@ -87,6 +87,7 @@ Rails.application.routes.draw do
   get '/import/download_guidance', to: 'import#download_guidance'
 
   get '/patients/:id/household_removeable', to: 'patients#household_removeable'
+  get '/patients/:id/unknown_patient', to: 'patients#unknown_patient'
   post '/patients/head_of_household_options', to: 'patients#head_of_household_options'
   post '/patients/bulk_edit', to: 'patients#bulk_update'
   post '/patients/:id/status', to: 'patients#update_status'
