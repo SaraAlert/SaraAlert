c21c1e90d686e386749e911dd51f368bf1dde21b
Tom Strassner <tstrassner@mitre.org>
2021-03-15 17:29:45 -0400

    Move primary contact validation error to :base
    This makes sense to do because the rule involves multiple attributes.
    This also fixes an issue where the validation error returned from
    the API for primary contact could be confusing.
    
    This change came from code review.

--- app/models/concerns/primary_contact_validator.rb
+++ app/models/concerns/primary_contact_validator.rb
@@ -2,12 +2,13 @@
 
 # Validates that a primary contact
 class PrimaryContactValidator < ActiveModel::Validator
+  include ValidationHelper
   def validate(record)
     if record.email.blank? && record.preferred_contact_method == 'E-mailed Web Link'
-      record.errors.add(:email, "is required when 'Primary Contact Method' is 'E-mailed Web Link'")
+      record.errors.add(:base, "#{VALIDATION[:email][:label]} is required when 'Primary Contact Method' is 'E-mailed Web Link'")
     end
     return unless record.primary_telephone.blank? && ['SMS Texted Weblink', 'Telephone call', 'SMS Text-message'].include?(record.preferred_contact_method)
 
-    record.errors.add(:primary_telephone, "is required when 'Primary Contact Method' is '#{record.preferred_contact_method}'")
+    record.errors.add(:base, "#{VALIDATION[:primary_telephone][:label]} is required when 'Primary Contact Method' is '#{record.preferred_contact_method}'")
   end
 end
