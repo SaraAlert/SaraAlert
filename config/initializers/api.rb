6d65935744a56f0080e690adcf96e5bc8fc01e83
Tom Strassner <tstrassner@mitre.org>
2021-03-22 21:14:58 -0400

    Suppress logging in fhir_models gem
    Specifically, in the call to FHIRPath.evaluate. This is just because
    it logs a ton of debug info that we don't care about.
    
    This required adding an initializer to set the FHIR logger.

---
+++ config/initializers/api.rb
@@ -0,0 +1,5 @@
+# Be sure to restart your server when you modify this file.
+
+# Take control of logging in the fhir_models gem
+# If we don't set this, it defaults to STDOUT
+FHIR.logger = Rails.logger
