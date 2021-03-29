1d3f0947735112ce27f70868ce8ce44befffb10c
dubem7 <7842704+dubem7@users.noreply.github.com>
2021-03-29 11:03:57 -0400

    Rename duplicate_data to duplicate_data_detection and change input
    Pass in a patient instead of fields
    respond to PR comments

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -101,11 +101,7 @@ class PatientsController < ApplicationController
 
     # Check for potential duplicate
     unless params[:bypass_duplicate]
-      duplicate_data = current_user.viewable_patients.duplicate_data(allowed_params[:first_name],
-                                                                     allowed_params[:last_name],
-                                                                     allowed_params[:sex],
-                                                                     allowed_params[:date_of_birth],
-                                                                     allowed_params[:user_defined_id_statelocal])
+      duplicate_data = current_user.viewable_patients.duplicate_data_detection(allowed_params)
 
       render(json: duplicate_data) && return if duplicate_data[:is_duplicate]
     end
