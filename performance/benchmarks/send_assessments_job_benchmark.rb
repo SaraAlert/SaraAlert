1f88480ff85f830539e942e20dbc5cb28512b7e2
Justin Kufro <jkufro@mitre.org>
2021-04-01 06:49:50 -0600

    revert job back to printing out all sent IDs

--- performance/benchmarks/send_assessments_job_benchmark.rb
+++ performance/benchmarks/send_assessments_job_benchmark.rb
@@ -3,10 +3,10 @@
 require_relative '../../config/environment'
 require_relative '../benchmark'
 
-# Time Threshold: 10 minutes
+# Time Threshold: 260 seconds
 benchmark(
   name: 'SendAssessmentsJob',
-  time_threshold: 10 * 60,
+  time_threshold: 260,
   setup: proc { Timecop.travel(Time.now.utc.change(hour: 21)) },
   teardown: proc { Timecop.return }
 ) { SendAssessmentsJob.perform_now }
