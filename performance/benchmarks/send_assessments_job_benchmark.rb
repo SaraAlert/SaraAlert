0d9b4698413139f134c9ffee163cb3404bf1def2
Justin Kufro <jkufro@mitre.org>
2021-04-07 08:17:40 -0600

    lowered threshold for send_assessments_job_benchmark

--- performance/benchmarks/send_assessments_job_benchmark.rb
+++ performance/benchmarks/send_assessments_job_benchmark.rb
@@ -3,10 +3,10 @@
 require_relative '../../config/environment'
 require_relative '../benchmark'
 
-# Time Threshold: 260 seconds
+# Time Threshold: 200 seconds
 benchmark(
   name: 'SendAssessmentsJob',
-  time_threshold: 260,
+  time_threshold: 200,
   setup: proc { Timecop.travel(Time.now.utc.change(hour: 21)) },
   teardown: proc { Timecop.return }
 ) { SendAssessmentsJob.perform_now }
