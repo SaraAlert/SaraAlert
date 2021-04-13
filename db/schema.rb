3bf5aa2839ca9700b51694f19aa34d3831009921
Justin Kufro <jkufro@mitre.org>
2021-04-13 08:53:56 -0600

    Allow monitorees that perfer SMS to receive SMS closed notifications & allow jurisdictions to opt-in to closed notifications for monitorees
    added patient helper and test that determines the next Time that they should be contacted
    
    add i18n for sms closed messaging
    
    added SendSmsJob that allows scheduling of SMS messages
    
    send SMS or email to patient upon close depending on preferred contact method
    
    sms job was unnecessary. Language update for closed SMS (Still need translations)
    
    close job strictly uses preferred contact or nothing. histories for closed patients if preferred contact is not usable for notification. additional tests for close job.
    
    language change for history item when closing and no viable preferred contact method
    
    added send_close column to jurisdictions (default is false) that will determine if the jurisdiction sends automated closed notifications to monitorees
    
    take into account jurisdiction send_close in close patients job
    
    add and test patient.time_to_notify_closed method, use new method for scheduling close notifications, alter closed SMS language
    
    updated language for closed email to be the same as closed SMS, new tests for changed language, placeholder text for non-eng locales
    
    add safe navigation for the unlikely case where closed_at is nil
    
    assertion helpers for asserting that a patient does or does not have a history that conatains text
    
    pull contact window times into constants
    
    jurisdiction_send_close performance improvement in close_patients_job
    
    It is faster just to pluck all of the jurisdiction ID and send_close and call to_h
    than it is to cache the send_close as the job runs.
    
    This was benchmarked on a 500k patient database to prove the performance bump.
    
    refactor history contains assertion helpers
    
    Made both helpers easier to read & understand
    
    correct references to histories contain test helpers
    
    added fra, som, spa-pr, and spa translations for monitoring complete thank you
    
    create monitoring_complete_message_sent history item with custom msg when no valid contact method
    
    create monitoring_complete_message_sent history item when sending with SMS
    
    should create custom monitoring_complete_message_sent history item when contact method was blank
    
    closed SMS now complies with new expectations for TwilioSender.send_sms
    
    update to updated assert histories contain method names
    
    update comment surrounding close notification criteria

--- db/schema.rb
+++ db/schema.rb
@@ -182,6 +182,7 @@ ActiveRecord::Schema.define(version: 2021_06_14_190551) do
     t.string "webpage"
     t.string "message"
     t.boolean "send_digest", default: false
+    t.boolean "send_close", default: false
     t.index ["ancestry"], name: "index_jurisdictions_on_ancestry"
   end
 
