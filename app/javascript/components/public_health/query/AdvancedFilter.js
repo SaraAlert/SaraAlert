8a5e01f87bc4e3281b01443be0f6cd61f2283ffa
sgober <35042815+sgober@users.noreply.github.com>
2021-04-28 14:22:27 -0400

    SARAALERT-1055: Advanced Filter Lab Results (#821)
    * initial setup for multi filter type
    
    * support for one multi statement rendering and change functionality
    
    * added support for array of multi values
    
    * added suppport to add more multi value statements
    
    * added support for removing multi statements
    
    * added support for not allowing the same option twice (disabling and adding filter)
    
    * updated filter params
    
    * backend support for lab results filter
    
    * updated tooltip and description for lab result filter
    
    * support multiple lab results + rubocop
    
    * added backend query tests, updated front end unit tests and added method comments
    
    * code cleanup
    
    * linting
    
    * added tooltip to multi type + button
    
    * change "report" to "report date" in dropdown

--- app/javascript/components/public_health/query/AdvancedFilter.js
+++ app/javascript/components/public_health/query/AdvancedFilter.js
@@ -286,9 +286,44 @@ class AdvancedFilter extends React.Component {
    * @param {Number} index - Current filter index
    */
   removeStatement = index => {
-    this.setState(state => ({
-      activeFilterOptions: state.activeFilterOptions.slice(0, index).concat(state.activeFilterOptions.slice(index + 1, state.activeFilterOptions?.length)),
-    }));
+    let activeFilterOptions = this.state.activeFilterOptions;
+    activeFilterOptions.splice(index, 1);
+    this.setState({ activeFilterOptions });
+  };
+
+  /**
+   * Adds another multi filter statement at certain index
+   * Ensures no field is repeated when a new statement is added
+   * @param {Object} filter - Current multi filter object containing all the possible fields
+   * @param {Number} statementIndex - Current overall statement filter index
+   */
+  addMultiStatement = (filter, statementIndex) => {
+    const currentMultiFilter = this.state.activeFilterOptions[parseInt(statementIndex)];
+    let possibleFields = currentMultiFilter.filterOption.fields;
+    currentMultiFilter.value.forEach(value => {
+      possibleFields = possibleFields.filter(field => field.name !== value.name);
+    });
+    const newValue = [...currentMultiFilter.value, this.getDefaultMultiValue(filter, possibleFields[0].name)];
+    this.changeValue(statementIndex, newValue);
+  };
+
+  /**
+   * Removes one of the multi filter statements at certain index
+   * @param {Number} statementIndex - Current overall statement filter index
+   * @param {Number} multiIndex - Index of the statement within the multi filter that will be removed
+   */
+  removeMultiStatement = (statementIndex, multiIndex) => {
+    const currentMultiFilter = this.state.activeFilterOptions[parseInt(statementIndex)];
+    let value = [...currentMultiFilter.value];
+
+    // if removing the last multi statement, remove the whole filter
+    // otherwise just remove that multi statement
+    if (value.length === 1) {
+      this.removeStatement(statementIndex);
+    } else {
+      value.splice(multiIndex, 1);
+      this.changeValue(statementIndex, value);
+    }
   };
 
   /**
@@ -322,6 +357,8 @@ class AdvancedFilter extends React.Component {
       value = 'today';
     } else if (filterOption.type === 'search') {
       value = '';
+    } else if (filterOption.type === 'multi') {
+      value = [this.getDefaultMultiValue(filterOption, filterOption.fields[0].name)];
     }
 
     activeFilterOptions[parseInt(index)] = {
@@ -446,6 +483,60 @@ class AdvancedFilter extends React.Component {
   };
 
   /**
+   * Change value of a filter statement of type multi
+   * @param {Number} statementIndex - Current overall statement filter index
+   * @param {Number} multiIndex - Index of the statement within the multi filter that needs to be updated
+   * @param {*} value - New value for the statement at the multi index
+   */
+  changeMultiValue = (statementIndex, multiIndex, value) => {
+    const currentMultiFilter = this.state.activeFilterOptions[parseInt(statementIndex)];
+    const newValue = [...currentMultiFilter.value];
+    newValue[parseInt(multiIndex)] = value;
+    this.changeValue(statementIndex, newValue);
+  };
+
+  /**
+   * Determines if a field within a multi statement should be disabled
+   * A field is disabled if it already exists in a different multi statement, but not the one it is selected in
+   * @param {String} name - Field name attribute of the dropdown option
+   * @param {Number} statementIndex - Current overall statement filter index
+   * @param {Number} multiIndex - Index of the statement within the multi filter
+   */
+  multiFieldDisabled = (name, multiIndex, statementIndex) => {
+    const currentMultiFilter = this.state.activeFilterOptions[parseInt(statementIndex)];
+    return currentMultiFilter.value.some((value, index) => value.name === name && index !== multiIndex);
+  };
+
+  /**
+   * Returns the field of a filter when given the name of that filter
+   * @param {Object} filter - Multi filter object containing all the possible fields
+   * @param {String} name - Field name attribute
+   */
+  getMultiFilter = (filter, name) => {
+    return filter.fields.find(field => field.name === name);
+  };
+
+  /**
+   * Given the name on of a field for a certain multi filter, returns the default value
+   * The default value is determined by the type of the field
+   * @param {Object} filter - Multi filter object containing all the possible fields
+   * @param {String} name - Field name attribute
+   */
+  getDefaultMultiValue = (filter, name) => {
+    const multiFilter = this.getMultiFilter(filter, name);
+    let value = null;
+    if (multiFilter.type === 'select') {
+      value = { name: name, value: multiFilter.options[0] };
+    } else if (multiFilter.type === 'date') {
+      value = {
+        name: name,
+        value: { when: 'before', date: moment().format('YYYY-MM-DD') },
+      };
+    }
+    return value;
+  };
+
+  /**
    * Gets the string inside the tooltip for relative date filter options.
    * @param {Object} filter - Filter currently selected
    * @param {Object} value - Filter value (object containing number, unit, operator and when)
@@ -548,13 +639,11 @@ class AdvancedFilter extends React.Component {
    * Renders remove statement button (minus button at far right of statement)
    * @param {Number} index - Filter index
    */
-  renderRemoveStatementButton = index => {
+  renderRemoveStatementButton = (index, sub_index, remove) => {
     return (
-      <div className="float-right">
-        <Button className="remove-filter-row" variant="danger" onClick={() => this.removeStatement(index)} aria-label="Remove Advanced Filter Option">
-          <i className="fas fa-minus"></i>
-        </Button>
-      </div>
+      <Button className="remove-filter-row" variant="danger" onClick={() => remove(index, sub_index)} aria-label="Remove Advanced Filter Option">
+        <i className="fas fa-minus"></i>
+      </Button>
     );
   };
 
@@ -606,7 +695,7 @@ class AdvancedFilter extends React.Component {
         }}
         placeholder="Select Field...."
         aria-label="Advanced Filter Options Dropdown"
-        className="advanced-filter-select"
+        className="advanced-filter-options-dropdown"
         styles={cursorPointerStyle}
         theme={theme => ({
           ...theme,
@@ -735,7 +824,7 @@ class AdvancedFilter extends React.Component {
         <Form.Control
           as="select"
           value={value}
-          className="advanced-filter-select-dropdown py-0 my-0"
+          className="advanced-filter-select py-0 my-0"
           aria-label="Advanced Filter Option Select"
           onChange={event => {
             this.changeValue(index, event.target.value);
@@ -978,6 +1067,120 @@ class AdvancedFilter extends React.Component {
   };
 
   /**
+   * Renders multi-select type line "statement"
+   * @param {Object} filter - Multi filter object containing all the possible fields
+   * @param {Number} statementIndex - Current overall statement filter index
+   * @param {Number} multiIndex - Index of the statement within the multi filter
+   * @param {Number} total - Total number of statments within the multi filter
+   * @param {*} multiValue - Value of this statement within the multi filter (name/value pair)
+   */
+  renderMultiStatement = (filter, statementIndex, multiIndex, total, multiValue) => {
+    return (
+      <React.Fragment key={'rowkey-filter-m' + multiIndex}>
+        {multiIndex > 0 && multiIndex < total && (
+          <Row className="and-row py-2">
+            <Col className="py-0">
+              <b>AND</b>
+            </Col>
+          </Row>
+        )}
+        <Row className="advanced-filter-multi-type-statement m-0">
+          <Col className="p-0">
+            <Form.Group className="form-group-inline py-0 my-0">
+              <Form.Control
+                as="select"
+                value={multiValue.name}
+                className="advanced-filter-multi-options advanced-filter-select py-0 my-0"
+                aria-label="Advanced Filter Multi Select Options"
+                onChange={event => {
+                  this.changeMultiValue(statementIndex, multiIndex, this.getDefaultMultiValue(filter, event.target.value));
+                }}>
+                {filter.fields?.map((field, f_index) => {
+                  return (
+                    <option key={f_index} value={field.name} disabled={this.multiFieldDisabled(field.name, multiIndex, statementIndex)}>
+                      {field.title}
+                    </option>
+                  );
+                })}
+              </Form.Control>
+              {this.getMultiFilter(filter, multiValue.name).type === 'select' && (
+                <Form.Control
+                  as="select"
+                  value={multiValue.value}
+                  className="advanced-filter-multi-select-options advanced-filter-select my-0 mx-3 py-0"
+                  aria-label="Advanced Filter Multi Select Options"
+                  onChange={event => {
+                    this.changeMultiValue(statementIndex, multiIndex, { name: multiValue.name, value: event.target.value });
+                  }}>
+                  {this.getMultiFilter(filter, multiValue.name).options.map((option, o_index) => {
+                    return <option key={o_index}>{option}</option>;
+                  })}
+                </Form.Control>
+              )}
+              {this.getMultiFilter(filter, multiValue.name).type === 'date' && (
+                <React.Fragment>
+                  <Form.Control
+                    as="select"
+                    value={multiValue.value.when}
+                    className="advanced-filter-date-options py-0 my-0 mx-3"
+                    aria-label="Advanced Filter Date Select Options"
+                    onChange={event => {
+                      this.changeMultiValue(statementIndex, multiIndex, {
+                        name: multiValue.name,
+                        value: { when: event.target.value, date: multiValue.value.date },
+                      });
+                    }}>
+                    <option value="before">before</option>
+                    <option value="after">after</option>
+                  </Form.Control>
+                  <div className="advanced-filter-date-input mr-3">
+                    <DateInput
+                      date={multiValue.value.date}
+                      onChange={date => {
+                        this.changeMultiValue(statementIndex, multiIndex, { name: multiValue.name, value: { when: multiValue.value.when, date: date } });
+                      }}
+                      placement="bottom"
+                      customClass="form-control-md"
+                      ariaLabel="Advanced Filter Date Input"
+                      minDate={'1900-01-01'}
+                      maxDate={moment()
+                        .add(2, 'years')
+                        .format('YYYY-MM-DD')}
+                      replaceBlank={true}
+                    />
+                  </div>
+                </React.Fragment>
+              )}
+              {multiIndex + 1 === total && multiIndex + 1 < filter.fields.length && (
+                <React.Fragment>
+                  <div className="my-auto" data-for={`${filter.name}-${statementIndex}-multi-add`} data-tip="">
+                    <Button
+                      className="btn-circle"
+                      variant="secondary"
+                      onClick={() => {
+                        this.addMultiStatement(filter, statementIndex);
+                      }}
+                      aria-label="Add Advanced Filter Multi Option">
+                      <i className="fas fa-plus"></i>
+                    </Button>
+                  </div>
+                  <ReactTooltip id={`${filter.name}-${statementIndex}-multi-add`} multiline={true} place="top" type="dark" effect="solid" className="tooltip-container">
+                    <span>Select to add multiple Lab Result search criteria.</span>
+                  </ReactTooltip>
+                </React.Fragment>
+              )}
+            </Form.Group>
+          </Col>
+          <Col className="p-0" md="auto">
+            {multiIndex === 0 && filter.tooltip && this.renderStatementTooltip(filter.name, statementIndex, filter.tooltip)}
+          </Col>
+          <Col md="auto">{this.renderRemoveStatementButton(statementIndex, multiIndex, this.removeMultiStatement)}</Col>
+        </Row>
+      </React.Fragment>
+    );
+  };
+
+  /**
    * Renders a single line "statement"
    * @param {Object} filterOption - Filter currently selected
    * @param {*} value - Current value for this statement (could be a string, bool, date or object)
@@ -1015,10 +1218,19 @@ class AdvancedFilter extends React.Component {
             {filterOption?.type === 'number' && this.renderNumberStatement(filterOption, index, value, numberOption, additionalFilterOption)}
             {filterOption?.type === 'date' && this.renderDateStatement(index, value, dateOption)}
             {filterOption?.type === 'relative' && this.renderRelativeDateStatement(filterOption, index, value, relativeOption)}
+            {filterOption?.type === 'multi' && (
+              <React.Fragment>
+                {value.map((m_value, m_index) => {
+                  return this.renderMultiStatement(filterOption, index, m_index, value.length, m_value);
+                })}
+              </React.Fragment>
+            )}
           </Col>
-          <Col className="py-0" md="auto">
-            {this.renderRemoveStatementButton(index)}
-          </Col>
+          {filterOption?.type !== 'multi' && (
+            <Col className="py-0" md="auto">
+              {this.renderRemoveStatementButton(index, null, this.removeStatement)}
+            </Col>
+          )}
         </Row>
       </React.Fragment>
     );
