3cfceea1903df8cc380cbd4d9a50660ee5f9c4e3
Matt Mayer <mmayer@mitre.org>
2021-04-29 14:30:10 -0400

    Add delete action and delete modal to lab result

--- app/javascript/components/patient/laboratory/Laboratory.js
+++ app/javascript/components/patient/laboratory/Laboratory.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { PropTypes } from 'prop-types';
-import { Button } from 'react-bootstrap';
+import { Button, Dropdown, Form, Modal } from 'react-bootstrap';
 import axios from 'axios';
-
+import { toast } from 'react-toastify';
+import ReactTooltip from 'react-tooltip';
 import LaboratoryModal from './LaboratoryModal';
 import reportError from '../../util/ReportError';
 
@@ -11,18 +12,60 @@ class Laboratory extends React.Component {
     super(props);
     this.state = {
       showModal: false,
+      showDeleteModal: false,
+      noReasonSelected: true,
+      selectedReason: '',
       loading: false,
     };
   }
 
-  toggleModal = () => {
-    this.setState(state => {
-      return {
-        showModal: !state.showModal,
-        loading: false,
-      };
-    });
-  };
+  createDeleteModal(toggle, submit) {
+    return (
+      <Modal size="lg" show centered onHide={toggle}>
+        <Modal.Header>
+          <Modal.Title>Delete Lab result</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form.Group>
+            <Form.Label>Please Select The Reason Why You Need To Delete This Lab Result</Form.Label>
+            <Form.Control
+              as="select"
+              size="lg"
+              className="form-square"
+              onChange={() => this.setState({ noReasonSelected: false, selectedReason: event.target.value })}>
+              <option disabled selected>
+                {' '}
+                -- Select a Reason --{' '}
+              </option>
+              <option>Duplicate Entry</option>
+              <option>Wrong Entry</option>
+              <option>Other</option>
+            </Form.Control>
+          </Form.Group>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary btn-square" onClick={toggle}>
+            Cancel
+          </Button>
+          <Button variant="primary btn-square" onClick={submit} disabled={this.state.loading || this.state.noReasonSelected}>
+            {this.state.loading && (
+              <React.Fragment>
+                <span className="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>&nbsp;
+              </React.Fragment>
+            )}
+            <span data-for="lab-result-delete" data-tip="">
+              Delete
+            </span>
+            {this.state.noReasonSelected && (
+              <ReactTooltip id="lab-result-delete" multiline={true} place="top" type="dark" effect="solid" className="tooltip-container">
+                <div>Please select a reason for deletion.</div>
+              </ReactTooltip>
+            )}
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  }
 
   submit = lab => {
     this.setState({ loading: true }, () => {
@@ -44,6 +87,29 @@ class Laboratory extends React.Component {
     });
   };
 
+  delete = () => {
+    this.setState({ loading: true }, () => {
+      axios.defaults.headers.common['X-CSRF-Token'] = this.props.authenticity_token;
+      axios
+        .delete(window.BASE_PATH + '/laboratories/' + this.props.lab.id, {
+          data: {
+            patient_id: this.props.patient.id,
+            reason: this.state.selectedReason,
+          },
+        })
+        .then(() => {
+          toast.success('Lab Result successfully deleted.');
+          this.setState({ loading: false, noReasonSelected: true, showDeleteModal: false });
+          location.reload(true);
+        })
+        .catch(error => {
+          toast.error('Failed to delete Lab Result.');
+          this.setState({ loading: false, noReasonSelected: true, showDeleteModal: false });
+          reportError(error);
+        });
+    });
+  };
+
   render() {
     return (
       <React.Fragment>
@@ -54,9 +120,25 @@ class Laboratory extends React.Component {
           </Button>
         )}
         {this.props.lab.id && (
-          <Button variant="link" onClick={() => this.setState({ showModal: true, loading: false })} className="btn btn-link py-0" size="sm">
-            <i className="fas fa-edit"></i> Edit
-          </Button>
+          <Dropdown>
+            <Dropdown.Toggle
+              id={`vaccine-action-button-${this.props.lab.id}`}
+              size="sm"
+              variant="primary"
+              aria-label={`vaccine-action-button-${this.props.lab.id}`}>
+              <i className="fas fa-cogs fw"></i>
+            </Dropdown.Toggle>
+            <Dropdown.Menu className="test-class" drop={'up'}>
+              <Dropdown.Item className="px-4 hi" onClick={() => this.setState({ showModal: true, loading: false })}>
+                <i className="fas fa-edit fa-fw"></i>
+                <span className="ml-2">Edit</span>
+              </Dropdown.Item>
+              <Dropdown.Item className="px-4 hi" onClick={() => this.setState({ showDeleteModal: true, loading: false })}>
+                <i className="fas fa-trash fa-fw"></i>
+                <span className="ml-2">Delete</span>
+              </Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
         )}
         {this.state.showModal && (
           <LaboratoryModal
@@ -67,6 +149,7 @@ class Laboratory extends React.Component {
             loading={this.state.loading}
           />
         )}
+        {this.state.showDeleteModal && this.createDeleteModal(() => this.setState({ showDeleteModal: false }), this.delete)}
       </React.Fragment>
     );
   }
