27ca7f5116b8917e73be247df9e0e3c15586c5b9
Kyle <Bialogs@users.noreply.github.com>
2021-05-05 07:52:46 -0700

    Notify a user of an export that had no patients matching export criteria. Notify a user if a worksheet has no data matching export criteria. Add test for download email when there are no lookups. (#847)

---
+++ test/mailers/user_mailer_test.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require 'test_case'
+
+class UserMailerTest < ActionMailer::TestCase
+  def setup
+    @user = create(:user)
+  end
+
+  test 'download email no lookups' do
+    # When no monitorees match export criteria lookups = []
+    email = UserMailer.download_email(@user, 'Export Label', [], 10_000).deliver_now
+    email_body = email.parts.first.body.to_s.gsub("\n", ' ')
+    assert_not(ActionMailer::Base.deliveries.empty?)
+    assert_includes(email_body, 'Export Label')
+    assert_includes(email_body, 'no monitorees or monitoree data matched the selected export criteria')
+  end
+end
