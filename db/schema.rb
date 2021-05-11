26376e409b72995e1c488231a42f10a4d9225cae
Sarah G Ober <sgober@mitre.org>
2021-05-11 14:57:11 -0400

    update migration to be the latest

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_08_141314) do
+ActiveRecord::Schema.define(version: 2021_05_11_185510) do
 
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
