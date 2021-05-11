69e11d9b3ac1f7db5e91ecf642594df45a7cc5d2
Sarah G Ober <sgober@mitre.org>
2021-05-11 09:45:39 -0400

    updated styling

--- app/javascript/components/patient/history/HistoryList.js
+++ app/javascript/components/patient/history/HistoryList.js
@@ -120,7 +120,7 @@ class HistoryList extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <Card id="histories" className="mx-2 mt-3 mb-4 card-square">
+        <Card id="histories" className="mx-2 my-4 card-square">
           <Card.Header>
             <div className="d-flex flex-row align-items-center">
               <div className="float-left flex-grow-1 mb-0 h5">
