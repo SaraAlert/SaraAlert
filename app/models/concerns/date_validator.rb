74490c46b730caab2eead8d1c7c285b016ca3486
Kyle Fagan <kfagan@mitre.org>
2021-05-28 09:59:12 -0700

    DateValidator Improvements
    Add an error to the validated Object when presented with an object that is
    either a Numeric or does not respond to #match?
    
    Use String#match? for performance improvement.
    
    Anchor Regular Expression for correctness improvement.

--- app/models/concerns/date_validator.rb
+++ app/models/concerns/date_validator.rb
@@ -4,18 +4,20 @@
 class DateValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     err_msg = 'is not a valid date'
-
     # If we can, validate using the pre-type cast value, since this will more accurately reflect user input
     value = record.public_send("#{attribute}_before_type_cast") || value
-    if value.is_a? Numeric
+    # Blank values are accepted.
+    return if value.blank?
+
+    # Numeric dates do not conform to import or API guidance
+    # Strings should respond to match and conform to import and API guidance
+    if value.is_a?(Numeric) || !value.respond_to?(:match?)
       record.errors.add(attribute, "#{err_msg}, please use the 'YYYY-MM-DD' format")
       return
     end
 
-    return if value.blank? || !value.respond_to?(:match)
-
-    unless value.match(/\d{4}-\d{2}-\d{2}/)
-      if value.match(%r{\d{2}/\d{2}/\d{4}})
+    unless value.match?(/^\d{4}-\d{2}-\d{2}$/)
+      if value.match?(%r{^\d{2}/\d{2}/\d{4}$})
         record.errors.add(attribute, "#{err_msg} due to ambiguity between 'MM/DD/YYYY' and 'DD/MM/YYYY', please use the 'YYYY-MM-DD' format instead")
       else
         record.errors.add(attribute, "#{err_msg}, please use the 'YYYY-MM-DD' format")
