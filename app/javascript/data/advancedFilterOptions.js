dff33a08a0e6c233372579a341d55dc4eacc9f87
Becky Hack <rhack@mitre.org>
2021-06-18 10:00:17 -0400

    Including flag reason to advanced filter that was missed

--- app/javascript/data/advancedFilterOptions.js
+++ app/javascript/data/advancedFilterOptions.js
@@ -190,7 +190,7 @@ export const advancedFilterOptions = [
     title: 'Flagged for Follow-up (Select)',
     description: 'Monitoree flagged for follow-up',
     type: 'select',
-    options: ['Any Reason', 'Deceased', 'Duplicate', 'High-Risk', 'Hospitalized', 'In Need of Follow-up', 'Lost to Follow-up', 'Needs Interpretation', 'Quality Assurance', 'Other'],
+    options: ['Any Reason', 'Deceased', 'Duplicate', 'High-Risk', 'Hospitalized', 'In Need of Follow-up', 'Lost to Follow-up', 'Needs Interpretation', 'Quality Assurance', 'Refused Active Monitoring', 'Other'],
     tooltip: 'This will return monitorees that are flagged for follow-up for the selected reason. To return all montitorees flagged for follow-up, select the “Any Reason” option.'
   },
 
