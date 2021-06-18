793f03a606a576436b313b737a074e28821f57c8
David Connolly <dpconnolly@mitre.org>
2021-06-18 11:59:47 -0400

    added aria-label to the header of the reports table

--- app/javascript/components/patient/assessment/actions/CurrentStatus.js
+++ app/javascript/components/patient/assessment/actions/CurrentStatus.js
@@ -12,43 +12,43 @@ class CurrentStatus extends React.Component {
   generateStatus(status) {
     if (status === 'exposure_symptomatic') {
       return (
-        <Badge variant="danger" className="badge-larger-font" aria-label="Notification Status Exposure Symptomatic">
+        <Badge variant="danger" className="badge-larger-font" aria-label="Workflow: Exposure. Linelist: Symptomatic">
           symptomatic
         </Badge>
       );
     } else if (status === 'exposure_asymptomatic') {
       return (
-        <Badge variant="success" className="badge-larger-font" aria-label="Notification Status Exposure Asymptomatic">
+        <Badge variant="success" className="badge-larger-font" aria-label="Workflow: Exposure. Linelist: Asymptomatic">
           asymptomatic
         </Badge>
       );
     } else if (status === 'exposure_non_reporting') {
       return (
-        <Badge variant="warning" className="badge-larger-font" aria-label="Notification Status Exposure Non Reporting">
+        <Badge variant="warning" className="badge-larger-font" aria-label="Workflow: Exposure. Linelist: Non Reporting">
           non-reporting
         </Badge>
       );
     } else if (status === 'exposure_under_investigation') {
       return (
-        <Badge variant="dark" className="badge-larger-font" aria-label="Notification Status Exposure Under Investigation">
+        <Badge variant="dark" className="badge-larger-font" aria-label="Workflow: Exposure. Linelist: Under Investigation">
           PUI
         </Badge>
       );
     } else if (status === 'purged') {
       return (
-        <Badge className="badge-muted badge-larger-font" aria-label="Notification Status Purged">
+        <Badge className="badge-muted badge-larger-font" aria-label="Workflow: Purged.">
           purged
         </Badge>
       );
     } else if (status === 'closed') {
       return (
-        <Badge variant="secondary" className="badge-larger-font" aria-label="Notification Status Closed">
+        <Badge variant="secondary" className="badge-larger-font" aria-label="Workflow: Closed.">
           not currently being monitored
         </Badge>
       );
     } else if (status === 'isolation_requiring_review') {
       return (
-        <Badge variant="danger" className="badge-larger-font" aria-label="Notification Status Isolation Requires Review">
+        <Badge variant="danger" className="badge-larger-font" aria-label="Workflow: Isolation. Linelist: Requires Review.">
           requires review
         </Badge>
       );
@@ -57,7 +57,7 @@ class CurrentStatus extends React.Component {
         <Badge
           variant="danger"
           className="badge-larger-font"
-          aria-label="Notification Status Isolation Symptomatic Non Test Based"
+          aria-label="Workflow: Isolation. Linelist: Symptomatic Non Test Based."
           data-for={`symptomatic-non-test-based`}
           data-tip="">
           requires review (symptomatic non test based)
@@ -68,7 +68,7 @@ class CurrentStatus extends React.Component {
         <Badge
           variant="danger"
           className="badge-larger-font"
-          aria-label="Notification Status Isolation Asymptomatic Non Test Based"
+          aria-label="Workflow: Isolation. Linelist: Asymptomatic Non Test Based."
           data-for={`aymptomatic-non-test-based`}
           data-tip="">
           requires review (asymptomatic non test based)
@@ -76,19 +76,19 @@ class CurrentStatus extends React.Component {
       );
     } else if (status === 'isolation_test_based') {
       return (
-        <Badge variant="danger" className="badge-larger-font" aria-label="Notification Status Isolation Test Based" data-for={`test-based`} data-tip="">
+        <Badge variant="danger" className="badge-larger-font" aria-label="Workflow: Isolation. Linelist: Test Based." data-for={`test-based`} data-tip="">
           requires review (test based)
         </Badge>
       );
     } else if (status === 'isolation_non_reporting') {
       return (
-        <Badge variant="warning" className="badge-larger-font" aria-label="Notification Status Isolation Non Reporting">
+        <Badge variant="warning" className="badge-larger-font" aria-label="Workflow: Isolation. Linelist: Non Reporting.">
           non-reporting
         </Badge>
       );
     } else if (status === 'isolation_reporting') {
       return (
-        <Badge variant="success" className="badge-larger-font" aria-label="Notification Status Isolation Reporting">
+        <Badge variant="success" className="badge-larger-font" aria-label="Workflow: Isolation. Linelist: Reporting.">
           reporting
         </Badge>
       );
@@ -132,6 +132,19 @@ class CurrentStatus extends React.Component {
     }
   }
 
+  getHeaderLabel = () => {
+    let eligibilityStatus = this.props.report_eligibility.household
+      ? 'In a household'
+      : this.props.report_eligibility.eligible
+      ? 'Eligible for notifications'
+      : this.props.report_eligibility.reported
+      ? 'Received a report today'
+      : this.props.report_eligibility.sent
+      ? 'Waiting for a response'
+      : 'Not eligible for notifications';
+    return ` Notification Status is: ${eligibilityStatus}`;
+  };
+
   render() {
     return (
       <React.Fragment>
@@ -141,7 +154,7 @@ class CurrentStatus extends React.Component {
           </b>
           {this.generateStatusTooltip(this.props.status)}
           <span className="b-border-right-3 pl-3"></span>
-          <b className="pl-3">
+          <b className="pl-3" aria-label={this.getHeaderLabel()}>
             Notification status is <EligibilityTooltip report_eligibility={this.props.report_eligibility} id={`eltt`} inline={true} />
           </b>
         </h4>
