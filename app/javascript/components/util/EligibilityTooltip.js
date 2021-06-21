54309759ee0cd81309f88142aca50424dc5b4735
David Connolly <dpconnolly@mitre.org>
2021-06-21 16:45:26 -0400

    optimized the eligibilitytooltip file

--- app/javascript/components/util/EligibilityTooltip.js
+++ app/javascript/components/util/EligibilityTooltip.js
@@ -1,136 +1,62 @@
 import React from 'react';
 import { PropTypes } from 'prop-types';
+import _ from 'lodash';
 
 import { formatTimestamp } from '../../utils/DateTime';
 import ReactTooltip from 'react-tooltip';
 
+const eligibility_options = [
+  { conditional: 'household', icon: 'fa-house-user', title: 'In a household:', messageCount: 1 },
+  { conditional: 'eligible', icon: 'fa-comment', title: 'Eligible for notifications:', messageCount: 1 },
+  { conditional: 'reported', icon: 'fa-comments', title: 'Received a report today:', messageCount: 'all' },
+  { conditional: 'sent', icon: 'fa-comment-dots', title: 'Waiting for a response:', messageCount: 'all' },
+  // This bottom entry is the default. If none of the `conditional` fields are truthy within props.report_eligibility, the bottom will render
+  { conditional: '', icon: 'fa-comment-slash', title: 'Not eligible for notifications:', messageCount: 'all' },
+];
+
 class EligibilityTooltip extends React.Component {
   constructor(props) {
     super(props);
-  }
-
-  formatEligibility(eligibility, id) {
-    if (eligibility.household) {
-      return (
-        <React.Fragment>
-          <span key={`re-icon${id}`} data-for={`re${id}`} data-tip="">
-            {this.props.inline && <i className="fa-fw fas fa-house-user"></i>}
-            {!this.props.inline && (
-              <div className="text-center ml-0">
-                <i className="fa-fw fas fa-house-user"></i>
-              </div>
-            )}
-          </span>
-          <ReactTooltip key={`re-tooltip${id}`} id={`re${id}`} multiline={true} type="dark" effect="solid" className="tooltip-container">
-            <div>
-              <p className="lead mb-0">In a household:</p>
-              <span>{eligibility.messages[0].message}</span>
-            </div>
-          </ReactTooltip>
-        </React.Fragment>
-      );
-    } else if (eligibility.eligible) {
-      return (
-        <React.Fragment>
-          <span key={`re-icon${id}`} data-for={`re${id}`} data-tip="">
-            {this.props.inline && <i className="fa-fw fas fa-comment"></i>}
-            {!this.props.inline && (
-              <div className="text-center ml-0">
-                <i className="fa-fw fas fa-comment"></i>
-              </div>
-            )}
-          </span>
-          <ReactTooltip key={`re-tooltip${id}`} id={`re${id}`} multiline={true} type="dark" effect="solid" className="tooltip-container">
-            <div>
-              <p className="lead mb-0">Eligible for notifications:</p>
-              <span>{eligibility.messages[0].message}</span>
-            </div>
-          </ReactTooltip>
-        </React.Fragment>
-      );
-    } else if (eligibility.reported) {
-      return (
-        <React.Fragment>
-          <span key={`re-icon${id}`} data-for={`re${id}`} data-tip="">
-            {this.props.inline && <i className="fa-fw fas fa-comments"></i>}
-            {!this.props.inline && (
-              <div className="text-center ml-0">
-                <i className="fa-fw fas fa-comments"></i>
-              </div>
-            )}
-          </span>
-          <ReactTooltip key={`re-tooltip${id}`} id={`re${id}`} multiline={true} type="dark" effect="solid" className="tooltip-container">
-            <div>
-              <p className="lead mb-0">Received a report today:</p>
-              <ul className="pl-3 mb-0">
-                {eligibility.messages.map((m, index) => (
-                  <li className="mb-0" key={`rei${id}${index}`}>
-                    {m.message}
-                    {m.datetime ? ` (${formatTimestamp(m.datetime)})` : ''}
-                  </li>
-                ))}
-              </ul>
-            </div>
-          </ReactTooltip>
-        </React.Fragment>
-      );
-    } else if (eligibility.sent) {
-      return (
-        <React.Fragment>
-          <span key={`re-icon${id}`} data-for={`re${id}`} data-tip="">
-            {this.props.inline && <i className="fa-fw fas fa-comment-dots"></i>}
-            {!this.props.inline && (
-              <div className="text-center ml-0">
-                <i className="fa-fw fas fa-comment-dots"></i>
-              </div>
-            )}
-          </span>
-          <ReactTooltip key={`re-tooltip${id}`} id={`re${id}`} multiline={true} type="dark" effect="solid" className="tooltip-container">
-            <div>
-              <p className="lead mb-0">Waiting for a response:</p>
-              <ul className="pl-3 mb-0">
-                {eligibility.messages.map((m, index) => (
-                  <li className="mb-0" key={`rei${id}${index}`}>
-                    {m.message}
-                    {m.datetime ? ` (${formatTimestamp(m.datetime)})` : ''}
-                  </li>
-                ))}
-              </ul>
-            </div>
-          </ReactTooltip>
-        </React.Fragment>
-      );
-    } else {
-      return (
-        <React.Fragment>
-          <span key={`re-icon${id}`} data-for={`re${id}`} data-tip="">
-            {this.props.inline && <i className="fa-fw fas fa-comment-slash"></i>}
-            {!this.props.inline && (
-              <div className="text-center ml-0">
-                <i className="fa-fw fas fa-comment-slash"></i>
-              </div>
-            )}
-          </span>
-          <ReactTooltip key={`re-tooltip${id}`} id={`re${id}`} multiline={true} type="dark" effect="solid" className="tooltip-container">
-            <div>
-              <p className="lead mb-0">Not eligible for notifications:</p>
-              <ul className="pl-3 mb-0">
-                {eligibility.messages.map((m, index) => (
-                  <li className="mb-0" key={`rei${id}${index}`}>
-                    {m.message}
-                    {m.datetime ? ` (${formatTimestamp(m.datetime)})` : ''}
-                  </li>
-                ))}
-              </ul>
-            </div>
-          </ReactTooltip>
-        </React.Fragment>
-      );
-    }
+    let ativeOption = eligibility_options.find(eo => props.report_eligibility[`${eo.conditional}`]) || _.last(eligibility_options);
+    ativeOption.messages = ativeOption.messageCount === 1 ? [this.props.report_eligibility.messages[0]] : this.props.report_eligibility.messages;
+    this.state = {
+      ativeOption,
+    };
   }
 
   render() {
-    return <React.Fragment>{this.formatEligibility(this.props.report_eligibility, this.props.id)}</React.Fragment>;
+    return (
+      <React.Fragment>
+        <span key={`re-icon${this.props.id}`} data-for={`re${this.props.id}`} data-tip="">
+          {this.props.inline && <i className={`fa-fw fas ${this.state.ativeOption.icon}`}></i>}
+          {!this.props.inline && (
+            <div className="text-center ml-0">
+              <i className={`fa-fw fas ${this.state.ativeOption.icon}`}></i>
+            </div>
+          )}
+        </span>
+        <ReactTooltip key={`re-tooltip${this.props.id}`} id={`re${this.props.id}`} multiline={true} type="dark" effect="solid" className="tooltip-container">
+          <div>
+            <p className="lead mb-0">{this.state.ativeOption.title}</p>
+            {this.state.ativeOption.messageCount === 1 ? (
+              <span aria-label={this.state.ativeOption.messages[0].message}>{this.state.ativeOption.messages[0].message}</span>
+            ) : (
+              <ul className="pl-3 mb-0">
+                {this.state.ativeOption.messages.map((m, index) => {
+                  const message = m.datetime ? `${m.message} (${formatTimestamp(m.datetime)})` : m.message;
+                  return (
+                    <li className="mb-0" key={`rei${this.props.id}${index}`} aria-label={message}>
+                      {' '}
+                      {message}{' '}
+                    </li>
+                  );
+                })}
+              </ul>
+            )}
+          </div>
+        </ReactTooltip>
+      </React.Fragment>
+    );
   }
 }
 
