aa6f1ece48afec49d5c692c13d3c68f29ef47f4e
Justin Kufro <jkufro@mitre.org>
2021-07-06 14:21:27 -0600

    close_patients_job: batch groups and bulk import histories

--- app/jobs/close_patients_job.rb
+++ app/jobs/close_patients_job.rb
@@ -31,69 +31,73 @@ class ClosePatientsJob < ApplicationJob
   def perform_batch(patients, monitoring_reason, jurisdiction_send_close, completed_message: false)
     closed = []
     not_closed = []
-    count = 0
+    histories = []
 
     # Close patients who are past the monitoring period (and are actually closable from above logic)
-    patients.each do |patient|
-      count += 1
-      # Update related fields
-      patient[:monitoring] = false
-      patient[:closed_at] = DateTime.now
-      patient[:monitoring_reason] = monitoring_reason
-      patient.save!
+    patients.find_in_batches(batch_size: 15_000).each do |group|
+      group.each do |patient|
+        # Update related fields
+        patient[:monitoring] = false
+        patient[:closed_at] = DateTime.now
+        patient[:monitoring_reason] = monitoring_reason
+        patient.save!
 
-      # Send closed notification to patient if they are a reporter, the closure reason warrants a close
-      # notification, and if the jurisdiction has opted-in to closed notifications.
-      #
-      # If 'sms texted weblink' or 'sms text-message' is preferred, then an SMS will be sent
-      # If 'e-mailed web link' is preferred, then an email will be sent
-      # If the preferred contact method is not supported for close notifications, then a history item will
-      # be created stating so.
-      #
-      # We are checking some of the same crieria here as in the patient mailer for closed messages.
-      # This is intentionally done to avoid enqueuing mail that is "destined to fail", as well as to cover the case
-      # where the patient record is modified between being enqueued and the mailer job actually being executed.
-      if completed_message && patient.self_reporter_or_proxy? && jurisdiction_send_close[patient.jurisdiction_id]
-        contact_method = patient.preferred_contact_method&.downcase
-        if ['sms texted weblink', 'sms text-message'].include? contact_method
-          # Do not enqueue if the contact method is blank or if SMS is blocked
-          if patient.blocked_sms
-            History.send_close_sms_blocked(patient: patient)
-          elsif patient.primary_telephone.blank?
-            History.send_close_conact_method_blank(patient: patient, type: 'primary phone number')
+        # Send closed notification to patient if they are a reporter, the closure reason warrants a close
+        # notification, and if the jurisdiction has opted-in to closed notifications.
+        #
+        # If 'sms texted weblink' or 'sms text-message' is preferred, then an SMS will be sent
+        # If 'e-mailed web link' is preferred, then an email will be sent
+        # If the preferred contact method is not supported for close notifications, then a history item will
+        # be created stating so.
+        #
+        # We are checking some of the same crieria here as in the patient mailer for closed messages.
+        # This is intentionally done to avoid enqueuing mail that is "destined to fail", as well as to cover the case
+        # where the patient record is modified between being enqueued and the mailer job actually being executed.
+        if completed_message && patient.self_reporter_or_proxy? && jurisdiction_send_close[patient.jurisdiction_id]
+          contact_method = patient.preferred_contact_method&.downcase
+          if ['sms texted weblink', 'sms text-message'].include? contact_method
+            # Do not enqueue if the contact method is blank or if SMS is blocked
+            if patient.blocked_sms
+              histories << History.send_close_sms_blocked(patient: patient, create: false)
+            elsif patient.primary_telephone.blank?
+              histories << History.send_close_conact_method_blank(patient: patient, type: 'primary phone number', create: false)
+            else
+              PatientMailer.closed_sms(patient).deliver_later(wait_until: patient.time_to_notify_closed)
+            end
+          elsif contact_method == 'e-mailed web link'
+            # Do not enqueue if the contact method is blank
+            if patient.email.blank?
+              histories << History.send_close_conact_method_blank(patient: patient, type: 'email', create: false)
+            else
+              PatientMailer.closed_email(patient).deliver_later(wait_until: patient.time_to_notify_closed)
+            end
           else
-            PatientMailer.closed_sms(patient).deliver_later(wait_until: patient.time_to_notify_closed)
+            history_friendly_method = patient.preferred_contact_method.blank? ? patient.preferred_contact_method : 'Unknown'
+            histories << History.monitoring_complete_message_sent(
+              patient: patient,
+              comment: 'The system was unable to send a monitoring complete message to this monitoree because their'\
+                       "preferred contact method, #{history_friendly_method}, is not supported for this message type.",
+              create: false
+            )
           end
-        elsif contact_method == 'e-mailed web link'
-          # Do not enqueue if the contact method is blank
-          if patient.email.blank?
-            History.send_close_conact_method_blank(patient: patient, type: 'email')
-          else
-            PatientMailer.closed_email(patient).deliver_later(wait_until: patient.time_to_notify_closed)
-          end
-        else
-          history_friendly_method = patient.preferred_contact_method.blank? ? patient.preferred_contact_method : 'Unknown'
-          History.monitoring_complete_message_sent(
-            patient: patient,
-            comment: 'The system was unable to send a monitoring complete message to this monitoree because their'\
-                     "preferred contact method, #{history_friendly_method}, is not supported for this message type."
-          )
         end
+
+        # History item for automatically closing the record
+        histories << History.record_automatically_closed(patient: patient, create: false)
+
+        closed << { id: patient.id }
+      rescue StandardError => e
+        not_closed << { id: patient.id, reason: e.message }
+        next
       end
-
-      # History item for automatically closing the record
-      History.record_automatically_closed(patient: patient)
-
-      closed << { id: patient.id }
-    rescue StandardError => e
-      not_closed << { id: patient.id, reason: e.message }
-      next
     end
+
+    History.import! histories
 
     {
       closed: closed,
       not_closed: not_closed,
-      count: count
+      count: closed.size + not_closed.size
     }
   end
 
