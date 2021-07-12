9a062a31eb55fdfca613346f33bddfdec5110824
Tom Strassner <tstrassner@mitre.org>
2021-07-12 09:51:12 -0400

    Fix issue with threshold hash migration (#978)
    This migration depends on previous migrations that add columns to the
    jurisdictions table. Therefore, you need to reload the class so that it
    doesn't attempt to use the cached version, which doesn't have the
    columns added by previous migrations, in the event that you run all the
    migrations at once (e.g. with db:migrate:reset).

--- db/migrate/20210616203622_add_threshold_hash_to_jurisdictions.rb
+++ db/migrate/20210616203622_add_threshold_hash_to_jurisdictions.rb
@@ -2,6 +2,8 @@ class AddThresholdHashToJurisdictions < ActiveRecord::Migration[6.1]
   def up
     add_column :jurisdictions, :current_threshold_condition_hash, :string
 
+    Jurisdiction.reset_column_information
+
     # Run admin:import_or_update_jurisdictions task without checks to ensure that threshold condition with :current_threshold_condition_hash exists
     ActiveRecord::Base.transaction do
       config_contents = YAML.load_file('config/sara/jurisdictions.yml')
