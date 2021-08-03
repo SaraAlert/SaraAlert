6c0e612989fb1cceecf9045e5ac217c25c994398
ashankland <ashankland@users.noreply.github.com>
2021-08-03 10:20:34 -0400

    SARAALERT-1509: When a symptom report is edited, include the change in "Reporter" in the resulting history entry (#995)
    * initial commit, adds 'Report Updated' and before and after IDs to report update histories, appended to the end of the comment
    
    * added test to check conditional reporter history updates
    
    * removed unused variable from the AssessmentTable
    
    * updated language of history to match other history items of a similar type, removed incomplete functionality for determining if assessment has been edited
    
    * Tweak wording of assessment edit history comment
    
    * Update assessment history verifier for changed wording
    
    Co-authored-by: ashankland <ashankland@mitre.org>
    Co-authored-by: Tom Strassner <tstrassner@mitre.org>

--- app/controllers/assessments_controller.rb
+++ app/controllers/assessments_controller.rb
@@ -213,14 +213,18 @@ class AssessmentsController < ApplicationController
       end
 
       assessment.symptomatic = assessment.symptomatic?
+      old_reporter = assessment.who_reported
       # Monitorees can't edit their own assessments, so the last person to touch this assessment was current_user
       assessment.who_reported = current_user.email
 
       # Attempt to save and continue; else if failed redirect to index
       return unless assessment.save
 
-      comment = 'User updated an existing report (ID: ' + assessment.id.to_s + ').'
-      comment += ' Symptom updates: ' + delta.join(', ') + '.' unless delta.empty?
+      comment = 'User edited an existing report (ID: ' + assessment.id.to_s + ').'
+      unless delta.empty?
+        comment += ' Symptom updates: ' + delta.join(', ') + '.'
+        comment += " Reporter update: (\"#{old_reporter}\" to \"#{current_user.email}\")." unless old_reporter == current_user.email
+      end
       History.report_updated(patient: patient, created_by: current_user.email, comment: comment)
     end
   end
