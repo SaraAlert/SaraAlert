daa761a2caadc3bde59bfe1ae0dee31629a398ac
Tim Wong <twong@mitre.org>
2021-08-05 05:08:19 -0700

    SARAALERT-1559: Do not allow the future-dating of Symptom Onset Date (#1018)
    * do not allow future symptom onset dates
    
    * add so validations to other so inputs in ui
    
    * only enforce model validation for symptom onset
    
    * prevent user from selecting future so date in enrollment
    
    * fix system tests

--- app/models/concerns/patient_date_validator.rb
+++ app/models/concerns/patient_date_validator.rb
@@ -6,15 +6,20 @@ class PatientDateValidator < ActiveModel::Validator
 
   def validate(record)
     year_start = Date.new(2020, 1, 1)
-    month_ahead = 30.days.from_now.to_date
-    month_behind = 30.days.ago.to_date
-    validate_between_dates(record, :date_of_birth, Date.new(1900, 1, 1), Time.now.to_date) if record.date_of_birth_changed?
-    validate_between_dates(record, :last_date_of_exposure, year_start, month_ahead) if record.last_date_of_exposure_changed?
-    validate_between_dates(record, :symptom_onset, year_start, month_ahead) if record.symptom_onset_changed?
-    validate_between_dates(record, :extended_isolation, month_behind, month_ahead) if record.extended_isolation_changed?
-    validate_between_dates(record, :date_of_departure, year_start, month_ahead) if record.date_of_departure_changed?
-    validate_between_dates(record, :date_of_arrival, year_start, month_ahead) if record.date_of_arrival_changed?
-    validate_between_dates(record, :additional_planned_travel_start_date, year_start, month_ahead) if record.additional_planned_travel_start_date_changed?
-    validate_between_dates(record, :additional_planned_travel_end_date, year_start, month_ahead) if record.additional_planned_travel_end_date_changed?
+    # NOTE: Commented out for rubocop until other validations are uncommented
+    # month_ahead = 30.days.from_now.to_date
+    # month_behind = 30.days.ago.to_date
+    # This is to handle the edge case when certain time zones such as Samoa are on the new day before UTC
+    earliest_tz_today = Time.now.in_time_zone(ActiveSupport::TimeZone.all.last).to_date
+    validate_between_dates(record, :symptom_onset, year_start, earliest_tz_today) if record.symptom_onset_changed?
+
+    # NOTE: Commented out until additional testing
+    # validate_between_dates(record, :date_of_birth, Date.new(1900, 1, 1), earliest_tz_today) if record.date_of_birth_changed?
+    # validate_between_dates(record, :last_date_of_exposure, year_start, month_ahead) if record.last_date_of_exposure_changed?
+    # validate_between_dates(record, :extended_isolation, month_behind, month_ahead) if record.extended_isolation_changed?
+    # validate_between_dates(record, :date_of_departure, year_start, month_ahead) if record.date_of_departure_changed?
+    # validate_between_dates(record, :date_of_arrival, year_start, month_ahead) if record.date_of_arrival_changed?
+    # validate_between_dates(record, :additional_planned_travel_start_date, year_start, month_ahead) if record.additional_planned_travel_start_date_changed?
+    # validate_between_dates(record, :additional_planned_travel_end_date, year_start, month_ahead) if record.additional_planned_travel_end_date_changed?
   end
 end
--- test/system/roles/enroller/enrollment/form_validator.rb
+++ test/system/roles/enroller/enrollment/form_validator.rb
@@ -21,63 +21,63 @@ class EnrollmentFormValidator < ApplicationSystemTestCase
 
   def verify_input_validation_for_identification(identification)
     @@system_test_utils.go_to_next_page
-    verify_text_displayed('Please enter a First Name')
-    verify_text_displayed('Please enter a Last Name')
-    verify_text_displayed('Please enter a Date of Birth')
+    page.assert_text('Please enter a First Name')
+    page.assert_text('Please enter a Last Name')
+    page.assert_text('Please enter a Date of Birth')
     @@enrollment_form.populate_enrollment_step(:identification, identification)
-    verify_text_not_displayed('Please enter a First Name')
-    verify_text_not_displayed('Please enter a Last Name')
-    verify_text_not_displayed('Please enter a Date of Birth')
+    page.assert_no_text('Please enter a First Name')
+    page.assert_no_text('Please enter a Last Name')
+    page.assert_no_text('Please enter a Date of Birth')
   end
 
   def verify_input_validation_for_address(address)
     @@system_test_utils.go_to_next_page
-    verify_text_displayed('Please enter first line of address')
-    verify_text_displayed('Please enter city of address')
-    verify_text_displayed('Please enter state of address')
-    verify_text_displayed('Please enter zip code of address')
-    verify_text_not_displayed('Please enter country of address')
+    page.assert_text('Please enter first line of address')
+    page.assert_text('Please enter city of address')
+    page.assert_text('Please enter state of address')
+    page.assert_text('Please enter zip code of address')
+    page.assert_no_text('Please enter country of address')
     click_on 'Home Address Outside USA (Foreign)'
     click_on 'Next'
-    verify_text_not_displayed('Please enter first line of address')
-    verify_text_displayed('Please enter city of address')
-    verify_text_not_displayed('Please enter state of address')
-    verify_text_not_displayed('Please enter zip code of address')
-    verify_text_displayed('Please enter country of address')
+    page.assert_no_text('Please enter first line of address')
+    page.assert_text('Please enter city of address')
+    page.assert_no_text('Please enter state of address')
+    page.assert_no_text('Please enter zip code of address')
+    page.assert_text('Please enter country of address')
     click_on 'Home Address Within USA'
     @@enrollment_form.populate_enrollment_step(:address, address)
-    verify_text_not_displayed('Please enter first line of address')
-    verify_text_not_displayed('Please enter city of address')
-    verify_text_not_displayed('Please enter state of address')
-    verify_text_not_displayed('Please enter zip code of address')
-    verify_text_not_displayed('Please enter country of address')
+    page.assert_no_text('Please enter first line of address')
+    page.assert_no_text('Please enter city of address')
+    page.assert_no_text('Please enter state of address')
+    page.assert_no_text('Please enter zip code of address')
+    page.assert_no_text('Please enter country of address')
   end
 
   def verify_input_validation_for_contact_information(contact_information)
     select 'Telephone call', from: 'preferred_contact_method'
     click_on 'Next'
-    verify_text_not_displayed('Please provide an Email or change Preferred Reporting Method')
-    verify_text_not_displayed('Please confirm Email')
-    verify_text_displayed('Please provide a Primary Telephone Number, or change Preferred Reporting Method.')
+    page.assert_no_text('Please provide an Email or change Preferred Reporting Method')
+    page.assert_no_text('Please confirm Email')
+    page.assert_text('Please provide a Primary Telephone Number, or change Preferred Reporting Method.')
     select 'SMS Text-message', from: 'preferred_contact_method'
     click_on 'Next'
-    verify_text_not_displayed('Please provide an Email or change Preferred Reporting Method')
-    verify_text_not_displayed('Please confirm Email')
-    verify_text_displayed('Please provide a Primary Telephone Number, or change Preferred Reporting Method.')
+    page.assert_no_text('Please provide an Email or change Preferred Reporting Method')
+    page.assert_no_text('Please confirm Email')
+    page.assert_text('Please provide a Primary Telephone Number, or change Preferred Reporting Method.')
     select 'E-mailed Web Link', from: 'preferred_contact_method'
     click_on 'Next'
-    verify_text_displayed('Please provide an Email or change Preferred Reporting Method')
-    verify_text_displayed('Please confirm Email')
-    verify_text_not_displayed('Please provide a Primary Telephone Number, or change Preferred Reporting Method.')
+    page.assert_text('Please provide an Email or change Preferred Reporting Method')
+    page.assert_text('Please confirm Email')
+    page.assert_no_text('Please provide a Primary Telephone Number, or change Preferred Reporting Method.')
     fill_in 'email', with: 'email@eample.com'
     click_on 'Next'
-    verify_text_not_displayed('Please provide an Email or change Preferred Reporting Method')
-    verify_text_displayed('Please confirm Email')
-    verify_text_not_displayed('Please provide a Primary Telephone Number, or change Preferred Reporting Method.')
+    page.assert_no_text('Please provide an Email or change Preferred Reporting Method')
+    page.assert_text('Please confirm Email')
+    page.assert_no_text('Please provide a Primary Telephone Number, or change Preferred Reporting Method.')
     @@enrollment_form.populate_enrollment_step(:contact_information, contact_information)
-    verify_text_not_displayed('Please provide an Email or change Preferred Reporting Method')
-    verify_text_not_displayed('Please confirm Email')
-    verify_text_not_displayed('Please provide a Primary Telephone Number, or change Preferred Reporting Method.')
+    page.assert_no_text('Please provide an Email or change Preferred Reporting Method')
+    page.assert_no_text('Please confirm Email')
+    page.assert_no_text('Please provide a Primary Telephone Number, or change Preferred Reporting Method.')
   end
 
   def verify_input_validation_for_arrival_information(arrival_information)
@@ -90,23 +90,23 @@ class EnrollmentFormValidator < ApplicationSystemTestCase
 
   def verify_input_validation_for_potential_exposure_information(potential_exposure_information)
     @@system_test_utils.go_to_next_page
-    verify_text_displayed('Please enter a Last Date of Exposure OR turn on Continuous Exposure')
+    page.assert_text('Please enter a Last Date of Exposure OR turn on Continuous Exposure')
     fill_in 'last_date_of_exposure', with: 5.days.ago.strftime('%m/%d/%Y')
     fill_in 'jurisdiction_id', with: '' # clear out jurisdiction to so that there is at least one validation error
     click_on 'Next'
-    verify_text_not_displayed('Please enter a Last Date of Exposure OR turn on Continuous Exposure')
+    page.assert_no_text('Please enter a Last Date of Exposure OR turn on Continuous Exposure')
     fill_in 'last_date_of_exposure', with: ''
     click_on 'Next'
-    verify_text_displayed('Please enter a Last Date of Exposure OR turn on Continuous Exposure')
+    page.assert_text('Please enter a Last Date of Exposure OR turn on Continuous Exposure')
     page.find('label', text: 'CONTINUOUS EXPOSURE').click
-    verify_text_not_displayed('Please enter a Last Date of Exposure OR turn on Continuous Exposure')
-    verify_text_displayed('Please enter a valid Assigned Jurisdiction')
+    page.assert_no_text('Please enter a Last Date of Exposure OR turn on Continuous Exposure')
+    page.assert_text('Please enter a valid Assigned Jurisdiction')
     fill_in 'jurisdiction_id', with: 'fake jurisdiction'
     click_on 'Next'
-    verify_text_displayed('Please enter a valid Assigned Jurisdiction')
+    page.assert_text('Please enter a valid Assigned Jurisdiction')
     fill_in 'jurisdiction_id', with: 'USA'
     click_on 'Next'
-    verify_text_displayed('Please enter a valid Assigned Jurisdiction')
+    page.assert_text('Please enter a valid Assigned Jurisdiction')
     fill_in 'jurisdiction_id', with: 'USA, State 1, County 1'
     fill_in 'assigned_user', with: '-8.5'
     assert_not_equal('-8.5', page.find_field('assigned_user').value)
@@ -118,7 +118,7 @@ class EnrollmentFormValidator < ApplicationSystemTestCase
     assert_not_equal('W(#*&R#(W&', page.find_field('assigned_user').value)
     @@enrollment_form.populate_enrollment_step(:potential_exposure_information, potential_exposure_information)
     click_on 'OK' # confirm jurisdiction change
-    verify_text_not_displayed('Please enter a Last Date of Exposure OR turn on Continuous Exposure')
+    page.assert_no_text('Please enter a Last Date of Exposure OR turn on Continuous Exposure')
 
     # isolation fields
     @@system_test_utils.wait_for_enrollment_page_transition
@@ -130,29 +130,21 @@ class EnrollmentFormValidator < ApplicationSystemTestCase
     click_on 'edit-potential_exposure_information-btn'
     fill_in 'jurisdiction_id', with: '' # clear out jurisdiction to so that there is at least one validation error
     click_on 'Next'
-    verify_text_displayed('Please enter a Symptom Onset Date AND/OR a positive lab result.')
+    page.assert_text('Please enter a Symptom Onset Date AND/OR a positive lab result.')
     fill_in 'symptom_onset', with: 3.days.ago.strftime('%m/%d/%Y')
     click_on 'Next'
-    verify_text_not_displayed('Please enter a Symptom Onset Date AND/OR a positive lab result.')
+    page.assert_no_text('Please enter a Symptom Onset Date AND/OR a positive lab result.')
     fill_in 'symptom_onset', with: ''
     click_on 'Next'
-    verify_text_displayed('Please enter a Symptom Onset Date AND/OR a positive lab result.')
+    page.assert_text('Please enter a Symptom Onset Date AND/OR a positive lab result.')
     click_on 'Enter Lab Result'
     assert page.has_button?('Create', disabled: true)
     fill_in 'specimen_collection', with: 2.days.ago.strftime('%m/%d/%Y')
     click_on 'Create'
     click_on 'Next'
-    verify_text_not_displayed('Please enter a Symptom Onset Date AND/OR a positive lab result.')
+    page.assert_no_text('Please enter a Symptom Onset Date AND/OR a positive lab result.')
     fill_in 'symptom_onset', with: 3.days.ago.strftime('%m/%d/%Y')
     click_on 'Next'
-    verify_text_not_displayed('Please enter a Symptom Onset Date AND/OR a positive lab result.')
-  end
-
-  def verify_text_displayed(text)
-    assert page.has_content?(text), "Monitoree enrollment input validation - should display error message: #{text}"
-  end
-
-  def verify_text_not_displayed(text)
-    assert page.has_no_content?(text), "Monitoree enrollment input validation - should not display error message: #{text}"
+    page.assert_no_text('Please enter a Symptom Onset Date AND/OR a positive lab result.')
   end
 end
