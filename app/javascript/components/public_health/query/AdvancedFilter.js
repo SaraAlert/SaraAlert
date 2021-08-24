c6863c1395e278969a63ab0ebdd33551388462b0
sgober <35042815+sgober@users.noreply.github.com>
2021-08-24 15:26:13 -0400

    SARAALERT-1020: Updated multi advanced filter tooltip language (#1045)
    * updated advanced filter multi select tooltip language
    
    * additional language changes

--- app/javascript/components/public_health/query/AdvancedFilter.js
+++ app/javascript/components/public_health/query/AdvancedFilter.js
@@ -881,9 +881,6 @@ class AdvancedFilter extends React.Component {
    * @param {Array} value - Current values selected
    */
   renderMultiStatement = (filter, index, value) => {
-    // Tooltip for Multi-select type
-    let tooltip = 'Leaving this field blank will not filter out any monitorees.';
-
     return (
       <React.Fragment>
         <div className="d-flex justify-content-between py-0 my-0">
@@ -899,7 +896,7 @@ class AdvancedFilter extends React.Component {
               this.changeMultiValue(index, event);
             }}
           />
-          {tooltip && this.renderStatementTooltip(filter.name, index, tooltip)}
+          {filter.tooltip && this.renderStatementTooltip(filter.name, index, filter.tooltip)}
         </div>
       </React.Fragment>
     );
