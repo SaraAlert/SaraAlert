7b28207206c9fb78a4792bc839068073650b864e
Kyle Fagan <kfagan@mitre.org>
2021-08-25 15:05:53 -0700

    Add additional rollbacks to the down migration

--- db/migrate/20210824154651_update_vaccine_names.rb
+++ db/migrate/20210824154651_update_vaccine_names.rb
@@ -11,12 +11,23 @@ class UpdateVaccineNames < ActiveRecord::Migration[6.1]
   end
 
   def down
+    Vaccine.where(dose_number: '3').destroy
+
     Vaccine.where(product_name: 'Moderna COVID-19 Vaccine').update_all(product_name: 'Moderna COVID-19 Vaccine (Non-US tradename: Spikevax)')
     Vaccine.where(product_name: 'Pfizer-BioNTech COVID-19 Vaccine').update_all(product_name: 'Pfizer-BioNTech COVID-19 Vaccine (Tradename: COMIRNATY)')
+    # New Vaccines will get set to unknown
+    Vaccine.where(product_name: ['AstraZeneca COVID-19 Vaccine (Non-US tradenames: VAXZEVRIA, COVISHIELD)', 'Coronavac (Sinovac) COVID-19 Vaccine (Non-US)', 'Sinopharm (BIBP) COVID-19 Vaccine (Non-US)']).update_all(product_name: 'Unknown')
 
     ActiveRecord::Base.connection.raw_connection.prepare(Arel.sql("UPDATE user_export_presets SET config = REPLACE(config, 'Moderna COVID-19 Vaccine (Non-US tradename: Spikevax)', 'Moderna COVID-19 Vaccine') WHERE config REGEXP 'Moderna'")).execute
     ActiveRecord::Base.connection.raw_connection.prepare(Arel.sql("UPDATE user_export_presets SET config = REPLACE(config, 'Pfizer-BioNTech COVID-19 Vaccine (Tradename: COMIRNATY)', 'Pfizer-BioNTech COVID-19 Vaccine') WHERE config REGEXP 'Pfizer'")).execute
+    ActiveRecord::Base.connection.raw_connection.prepare(Arel.sql("UPDATE user_export_presets SET config = REPLACE(config, 'Coronavac (Sinovac) COVID-19 Vaccine (Non-US)', 'Unknown') WHERE config REGEXP 'Coronovac'")).execute
+    ActiveRecord::Base.connection.raw_connection.prepare(Arel.sql("UPDATE user_export_presets SET config = REPLACE(config, 'Sinopharm (BIBP) COVID-19 Vaccine (Non-US)', 'Unknown') WHERE config REGEXP 'Sinopharm'")).execute
+    ActiveRecord::Base.connection.raw_connection.prepare(Arel.sql("UPDATE user_export_presets SET config = REPLACE(config, 'AstraZeneca COVID-19 Vaccine (Non-US tradenames: VAXZEVRIA, COVISHIELD)', 'Unknown') WHERE config REGEXP 'AstraZeneca'")).execute
+
     ActiveRecord::Base.connection.raw_connection.prepare(Arel.sql("UPDATE user_filters SET contents = REPLACE(contents, 'Moderna COVID-19 Vaccine (Non-US tradename: Spikevax)', 'Moderna COVID-19 Vaccine') WHERE contents REGEXP 'Moderna'")).execute
     ActiveRecord::Base.connection.raw_connection.prepare(Arel.sql("UPDATE user_filters SET contents = REPLACE(contents, 'Pfizer-BioNTech COVID-19 Vaccine (Tradename: COMIRNATY)', 'Pfizer-BioNTech COVID-19 Vaccine') WHERE contents REGEXP 'Pfizer'")).execute
+    ActiveRecord::Base.connection.raw_connection.prepare(Arel.sql("UPDATE user_filters SET contents = REPLACE(contents, 'Coronavac (Sinovac) COVID-19 Vaccine (Non-US)', 'Unknown') WHERE contents REGEXP 'Coronovac'")).execute
+    ActiveRecord::Base.connection.raw_connection.prepare(Arel.sql("UPDATE user_filters SET contents = REPLACE(contents, 'Sinopharm (BIBP) COVID-19 Vaccine (Non-US)', 'Unknown') WHERE contents REGEXP 'Sinopharm'")).execute
+    ActiveRecord::Base.connection.raw_connection.prepare(Arel.sql("UPDATE user_filters SET contents = REPLACE(contents, 'AstraZeneca COVID-19 Vaccine (Non-US tradenames: VAXZEVRIA, COVISHIELD)', 'Unknown') WHERE contents REGEXP 'AstraZeneca'")).execute
   end
 end
