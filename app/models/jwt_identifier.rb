38e4cdb567b2e7d61a03d464c55b8c8c07479f1f
Kyle <Bialogs@users.noreply.github.com>
2021-09-17 10:32:53 -0700

    SARAALERT-1470: Rubocop/Rails and Rubocop/Performance updates to JwtIdentifier (#1070)

--- app/models/jwt_identifier.rb
+++ app/models/jwt_identifier.rb
@@ -6,7 +6,9 @@ class JwtIdentifier < ApplicationRecord
 
   # Finds all of the JWT identifiers that belong to JWTs that have since expired
   # and therefore no longer need to be checked.
+  # Time.zone is set by Rails.application.config.time_zone which defaults to UTC.
+  # Therefore, Time.zone.now makes UTC explicit and is consistient with previous behavior.
   scope :purge_eligible, lambda {
-    where('expiration_date <= ?', Time.now)
+    where('expiration_date <= ?', Time.zone.now)
   }
 end
