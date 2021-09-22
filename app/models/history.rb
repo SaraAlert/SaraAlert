1906983a1c9cfbb0a73393218788e68af934621a
Sarah G Ober <sgober@mitre.org>
2021-09-22 13:25:34 -0400

    reverted unintentional changes

--- app/models/history.rb
+++ app/models/history.rb
@@ -72,7 +72,7 @@ class History < ApplicationRecord
   # All histories within the given time frame
   scope :in_time_frame, lambda { |time_frame|
     case time_frame
-    when 'Yesterday'
+    when 'Last 24 Hours'
       where('histories.created_at >= ?', DateTime.now.utc - 24.hours)
     when 'Last 7 Days'
       where('histories.created_at >= ? AND histories.created_at < ?', (DateTime.now.utc - 7.days).beginning_of_day, DateTime.now.utc.beginning_of_day)
