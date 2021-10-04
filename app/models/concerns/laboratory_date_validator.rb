e01903fc99634536f739dce614c59163f3f5ea78
Kyle <Bialogs@users.noreply.github.com>
2021-10-04 14:50:11 -0700

    SARAALERT-1470: Validators (#1095)
    * Rubocop/Rails and Rubocop/Performance updates to LaboratoryDateValidator
    
    * Rubocop/Rails and Rubocop/Performance updates to IsolationSymptomOnsetValidator

--- app/models/concerns/laboratory_date_validator.rb
+++ app/models/concerns/laboratory_date_validator.rb
@@ -4,10 +4,12 @@
 class LaboratoryDateValidator < ActiveModel::Validator
   include ValidationHelper
 
+  # Time.zone is set by Rails.application.config.time_zone which defaults to UTC.
+  # Therefore, Time.zone.today makes UTC explicit and is consistient with previous behavior.
   def validate(record)
     year_start = Date.new(2020, 1, 1)
-    validate_between_dates(record, :report, year_start, Time.now.to_date) if record.report_changed?
-    validate_between_dates(record, :specimen_collection, year_start, Time.now.to_date) if record.specimen_collection_changed?
+    validate_between_dates(record, :report, year_start, Time.zone.today) if record.report_changed?
+    validate_between_dates(record, :specimen_collection, year_start, Time.zone.today) if record.specimen_collection_changed?
     return unless record.report && record.specimen_collection && (record.specimen_collection > record.report)
 
     record.errors.add(:report, 'cannot be before Specimen Collection Date.')
