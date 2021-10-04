73b2d056f007c1571ad050b51358ca0480309df2
Wong, Tim <twong@mitre.org>
2021-10-04 17:03:59 -0400

    set default limit to 200

---
+++ config/initializers/schema_string_limit.rb
@@ -0,0 +1,4 @@
+ActiveRecord::ConnectionAdapters::AbstractMysqlAdapter::NATIVE_DATABASE_TYPES[:string] = {
+  name: 'varchar',
+  limit: 200
+}
--- db/migrate/20211001151212_update_patient_table_string_lengths.rb
+++ db/migrate/20211001151212_update_patient_table_string_lengths.rb
@@ -1,185 +1,115 @@
 class UpdatePatientTableStringLengths < ActiveRecord::Migration[6.1]
-  def up
+  STRING_FIELDS = [
     # Identification
-    change_column :patients, :first_name, :string, limit: 200
-    change_column :patients, :last_name, :string, limit: 200
-    change_column :patients, :middle_name, :string, limit: 200
-    change_column :patients, :sex, :string, limit: 200
-    change_column :patients, :gender_identity, :string, limit: 200
-    change_column :patients, :sexual_orientation, :string, limit: 200
-    change_column :patients, :ethnicity, :string, limit: 200
-    change_column :patients, :nationality, :string, limit: 200
-    change_column :patients, :primary_language, :string, limit: 200
-    change_column :patients, :secondary_language, :string, limit: 200
-    change_column :patients, :user_defined_id_statelocal, :string, limit: 200
-    change_column :patients, :user_defined_id_cdc, :string, limit: 200
-    change_column :patients, :user_defined_id_nndss, :string, limit: 200
+    :first_name,
+    :last_name,
+    :middle_name,
+    :sex,
+    :gender_identity,
+    :sexual_orientation,
+    :ethnicity,
+    :nationality,
+    :primary_language,
+    :secondary_language,
+    :user_defined_id_statelocal,
+    :user_defined_id_cdc,
+    :user_defined_id_nndss,
 
     # Address
-    change_column :patients, :address_line_1, :string, limit: 200
-    change_column :patients, :address_line_2, :string, limit: 200
-    change_column :patients, :address_city, :string, limit: 200
-    change_column :patients, :address_state, :string, limit: 200
-    change_column :patients, :address_zip, :string, limit: 200
-    change_column :patients, :address_county, :string, limit: 200
-    change_column :patients, :foreign_address_line_1, :string, limit: 200
-    change_column :patients, :foreign_address_line_2, :string, limit: 200
-    change_column :patients, :foreign_address_city, :string, limit: 200
-    change_column :patients, :foreign_address_country, :string, limit: 200
-    change_column :patients, :foreign_address_zip, :string, limit: 200
-    change_column :patients, :foreign_address_line_3, :string, limit: 200
-    change_column :patients, :foreign_address_state, :string, limit: 200
-    change_column :patients, :monitored_address_line_1, :string, limit: 200
-    change_column :patients, :monitored_address_line_2, :string, limit: 200
-    change_column :patients, :monitored_address_city, :string, limit: 200
-    change_column :patients, :monitored_address_state, :string, limit: 200
-    change_column :patients, :monitored_address_zip, :string, limit: 200
-    change_column :patients, :monitored_address_county, :string, limit: 200
-    change_column :patients, :foreign_monitored_address_line_1, :string, limit: 200
-    change_column :patients, :foreign_monitored_address_line_2, :string, limit: 200
-    change_column :patients, :foreign_monitored_address_city, :string, limit: 200
-    change_column :patients, :foreign_monitored_address_state, :string, limit: 200
-    change_column :patients, :foreign_monitored_address_zip, :string, limit: 200
-    change_column :patients, :foreign_monitored_address_county, :string, limit: 200
+    :address_line_1,
+    :address_line_2,
+    :address_city,
+    :address_state,
+    :address_zip,
+    :address_county,
+    :foreign_address_line_1,
+    :foreign_address_line_2,
+    :foreign_address_city,
+    :foreign_address_country,
+    :foreign_address_zip,
+    :foreign_address_line_3,
+    :foreign_address_state,
+    :monitored_address_line_1,
+    :monitored_address_line_2,
+    :monitored_address_city,
+    :monitored_address_state,
+    :monitored_address_zip,
+    :monitored_address_county,
+    :foreign_monitored_address_line_1,
+    :foreign_monitored_address_line_2,
+    :foreign_monitored_address_city,
+    :foreign_monitored_address_state,
+    :foreign_monitored_address_zip,
+    :foreign_monitored_address_county,
 
     # Contact
-    change_column :patients, :preferred_contact_method, :string, limit: 200
-    change_column :patients, :preferred_contact_time, :string, limit: 200
-    change_column :patients, :primary_telephone, :string, limit: 200
-    change_column :patients, :primary_telephone_type, :string, limit: 200
-    change_column :patients, :secondary_telephone, :string, limit: 200
-    change_column :patients, :secondary_telephone_type, :string, limit: 200
-    change_column :patients, :international_telephone, :string, limit: 200
-    change_column :patients, :email, :string, limit: 200
+    :preferred_contact_method,
+    :preferred_contact_time,
+    :primary_telephone,
+    :primary_telephone_type,
+    :secondary_telephone,
+    :secondary_telephone_type,
+    :international_telephone,
+    :email,
 
     # Arrival
-    change_column :patients, :port_of_origin, :string, limit: 200
-    change_column :patients, :flight_or_vessel_number, :string, limit: 200
-    change_column :patients, :flight_or_vessel_carrier, :string, limit: 200
-    change_column :patients, :port_of_entry_into_usa, :string, limit: 200
-    change_column :patients, :source_of_report, :string, limit: 200
-    change_column :patients, :source_of_report_specify, :string, limit: 200
+    :port_of_origin,
+    :flight_or_vessel_number,
+    :flight_or_vessel_carrier,
+    :port_of_entry_into_usa,
+    :source_of_report,
+    :source_of_report_specify,
 
     # Additional Planned Travel
-    change_column :patients, :additional_planned_travel_type, :string, limit: 200
-    change_column :patients, :additional_planned_travel_destination, :string, limit: 200
-    change_column :patients, :additional_planned_travel_destination_state, :string, limit: 200
-    change_column :patients, :additional_planned_travel_destination_country, :string, limit: 200
-    change_column :patients, :additional_planned_travel_port_of_departure, :string, limit: 200
+    :additional_planned_travel_type,
+    :additional_planned_travel_destination,
+    :additional_planned_travel_destination_state,
+    :additional_planned_travel_destination_country,
+    :additional_planned_travel_port_of_departure,
 
     # Exposure
-    change_column :patients, :potential_exposure_location, :string, limit: 200
-    change_column :patients, :potential_exposure_country, :string, limit: 200
-    change_column :patients, :contact_of_known_case_id, :string, limit: 200
-    change_column :patients, :was_in_health_care_facility_with_known_cases_facility_name, :string, limit: 200
-    change_column :patients, :laboratory_personnel_facility_name, :string, limit: 200
-    change_column :patients, :healthcare_personnel_facility_name, :string, limit: 200
-    change_column :patients, :member_of_a_common_exposure_cohort_type, :string, limit: 200
+    :potential_exposure_location,
+    :potential_exposure_country,
+    :contact_of_known_case_id,
+    :was_in_health_care_facility_with_known_cases_facility_name,
+    :laboratory_personnel_facility_name,
+    :healthcare_personnel_facility_name,
+    :member_of_a_common_exposure_cohort_type,
 
     # Monitoring
-    change_column :patients, :exposure_risk_assessment, :string, limit: 200
-    change_column :patients, :monitoring_plan, :string, limit: 200
-    change_column :patients, :case_status, :string, limit: 200
-    change_column :patients, :public_health_action, :string, limit: 200
-    change_column :patients, :monitoring_reason, :string, limit: 200
-    change_column :patients, :follow_up_reason, :string, limit: 200
+    :exposure_risk_assessment,
+    :monitoring_plan,
+    :case_status,
+    :public_health_action,
+    :monitoring_reason,
+    :follow_up_reason,
 
     # Other
-    change_column :patients, :legacy_primary_language, :string, limit: 200
-    change_column :patients, :legacy_secondary_language, :string, limit: 200
-    change_column :patients, :time_zone, :string, limit: 200
-    change_column :patients, :submission_token, :binary, limit: 200
+    :legacy_primary_language,
+    :legacy_secondary_language,
+    :time_zone
+  ]
+
+  BINARY_FIELDS = %i[submission_token]
+
+  OLD_LIMIT = 255
+  NEW_LIMIT = 200
+
+  def up
+    ActiveRecord::Base.record_timestamps = false
+
+    STRING_FIELDS.each { |field| change_column :patients, field, :string, limit: NEW_LIMIT }
+    BINARY_FIELDS.each { |field| change_column :patients, field, :binary, limit: NEW_LIMIT }
+
+    ActiveRecord::Base.record_timestamps = true
   end
 
   def down
-    # Identification
-    change_column :patients, :first_name, :string
-    change_column :patients, :last_name, :string
-    change_column :patients, :middle_name, :string
-    change_column :patients, :sex, :string
-    change_column :patients, :gender_identity, :string
-    change_column :patients, :sexual_orientation, :string
-    change_column :patients, :ethnicity, :string
-    change_column :patients, :nationality, :string
-    change_column :patients, :primary_language, :string
-    change_column :patients, :secondary_language, :string
-    change_column :patients, :user_defined_id_statelocal, :string
-    change_column :patients, :user_defined_id_cdc, :string
-    change_column :patients, :user_defined_id_nndss, :string
+    ActiveRecord::Base.record_timestamps = false
 
-    # Address
-    change_column :patients, :address_line_1, :string
-    change_column :patients, :address_line_2, :string
-    change_column :patients, :address_city, :string
-    change_column :patients, :address_state, :string
-    change_column :patients, :address_zip, :string
-    change_column :patients, :address_county, :string
-    change_column :patients, :foreign_address_line_1, :string
-    change_column :patients, :foreign_address_line_2, :string
-    change_column :patients, :foreign_address_city, :string
-    change_column :patients, :foreign_address_country, :string
-    change_column :patients, :foreign_address_zip, :string
-    change_column :patients, :foreign_address_line_3, :string
-    change_column :patients, :foreign_address_state, :string
-    change_column :patients, :monitored_address_line_1, :string
-    change_column :patients, :monitored_address_line_2, :string
-    change_column :patients, :monitored_address_city, :string
-    change_column :patients, :monitored_address_state, :string
-    change_column :patients, :monitored_address_zip, :string
-    change_column :patients, :monitored_address_county, :string
-    change_column :patients, :foreign_monitored_address_line_1, :string
-    change_column :patients, :foreign_monitored_address_line_2, :string
-    change_column :patients, :foreign_monitored_address_city, :string
-    change_column :patients, :foreign_monitored_address_state, :string
-    change_column :patients, :foreign_monitored_address_zip, :string
-    change_column :patients, :foreign_monitored_address_county, :string
+    STRING_FIELDS.each { |field| change_column :patients, field, :string, limit: OLD_LIMIT }
+    BINARY_FIELDS.each { |field| change_column :patients, field, :binary, limit: OLD_LIMIT }
 
-    # Contact
-    change_column :patients, :preferred_contact_method, :string
-    change_column :patients, :preferred_contact_time, :string
-    change_column :patients, :primary_telephone, :string
-    change_column :patients, :primary_telephone_type, :string
-    change_column :patients, :secondary_telephone, :string
-    change_column :patients, :secondary_telephone_type, :string
-    change_column :patients, :international_telephone, :string
-    change_column :patients, :email, :string
-
-    # Arrival
-    change_column :patients, :port_of_origin, :string
-    change_column :patients, :flight_or_vessel_number, :string
-    change_column :patients, :flight_or_vessel_carrier, :string
-    change_column :patients, :port_of_entry_into_usa, :string
-    change_column :patients, :source_of_report, :string
-    change_column :patients, :source_of_report_specify, :string
-
-    # Additional Planned Travel
-    change_column :patients, :additional_planned_travel_type, :string
-    change_column :patients, :additional_planned_travel_destination, :string
-    change_column :patients, :additional_planned_travel_destination_state, :string
-    change_column :patients, :additional_planned_travel_destination_country, :string
-    change_column :patients, :additional_planned_travel_port_of_departure, :string
-
-    # Exposure
-    change_column :patients, :potential_exposure_location, :string
-    change_column :patients, :potential_exposure_country, :string
-    change_column :patients, :contact_of_known_case_id, :string
-    change_column :patients, :was_in_health_care_facility_with_known_cases_facility_name, :string
-    change_column :patients, :laboratory_personnel_facility_name, :string
-    change_column :patients, :healthcare_personnel_facility_name, :string
-    change_column :patients, :member_of_a_common_exposure_cohort_type, :string
-
-    # Monitoring
-    change_column :patients, :exposure_risk_assessment, :string
-    change_column :patients, :monitoring_plan, :string
-    change_column :patients, :case_status, :string
-    change_column :patients, :public_health_action, :string
-    change_column :patients, :monitoring_reason, :string
-    change_column :patients, :follow_up_reason, :string
-
-    # Other
-    change_column :patients, :legacy_primary_language, :string
-    change_column :patients, :legacy_secondary_language, :string
-    change_column :patients, :time_zone, :string
-    change_column :patients, :submission_token, :binary, limit: 255
+    ActiveRecord::Base.record_timestamps = true
   end
 end
