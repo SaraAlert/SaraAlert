71a4c8d4fdb1eb2dc506eb25b7ffa85b784069e1
Wong, Tim <twong@mitre.org>
2021-10-05 14:36:38 -0400

    remove and add indexes

--- db/migrate/20211001151212_update_patient_table_string_lengths.rb
+++ db/migrate/20211001151212_update_patient_table_string_lengths.rb
@@ -95,11 +95,77 @@ class UpdatePatientTableStringLengths < ActiveRecord::Migration[6.1]
   OLD_LIMIT = 255
   NEW_LIMIT = 200
 
+  def remove_current_indexes
+    remove_index :patients, name: "index_patients_on_assigned_user"
+    remove_index :patients, name: "index_patients_on_creator_id"
+    remove_index :patients, name: "index_patients_on_date_of_birth"
+    remove_index :patients, name: "index_patients_on_first_name"
+    remove_index :patients, name: "index_patients_on_id"
+    remove_index :patients, name: "index_patients_on_jurisdiction_id"
+    remove_index :patients, name: "index_patients_on_last_date_of_exposure"
+    remove_index :patients, name: "index_patients_on_potential_exposure_country"
+    remove_index :patients, name: "index_patients_on_public_health_action"
+    remove_index :patients, name: "index_patients_on_purged"
+    remove_index :patients, name: "index_patients_on_responder_id"
+    remove_index :patients, name: "index_patients_on_sex"
+    remove_index :patients, name: "index_patients_on_submission_token"
+    remove_index :patients, name: "index_patients_on_user_defined_id_cdc"
+    remove_index :patients, name: "index_patients_on_user_defined_id_nndss"
+    remove_index :patients, name: "index_patients_on_user_defined_id_statelocal"
+    remove_index :patients, name: "patients_index_chain_1"
+    remove_index :patients, name: "patients_index_chain_2"
+    remove_index :patients, name: "patients_index_chain_3"
+    remove_index :patients, name: "patients_index_chain_4"
+    remove_index :patients, name: "patients_index_chain_5"
+    remove_index :patients, name: "patients_index_chain_6"
+    remove_index :patients, name: "patients_index_chain_7"
+    remove_index :patients, name: "patients_index_chain_8"
+    remove_index :patients, name: "patients_index_chain_two_1"
+    remove_index :patients, name: "patients_index_chain_two_2"
+    remove_index :patients, name: "patients_index_chain_three_1"
+  end
+
+  def add_current_indexes
+    add_index :patients, :assigned_user
+    add_index :patients, :creator_id
+    add_index :patients, :date_of_birth
+    add_index :patients, :first_name
+    add_index :patients, :id
+    add_index :patients, :jurisdiction_id
+    add_index :patients, :last_date_of_exposure
+    add_index :patients, :potential_exposure_country
+    add_index :patients, :public_health_action
+    add_index :patients, :purged
+    add_index :patients, :responder_id
+    add_index :patients, :sex
+    add_index :patients, :submission_token
+    add_index :patients, :user_defined_id_cdc
+    add_index :patients, :user_defined_id_nndss
+    add_index :patients, :user_defined_id_statelocal
+    add_index :patients, [:monitoring, :purged, :public_health_action, :isolation, :jurisdiction_id], name: 'patients_index_chain_1'
+    add_index :patients, [:monitoring, :purged, :isolation, :jurisdiction_id], name: 'patients_index_chain_2'
+    add_index :patients, [:last_name, :first_name], name: 'patients_index_chain_3'
+    add_index :patients, [:id, :monitoring, :purged, :isolation, :symptom_onset], name: 'patients_index_chain_4'
+    add_index :patients, [:monitoring, :purged, :isolation, :id, :public_health_action], name: 'patients_index_chain_5'
+    add_index :patients, [:isolation, :jurisdiction_id], name: 'patients_index_chain_6'
+    add_index :patients, [:monitoring, :purged, :isolation], name: 'patients_index_chain_7'
+    add_index :patients, [:jurisdiction_id, :assigned_user], name: 'patients_index_chain_8'
+    add_index :patients, [:primary_telephone, :responder_id, :id, :jurisdiction_id], name: 'patients_index_chain_two_1'
+    add_index :patients, [:email, :responder_id, :id, :jurisdiction_id], name: 'patients_index_chain_two_2'
+    add_index :patients, [:jurisdiction_id, :isolation, :purged, :assigned_user], name: 'patients_index_chain_three_1'
+  end
+
+  # NOTE: removing and adding indexes significantly improves performance
+
   def up
     ActiveRecord::Base.record_timestamps = false
 
+    remove_current_indexes
+
     STRING_FIELDS.each { |field| change_column :patients, field, :string, limit: NEW_LIMIT }
     BINARY_FIELDS.each { |field| change_column :patients, field, :binary, limit: NEW_LIMIT }
+
+    add_current_indexes
 
     ActiveRecord::Base.record_timestamps = true
   end
@@ -107,8 +173,12 @@ class UpdatePatientTableStringLengths < ActiveRecord::Migration[6.1]
   def down
     ActiveRecord::Base.record_timestamps = false
 
+    remove_current_indexes
+
     STRING_FIELDS.each { |field| change_column :patients, field, :string, limit: OLD_LIMIT }
     BINARY_FIELDS.each { |field| change_column :patients, field, :binary, limit: OLD_LIMIT }
+
+    add_current_indexes
 
     ActiveRecord::Base.record_timestamps = true
   end
