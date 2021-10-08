743b0192311e7f7910dbce8bce343473f6cc4540
ngfreiter <nfreiter@mitre.org>
2021-10-08 16:49:54 -0500

    Add supplemental true to migrated symptoms when applicable
    The used-a-fever-reducer symptoms should all be set to supplemental
    true, so that they render correctly when editing an old report,
    and the rendering of reports is consistent across all reports.

--- db/migrate/20211006232125_add_supplemental_to_symptoms.rb
+++ db/migrate/20211006232125_add_supplemental_to_symptoms.rb
@@ -1,5 +1,6 @@
 class AddSupplementalToSymptoms < ActiveRecord::Migration[6.1]
   def change
     add_column :symptoms, :supplemental, :boolean, default: false
+    Symptom.where(name: 'used-a-fever-reducer').update_all(supplemental: true)
   end
 end
