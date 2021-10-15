e82f81c6a2adef70176bc71553b762a3bab4e97c
Kyle Fagan <kfagan@mitre.org>
2021-10-15 08:55:32 -0700

    add error class documentation

--- app/errors/invalid_messaging_method_error.rb
+++ app/errors/invalid_messaging_method_error.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# Raised when an invalid method is passed to the message sender (i.e. TwilioSender)
 class InvalidMessagingMethodError < StandardError
   def initialize(klass, method)
     super("#{klass}##{method} is not a valid message method")
