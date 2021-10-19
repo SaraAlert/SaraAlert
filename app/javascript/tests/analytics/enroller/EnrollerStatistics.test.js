226180dabf9cf78621db8e82ddbffc71393b9a09
Nick Freiter <52747882+ngfreiter@users.noreply.github.com>
2021-10-19 11:15:05 -0500

    Fix security issues (#1117)
    * Update several packages which were causing audit-ci errors
    
    Unfortunately, not all errors can be addressed until we can update
    @rails/webpacker, which we are waiting to do until they release
    6.0.0. So for now we are also ignorning certain errors that
    come from that package in audit-ci.
    
    Some tests also had to be updated after the jest update.
    
    * Bump puma to 4.3.9
    
    * Update tests for more consistent casting

--- app/javascript/tests/analytics/enroller/EnrollerStatistics.test.js
+++ app/javascript/tests/analytics/enroller/EnrollerStatistics.test.js
@@ -16,14 +16,14 @@ describe('EnrollerStatistics', () => {
   it('Properly Renders All Values and Headers correctly', () => {
     const wrapper = shallow(<EnrollerStatistics title={componentProps.title} total_monitorees={componentProps.total_monitorees} new_monitorees={componentProps.new_monitorees} total_reports={componentProps.total_reports} new_reports={componentProps.new_reports} />);
     expect(wrapper.find(Card).exists());
-    expect(wrapper.find(Card.Header).text()).toContain(componentProps.title);
-    expect(wrapper.find(Card.Body).find('#monitoree-analytics').find('h4').at(0).text()).toContain('TOTAL MONITOREES');
-    expect(wrapper.find(Card.Body).find('#monitoree-analytics').find('h3').at(0).text()).toContain(componentProps.total_monitorees);
-    expect(wrapper.find(Card.Body).find('#monitoree-analytics').find('h4').at(1).text()).toContain('NEW LAST 24 HOURS');
-    expect(wrapper.find(Card.Body).find('#monitoree-analytics').find('h3').at(1).text()).toContain(componentProps.new_monitorees);
-    expect(wrapper.find(Card.Body).find('#report-analytics').find('h4').at(0).text()).toContain('TOTAL REPORTS');
-    expect(wrapper.find(Card.Body).find('#report-analytics').find('h3').at(0).text()).toContain(componentProps.total_reports);
-    expect(wrapper.find(Card.Body).find('#report-analytics').find('h4').at(1).text()).toContain('NEW LAST 24 HOURS');
-    expect(wrapper.find(Card.Body).find('#report-analytics').find('h3').at(1).text()).toContain(componentProps.new_reports);
+    expect(wrapper.find(Card.Header).text()).toEqual(componentProps.title);
+    expect(wrapper.find(Card.Body).find('#monitoree-analytics').find('h4').at(0).text()).toEqual('TOTAL MONITOREES');
+    expect(wrapper.find(Card.Body).find('#monitoree-analytics').find('h3').at(0).text()).toEqual(String(componentProps.total_monitorees));
+    expect(wrapper.find(Card.Body).find('#monitoree-analytics').find('h4').at(1).text()).toEqual('NEW LAST 24 HOURS');
+    expect(wrapper.find(Card.Body).find('#monitoree-analytics').find('h3').at(1).text()).toEqual(String(componentProps.new_monitorees));
+    expect(wrapper.find(Card.Body).find('#report-analytics').find('h4').at(0).text()).toEqual('TOTAL REPORTS');
+    expect(wrapper.find(Card.Body).find('#report-analytics').find('h3').at(0).text()).toEqual(String(componentProps.total_reports));
+    expect(wrapper.find(Card.Body).find('#report-analytics').find('h4').at(1).text()).toEqual('NEW LAST 24 HOURS');
+    expect(wrapper.find(Card.Body).find('#report-analytics').find('h3').at(1).text()).toEqual(String(componentProps.new_reports));
   });
 });
