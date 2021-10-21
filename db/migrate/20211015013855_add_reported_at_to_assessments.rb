7c93a061277f81e33cf7d568e422253489913ad5
Tom Strassner <tstrassner@mitre.org>
2021-10-21 14:04:35 -0400

    Move reported_at index to add_column statement

--- db/migrate/20211015013855_add_reported_at_to_assessments.rb
+++ db/migrate/20211015013855_add_reported_at_to_assessments.rb
@@ -2,13 +2,11 @@ class AddReportedAtToAssessments < ActiveRecord::Migration[6.1]
   def change
     ActiveRecord::Base.record_timestamps = false
 
-    add_column :assessments, :reported_at, :datetime, precision: 6
+    add_column :assessments, :reported_at, :datetime, precision: 6, index: true
 
     up_only do
       execute 'update assessments set reported_at=created_at'
     end
-
-    add_index :assessments, :reported_at
 
     ActiveRecord::Base.record_timestamps = true
   end
