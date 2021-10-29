ebc47eca6c844da2250214d185865622b235f984
Nick Freiter <52747882+ngfreiter@users.noreply.github.com>
2021-10-29 15:41:03 -0500

    Fix bug with reporting eligibility icon in patient table (#1124)
    * Fix bug with display of status icon in patient table after filtering
    
    After filtering, the props of the EligibilityTooltip would change,
    but the state would not be updated, so the incorrect icon and tooltip
    would be shown. This fixes that so that state is updated when the props
    are updated.
    
    * Remove tracking of activeOption in state
    
    We really don't actually need to track activeOption in state,
    because as far as I can tell it should update every time that
    the component renders anyway.

--- app/javascript/components/util/EligibilityTooltip.js
+++ app/javascript/components/util/EligibilityTooltip.js
@@ -17,32 +17,30 @@ const eligibility_options = [
 class EligibilityTooltip extends React.Component {
   constructor(props) {
     super(props);
-    let activeOption = eligibility_options.find(eo => props.report_eligibility[`${eo.conditional}`]) || _.last(eligibility_options);
-    activeOption.messages = activeOption.messageCount === 1 ? [this.props.report_eligibility.messages[0]] : this.props.report_eligibility.messages;
-    this.state = {
-      activeOption,
-    };
   }
 
   render() {
+    let activeOption = eligibility_options.find(eo => this.props.report_eligibility[`${eo.conditional}`]) || _.last(eligibility_options);
+    activeOption.messages = activeOption.messageCount === 1 ? [this.props.report_eligibility.messages[0]] : this.props.report_eligibility.messages;
+
     return (
       <React.Fragment>
         <span key={`re-icon${this.props.id}`} data-for={`re${this.props.id}`} data-tip="">
-          {this.props.inline && <i className={`fa-fw fas ${this.state.activeOption.icon}`}></i>}
+          {this.props.inline && <i className={`fa-fw fas ${activeOption.icon}`}></i>}
           {!this.props.inline && (
             <div className="text-center ml-0">
-              <i className={`fa-fw fas ${this.state.activeOption.icon}`}></i>
+              <i className={`fa-fw fas ${activeOption.icon}`}></i>
             </div>
           )}
         </span>
         <ReactTooltip key={`re-tooltip${this.props.id}`} id={`re${this.props.id}`} multiline={true} type="dark" effect="solid" className="tooltip-container">
           <div>
-            <p className="lead mb-0">{this.state.activeOption.title}</p>
-            {this.state.activeOption.messageCount === 1 ? (
-              <span aria-label={this.state.activeOption.messages[0].message}>{this.state.activeOption.messages[0].message}</span>
+            <p className="lead mb-0">{activeOption.title}</p>
+            {activeOption.messageCount === 1 ? (
+              <span aria-label={activeOption.messages[0].message}>{activeOption.messages[0].message}</span>
             ) : (
               <ul className="pl-3 mb-0">
-                {this.state.activeOption.messages.map((m, index) => {
+                {activeOption.messages.map((m, index) => {
                   const message = m.datetime ? `${m.message} (${formatTimestamp(m.datetime)})` : m.message;
                   return (
                     <li className="mb-0" key={`rei${this.props.id}${index}`} aria-label={message}>
