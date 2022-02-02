d37c9d88d193e5418fd5bf7aca46223aac8d636f
Diana Eastman <deastman@mitre.org>
2022-02-02 13:50:44 -0500

    Add index after migration.

--- db/migrate/20220126135321_add_last_activity_at_to_user.rb
+++ db/migrate/20220126135321_add_last_activity_at_to_user.rb
@@ -3,12 +3,13 @@ class AddLastActivityAtToUser < ActiveRecord::Migration[6.1]
     ActiveRecord::Base.record_timestamps = false
 
     add_column :users, :last_activity_at, :datetime
-    add_index :users, :last_activity_at
 
     up_only do
       execute 'update users set last_activity_at=current_sign_in_at'
     end
 
     ActiveRecord::Base.record_timestamps = true
+
+    add_index :users, :last_activity_at
   end
 end
