fd1ed747d33c93cd30b445307bfe76c73c5695d6
Tim Wong <twong@mitre.org>
2022-02-24 15:02:37 -0500

    SARAALERT-1593: Clicking "All" or "None" for Assigned User filter should clear any entered numbers (#1207)
    * Clicking "All" or "None" for Assigned User filter should clear any entered numbers
    
    * fix sticky settings

--- app/javascript/components/public_health/query/AssignedUserFilter.js
+++ app/javascript/components/public_health/query/AssignedUserFilter.js
@@ -5,6 +5,20 @@ import { Button, Form, InputGroup, OverlayTrigger, Tooltip } from 'react-bootstr
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 class AssignedUserFilter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      assigned_user: props.assigned_user !== 'none' ? props.assigned_user : '',
+    };
+  }
+
+  // This update is necessary since sticky settings are loaded after this component is constructed
+  componentDidUpdate() {
+    if (this.state.assigned_user !== (this.props.assigned_user !== 'none' ? this.props.assigned_user : '')) {
+      this.setState({ assigned_user: this.props.assigned_user !== 'none' ? this.props.assigned_user : '' });
+    }
+  }
+
   handleAssignedUserChange = assigned_user => {
     if (!isNaN(assigned_user) && parseInt(assigned_user) > 0 && parseInt(assigned_user) <= 999999) {
       this.setState({ assigned_user }, () => {
@@ -36,7 +50,7 @@ class AssignedUserFilter extends React.Component {
           type="text"
           autoComplete="off"
           list="assigned_users"
-          defaultValue={this.props.assigned_user !== 'none' ? this.props.assigned_user : ''}
+          value={this.state.assigned_user || ''}
           onChange={event => this.handleAssignedUserChange(event?.target?.value)}
         />
         <datalist id="assigned_users">
