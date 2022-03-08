fd3f9be02e53410c9262a450beebc4cea3f45fea
Tim Wong <twong@mitre.org>
2022-03-08 15:52:32 -0500

    remove datatables (#1234)

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,24 +1,17 @@
-const { environment } = require('@rails/webpacker')
-const webpack = require('webpack')
-const datatables = require('./loaders/datatables');
+const { environment } = require('@rails/webpacker');
+const webpack = require('webpack');
 
-environment.plugins.prepend('Provide', new webpack.ProvidePlugin({
-  $: 'jquery/src/jquery',
-  jQuery: 'jquery/src/jquery',
-  jquery: 'jquery',
-  'window.jQuery': 'jquery',
-  Popper: ['popper.js', 'default']
-}))
+environment.plugins.prepend(
+  'Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery',
+    jquery: 'jquery',
+    'window.jQuery': 'jquery',
+    Popper: ['popper.js', 'default'],
+  })
+);
 
-environment.loaders.prepend('datatables', datatables)
+environment.splitChunks();
 
-const nodeModulesLoader = environment.loaders.get('nodeModules');
-if (!Array.isArray(nodeModulesLoader.exclude)) {
-  nodeModulesLoader.exclude = nodeModulesLoader.exclude == null ? [] : [nodeModulesLoader.exclude];
-}
-
-nodeModulesLoader.exclude.push(/react-table/);
-
-environment.splitChunks()
-
-module.exports = environment
+module.exports = environment;
