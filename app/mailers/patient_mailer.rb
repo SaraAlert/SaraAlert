8fac5befa1faba9061b8ef057506327d991f57f2
Wong, Tim <twong@mitre.org>
2022-04-06 14:24:28 -0400

    address feedback from @tstrass

--- app/mailers/patient_mailer.rb
+++ app/mailers/patient_mailer.rb
@@ -40,13 +40,14 @@ class PatientMailer < ApplicationMailer
     custom_msg = patient&.jurisdiction&.custom_messages_for_hierarchy&.dig('assessments', 'twilio', 'sms', 'prompt', 'intro', lang&.to_s)
     contents = if custom_msg.present?
                  "#{patient&.initials_age('-')},\n#{custom_msg}" +
-                   (custom_msg.exclude?(ADMIN_OPTIONS['privacy_policy_url']) && ADMIN_OPTIONS['privacy_policy_url'].present? ?
+                   (ADMIN_OPTIONS['privacy_policy_url'].present? ?
                      I18n.t('assessments.shared.privacy_info', locale: lang, privacy_policy_url: ADMIN_OPTIONS['privacy_policy_url']) : '')
                else
                  I18n.t('assessments.twilio.sms.prompt.intro', locale: lang, name: patient&.initials_age('-')) +
                    (ADMIN_OPTIONS['privacy_policy_url'].present? ?
                      ' ' + I18n.t('assessments.shared.privacy_info', locale: lang, privacy_policy_url: ADMIN_OPTIONS['privacy_policy_url']) : '')
                end
+    puts contents
     threshold_hash = patient.jurisdiction[:current_threshold_condition_hash]
     message = { prompt: contents, patient_submission_token: patient.submission_token, threshold_hash: threshold_hash }
     success = TwilioSender.send_sms(patient, [message])
